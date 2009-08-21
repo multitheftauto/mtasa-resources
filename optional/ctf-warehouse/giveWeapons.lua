55faa7b3a1e5c22e4af129e014860e7976bb87fa
matthewm.hinson <matthewm.hinson@gmail.com>
2009-08-21 16:38:44 +0000

    Removed ctf-warehouse and tdma-warehouse

--- optional/ctf-warehouse/giveWeapons.lua
+++
@@ -1,7 +0,0 @@
--- giveWeaponsOnSpawn for CTF: CS Italy by Ratt
-
-function giveWeaponsOnSpawn (  )
-	giveWeapon ( source, 31, 120 ) -- Gives the M4 weapon with 120 ammo
-    	giveWeapon ( source, 24, 56 ) -- Gives the Desert Eagle weapon with 56 ammo
-end
-addEventHandler ( "onPlayerSpawn", getRootElement(), giveWeaponsOnSpawn )
