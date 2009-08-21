6e543b4eaec782988acd682ac3cea1b3fa1c15db
matthewm.hinson <matthewm.hinson@126a8462-9353-1474-c972-ef1b1113b2d6>
2009-08-21 17:50:09 +0000

    Added defaultstats resource

---
+++ trunk/required/defaultstats/defaultstats.lua
@@ -0,0 +1,36 @@
+local stats = {
+    [ 69 ] = 500,
+    [ 70 ] = 999,
+    [ 71 ] = 999,
+    [ 72 ] = 999,
+    [ 73 ] = 500,
+    [ 74 ] = 999,
+    [ 75 ] = 500,
+    [ 76 ] = 999,
+    [ 77 ] = 999,
+    [ 78 ] = 999,
+    [ 79 ] = 999,
+	[ 160 ] = 999,
+	[ 229 ] = 999,
+	[ 230 ] = 999
+}
+
+local function applyStats(player)
+	for stat,value in pairs(stats) do
+		setPedStat(player, stat, value)
+	end
+end
+
+addEventHandler('onResourceStart', resourceRoot,
+	function()
+		for i,player in ipairs(getElementsByType('player')) do
+			applyStats(player)
+		end
+	end
+)
+
+addEventHandler('onPlayerJoin', root,
+	function()
+		applyStats(source)
+	end
+)
