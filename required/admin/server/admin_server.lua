08f682e2dac2c4f59ee43cc6c528ccdcdfc8ee02
ccw808 <ccw808@googlemail.com>
2009-11-22 18:04:39 +0000

    admin: Fixed 5071: (Patch: slapping players always takes 20hp regardless of chosen value)  Patch from Flobu

--- required/admin/server/admin_server.lua
+++ required/admin/server/admin_server.lua
@@ -783,6 +783,7 @@ addEventHandler ( "aPlayer", _root, function ( player, action, data, additional 
 			end
 		elseif ( action == "slap" ) then
 			if ( getElementHealth ( player ) > 0 ) and ( not isPedDead ( player ) ) then
+				local health = tonumber ( data )
 				if ( ( not health ) or ( not tonumber ( health ) ) ) then data = 20 end
 				if ( tonumber ( data ) > getElementHealth ( player ) ) then setTimer ( killPed, 50, 1, player )
 				else setElementHealth ( player, getElementHealth ( player ) - data ) end
