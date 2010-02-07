08161ceb351ab56f44084be1576242258cc04fa5
flobu@gmx.net <flobu@gmx.net@126a8462-9353-1474-c972-ef1b1113b2d6>
2010-02-07 19:39:39 +0000

    race_delay_indicator: database update from infoName to resName

--- trunk/optional/race_delay_indicator/delayindicator_server.lua
+++ trunk/optional/race_delay_indicator/delayindicator_server.lua
@@ -13,72 +13,38 @@ addEventHandler('onResourceStart', g_ResRoot,
 	function()
 		-- executeSQLDropTable("mapInterims")
 		-- Add table if required
-		executeSQLQuery("CREATE TABLE IF NOT EXISTS mapInterims (mapname TEXT, playername TEXT, interims TEXT)")
+		executeSQLQuery("CREATE TABLE IF NOT EXISTS mapinterims (mapname TEXT, playername TEXT, interims TEXT)")
 		-- Remove any duplicate mapname entries
-		executeSQLQuery("DELETE FROM mapInterims WHERE rowid in "
+		executeSQLQuery("DELETE FROM mapinterims WHERE rowid in "
 							.. " (SELECT A.rowid"
-							.. " FROM mapInterims A, mapInterims B"
+							.. " FROM mapinterims A, mapinterims B"
 							.. " WHERE A.rowid > B.rowid AND A.mapname = B.mapname)")
 		-- Add unique index to speed things up (Also means INSERT will fail if the unique index (mapname) already exists)
-		executeSQLQuery("CREATE UNIQUE INDEX IF NOT EXISTS IDX_MAPINTERIMS_MAPNAME on mapInterims(mapname)")
+		executeSQLQuery("CREATE UNIQUE INDEX IF NOT EXISTS IDX_MAPINTERIMS_MAPNAME on mapinterims(mapname)")
 
-		-- Perform upgrade from an old version
+		-- Perform upgrade from an old version if necessary
+		updateMapNames()
 		updatetopTimeInterims()
 	end
 )
 
-addEvent('onMapStarting')
-addEventHandler('onMapStarting', g_Root,
-	function(mapInfo)
-		-- if debug then outputDebugString("delay_indicator: sending data: "..tostring(mapInfo.name)) end
-		mapName = mapInfo.name
+addEventHandler('onGamemodeMapStart', g_Root,
+	function(mapres)
+		if debug then outputDebugString("delay_indicator: sending data: "..tostring(mapInfo.name)) end
+		mapName = getResourceName(mapres)
 		cpTimes = {}
 		allCpTimes = {}
 		topTimeInterims = nil
-		local sql = executeSQLQuery("SELECT * FROM mapInterims WHERE mapname = ?", mapName )
+		local sql = executeSQLQuery("SELECT playername, interims FROM mapinterims WHERE mapname = ?", mapName )
 		if #sql > 0 then
-			-- if debug then outputDebugString(tostring(sql[1].playername).." "..tostring(sql[1].interims)) end
+			if debug then outputDebugString(tostring(sql[1].playername).." "..tostring(sql[1].interims)) end
 			topTimeRankPlayer = split(sql[1].playername, string.byte(' '))
 			topTimeInterims = split(sql[1].interims, string.byte(','))
 			topTimeRankPlayer[1] = tonumber(topTimeRankPlayer[1])
 		end
-		-- if debug then outputDebugString(tostring(topTimeRankPlayer[1]).." "..tostring(topTimeRankPlayer[2])) end
+		if debug then outputDebugString(tostring(topTimeRankPlayer[1]).." "..tostring(topTimeRankPlayer[2])) end
 	end
 )
---[[
-addEvent('onPlayerReachCheckpoint')
-addEventHandler('onPlayerReachCheckpoint', g_Root,
-	function(checkpointNum, timePassed)
-		timePassed = math.floor(timePassed)
-		-- if debug then outputDebugString(tostring(getPlayerName(source)).." checkpointNum = "..tostring(checkpointNum).." timePassed = "..tostring(timePassed)) end
-		if not cpTimes[checkpointNum] then cpTimes[checkpointNum] = {} end
-		-- if debug then outputDebugString("player: "..tostring(cpTimes[checkpointNum].player).." checkpointNum = "..tostring(checkpointNum).." timePassed: "..tostring(cpTimes[checkpointNum].time).." "..tostring(topTimeInterims)) end
-		if cpTimes[checkpointNum].time and cpTimes[checkpointNum].player then
-			if cpTimes[checkpointNum].player ~= source then
-				local diff = timePassed - cpTimes[checkpointNum].time
-				if not players[source] then
-					triggerClientEvent(source, "showDelay", cpTimes[checkpointNum].player, diff)
-				end
-				if not players[cpTimes[checkpointNum].player] then
-					triggerClientEvent(cpTimes[checkpointNum].player, "showDelay", source, diff, checkpointNum)
-				end
-			end
-		else
-			if topTimeInterims and topTimeRankPlayer then
-				local diff = topTimeInterims[checkpointNum] - timePassed
-				-- if debug then outputDebugString(getPlayerName(source).." "..diff.." current record") end
-				if not players[source] then
-					triggerClientEvent(source, "showDelay", source, diff, topTimeRankPlayer)
-				end
-			end
-		end
-		cpTimes[checkpointNum].player = source
-		cpTimes[checkpointNum].time = timePassed
-		if not allCpTimes[source] then allCpTimes[source] = {} end
-		allCpTimes[source][checkpointNum] = timePassed
-		-- if debug then outputDebugString("allCpTimes: "..tostring(getPlayerName(source)).." checkpointNum = "..tostring(checkpointNum).." timePassed = "..tostring(allCpTimes[source][checkpointNum])) end
-	end
-)]]
 
 addEvent('onPlayerReachCheckpoint')
 addEventHandler('onPlayerReachCheckpoint', g_Root,
@@ -107,7 +73,7 @@ addEventHandler('onPlayerReachCheckpoint', g_Root,
                     if debug then outputDebugString("race_delay_indicator: ".."no lastPlayer use nothing") end
                 end
             end
-            if allCpTimes[frontPlayer][checkpointNum] then
+            if allCpTimes[frontPlayer] and allCpTimes[frontPlayer][checkpointNum] then
                 if debug then outputDebugString("race_delay_indicator: ".."use allCpTimes["..getPlayerName(frontPlayer).."]["..checkpointNum.."] = "..tostring(allCpTimes[frontPlayer][checkpointNum])) end
                 diff = timePassed - allCpTimes[frontPlayer][checkpointNum]
             else
@@ -138,35 +104,71 @@ end
 addEvent("onPlayerToptimeImprovement")
 addEventHandler("onPlayerToptimeImprovement", g_Root,
 	function(newPos)
-		-- if debug then outputDebugString(tostring(getPlayerName(source)).." "..tostring(newPos).." "..tostring(topTimeRankPlayer[1])) end
-		if newPos <= topTimeRankPlayer[1] then
+		if debug then outputDebugString(tostring(getPlayerName(source)).." "..tostring(newPos).." "..tostring(topTimeRankPlayer[1])) end
+		if newPos <= topTimeRankPlayer[1] and allCpTimes[source] then
 			local playername = newPos.." "..getPlayerName(source)
 			local interims = table.concat(allCpTimes[source], ",")
-			local sql = executeSQLQuery("SELECT * FROM mapInterims WHERE mapname = ?", mapName )
+			local sql = executeSQLQuery("SELECT * FROM mapinterims WHERE mapname = ?", mapName )
 			if #sql > 0 then
-				-- if debug then outputDebugString("mapinterims: update mapinterims "..playername.." "..interims) end
-				executeSQLQuery("UPDATE mapInterims SET playername=?, interims=? WHERE mapname=?", playername, interims, mapName )
+				if debug then outputDebugString("mapinterims: update mapinterims "..playername.." "..interims) end
+				executeSQLQuery("UPDATE mapinterims SET playername=?, interims=? WHERE mapname=?", playername, interims, mapName )
 			else
-				-- if debug then outputDebugString("mapinterims: insert mapinterims "..playername.." "..interims) end
-				executeSQLQuery("INSERT INTO mapInterims (mapName,playername,interims) VALUES (?,?,?)", mapName, playername, interims )
+				if debug then outputDebugString("mapinterims: insert mapinterims "..playername.." "..interims) end
+				executeSQLQuery("INSERT INTO mapinterims (mapname,playername,interims) VALUES (?,?,?)", mapName, playername, interims )
 			end
 		end
 	end
 )
 
 function updatetopTimeInterims()
-	sql = executeSQLQuery("SELECT * FROM mapInterims")
-	if debug then outputDebugString("mapInterims: start update "..tostring(#sql)) end
+	sql = executeSQLQuery("SELECT * FROM mapinterims")
+	local needUpdate
+	if sql and #sql > 0 then 
+		for i=1,math.min(5,#sql) do
+			if not string.find(sql[math.random(1,#sql)].playername," ") then
+				needUpdate = true
+				break
+			end
+		end
+	end
+	if not needUpdate then return end
+	outputDebugString("mapinterims: Need Player Update")
+	if debug then outputDebugString("mapinterims: start update "..tostring(#sql)) end
 	for _,row in ipairs(sql) do
 		if not string.find(row.playername, " ") then
 			local old = row.playername
 			row.playername = "1 "..old
-			executeSQLQuery("UPDATE mapInterims SET playername=? WHERE mapname=?", row.playername, row.mapname )
-			--outputConsole("mapInterims: changed "..tostring(old).." to "..tostring(row.playername).." in "..tostring(row.mapname), player)
-			if debug then outputDebugString("mapInterims: changed "..tostring(old).." to "..tostring(row.playername).." in "..tostring(row.mapname)) end
-		else
-			-- outputConsole("mapInterims: not changed "..tostring(row.playername).." in "..tostring(row.mapname), player)
-			if debug then outputDebugString("mapInterims: not changed "..tostring(row.playername).." in "..tostring(row.mapname)) end
+			executeSQLQuery("UPDATE mapinterims SET playername=? WHERE mapname=?", row.playername, row.mapname )
+			outputDebugString("mapinterims: changed "..tostring(old).." to "..tostring(row.playername).." in "..tostring(row.mapname))
+		end
+	end
+end
+
+function updateMapNames()
+	local sql = executeSQLQuery("SELECT * FROM mapinterims")
+	local needUpdate
+	if sql and #sql > 0 then 
+		for i=1,math.min(5,#sql) do
+			if not getResourceFromName(sql[math.random(1,#sql)].mapname) then
+				needUpdate = true
+				break
+			end
+		end
+	end
+	if not needUpdate then return end
+	outputDebugString("mapinterims: Need Mapname Update")
+	local maps = exports.mapmanager:getMaps()
+	local infoMaps = {}
+	for _,map in ipairs(exports.mapmanager:getMaps()) do
+		infoMaps[getResourceInfo(map, "name") or getResourceName(map)] = getResourceName(map)
+	end
+	for _,row in ipairs(sql) do
+		if infoMaps[row.mapname] then
+			executeSQLQuery("UPDATE mapinterims SET mapname=? WHERE mapname=?", infoMaps[row.mapname], row.mapname)
+			outputDebugString("mapinterims: changed "..tostring(row.mapname).." to "..tostring(infoMaps[row.mapname]))
+		elseif not getResourceFromName(row.mapname) then
+			executeSQLQuery("DELETE FROM mapinterims WHERE mapname=?", row.mapname)
+			outputDebugString("mapinterims: deleted "..tostring(row.mapname))
 		end
 	end
 end
