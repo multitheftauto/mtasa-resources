be089c3ddd50d128175d4b88d3c4c366e36d3ae0
ccw808 <ccw808@126a8462-9353-1474-c972-ef1b1113b2d6>
2010-05-23 13:03:48 +0000

    Fixed #5328: ([patch] Scoreboard: error on player quit) - patch from robhol

--- trunk/required/scoreboard/scoreboard_http.lua
+++ trunk/required/scoreboard/scoreboard_http.lua
@@ -21,6 +21,9 @@ local function getName(element)
 end
 
 local function getRowData( element )
+
+	if not isElement(element) then return end
+
 	local rowData = {getElementType(element),}
 	for i, column in ipairs(httpColumns) do
 		if column.name == "name" then
@@ -63,7 +66,9 @@ local function refreshServerScoreboard()
 		if isElement(team) then		-- Sanity check, as sometimes team is not valid here. (Why?)
 			table.insert(scoreboardNewRows,getRowData(team))
 			for i,player in ipairs(getPlayersInTeam(team)) do
-				table.insert(scoreboardNewRows,getRowData(player))
+				if isElement(player) and getElementType(player) == "player" then
+					table.insert(scoreboardNewRows,getRowData(player))
+				end
 			end
 		end
 	end
