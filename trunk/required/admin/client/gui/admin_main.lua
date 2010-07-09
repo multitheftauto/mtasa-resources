505cf03a2f2d60bb8e90a80c1670645f3bb1f0f3
ccw808 <ccw808@126a8462-9353-1474-c972-ef1b1113b2d6>
2010-07-09 10:48:25 +0000

    admin: Removed non working auto login checkbox

--- trunk/required/admin/client/gui/admin_main.lua
+++ trunk/required/admin/client/gui/admin_main.lua
@@ -242,6 +242,7 @@ function aAdminMenu ()
 						  guiCreateHeader (  0.03, 0.30, 0.47, 0.04, "Appearance:", true, aTab6.Tab )
 						  guiCreateHeader ( 0.63, 0.05, 0.10, 0.05, "Account:", true, aTab6.Tab )
 		aTab6.AutoLogin		= guiCreateCheckBox ( 0.65, 0.10, 0.47, 0.04, "Auto-login by serial", false, true, aTab6.Tab )
+						  guiSetVisible ( aTab6.AutoLogin, false )	-- Not used
 						  guiCreateHeader ( 0.63, 0.15, 0.25, 0.05, "Change Password:", true, aTab6.Tab )
 						  guiCreateLabel ( 0.65, 0.20, 0.15, 0.05, "Old password:", true, aTab6.Tab )
 						  guiCreateLabel ( 0.65, 0.25, 0.15, 0.05, "New password:", true, aTab6.Tab )
