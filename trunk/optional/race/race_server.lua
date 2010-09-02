7fa85c0192bc2ea8a1c1da0c415d92cd87581c78
flobu@gmx.net <flobu@gmx.net@126a8462-9353-1474-c972-ef1b1113b2d6>
2010-09-02 23:05:40 +0000

    race: Added an option to set the default respawn mode

--- trunk/optional/race/race_server.lua
+++ trunk/optional/race/race_server.lua
@@ -86,6 +86,7 @@ function cacheGameOptions()
 	g_GameOptions = {}
 	g_GameOptions.timeafterfirstfinish  = getNumber('race.timeafterfirstfinish',30) * 1000
 	g_GameOptions.hurrytime				= getNumber('race.hurrytime',15) * 1000
+	g_GameOptions.defaultrespawnmode	= getString('race.respawnmode','timelimit')
 	g_GameOptions.defaultrespawntime	= getNumber('race.respawntime',5) * 1000
 	g_GameOptions.defaultduration		= getNumber('race.duration',6000) * 1000
 	g_GameOptions.ghostmode				= getBool('race.ghostmode',false)
@@ -135,10 +136,8 @@ function cacheMapOptions(map)
 	if g_MapOptions.duration > 86400000 then
 		g_MapOptions.duration = 86400000
 	end
-	g_MapOptions.respawn = map.respawn
-	if not g_MapOptions.respawn then
-		g_MapOptions.respawn = 'none'
-	elseif g_MapOptions.respawn ~= 'timelimit' and g_MapOptions.respawn ~= 'none' then
+	g_MapOptions.respawn = map.respawn or g_GameOptions.defaultrespawnmode
+	if g_MapOptions.respawn ~= 'timelimit' and g_MapOptions.respawn ~= 'none' then
 		g_MapOptions.respawn = 'timelimit'
 	end
 	g_MapOptions.respawntime	= g_MapOptions.respawn == 'timelimit' and (map.respawntime and map.respawntime*1000 or g_GameOptions.defaultrespawntime)
