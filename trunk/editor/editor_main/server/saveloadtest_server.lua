977bf0614e58d13ee26340e3198c8f4c1f2e7ce2
arrantuna@hotmail.co.uk <arrantuna@hotmail.co.uk@126a8462-9353-1474-c972-ef1b1113b2d6>
2011-02-23 10:46:31 +0000

    Map Editor: Fixed #5882 (editor_main: Too long execution error when saving maps) Coroutines are now in use for 'quick save' and 'save as' which removes the possibility of maps failing to save due to the script being aborted from a too long execution. Also added math.floor() to the milisecond output for loading because sometimes it would display 10.0000000001 ms for example.

--- trunk/editor/editor_main/server/saveloadtest_server.lua
+++ trunk/editor/editor_main/server/saveloadtest_server.lua
@@ -126,7 +126,7 @@ function handleOpenResource()
 			dumpSave()
 		else
 			loadedMap = openingMapName
-			outputDebugString ( "Loaded map in "..getTickCount()-openingStartTick.." ms" )
+			outputDebugString ( "Loaded map in "..math.floor(getTickCount()-openingStartTick).." ms" )
 		end
 		triggerEvent("onMapOpened", mapContainer, openingResource)
 		
@@ -232,7 +232,8 @@ addEventHandler ( "openResource", rootElement, openResource )
 
 
 ---Save
-function saveResource ( resourceName, test )
+
+function saveResource(resourceName, test)
 	if isEditorOpeningResource() then
 		return
 	end
@@ -242,6 +243,22 @@ function saveResource ( resourceName, test )
 		return
 	end
 	
+	saveResourceCoroutine = coroutine.create(saveResourceCoroutineFunction)
+	coroutine.resume(saveResourceCoroutine, resourceName, test, client, client)
+end
+addEventHandler ( "saveResource", rootElement, saveResource )
+
+local specialSyncers = {
+	position = function() end,
+	rotation = function() end,
+	dimension = function(element) return 0 end,
+	interior = function(element) return edf.edfGetElementInterior(element) end,
+	parent = function(element) return getElementData(element, "me:parent") end,
+}
+
+function saveResourceCoroutineFunction ( resourceName, test, theSaver, client )
+	local tick = getTickCount()
+	local iniTick = getTickCount()
 	if ( loadedMap ) then
 		if string.lower(loadedMap) == string.lower(resourceName) then
 			quickSave(true)
@@ -280,11 +297,106 @@ function saveResource ( resourceName, test )
 			return
 		end
 	end
+	editor_gui.outputMessage("The map is being saved... (this could take a while)", root,255,0,0)
 	if ( loadedMap ) and ( loadedMap ~= resourceName ) then
 		metaNodes = copyResourceFiles ( getResourceFromName(loadedMap), resource )
 	end
 	local xmlNode = addResourceMap ( resource, resourceName..".map" )
-	local returnValue = dumpMap ( xmlNode, true )
+	--local returnValue = dumpMap ( xmlNode, true )
+	baseElement = mapContainer
+	local elementChildren = {}
+	local rootElements = {}
+	local usedResources = {}
+	local tick = getTickCount()
+	for i, element in ipairs(getElementChildren(baseElement)) do  --Find parents to start with
+		--ignore representations and destroyed elements
+		if not edf.edfIsRepresentation(element) and getElementDimension(element) ~= DESTROYED_ELEMENT_DIMENSION then
+			local parent = getElementData ( element, "me:parent" )
+			if not parent or getElementType(parent) == "map" then
+				table.insert ( rootElements, element )
+				elementChildren[element] = elementChildren[element] or {}
+			else
+				elementChildren[element] = elementChildren[element] or {}
+				elementChildren[parent] = elementChildren[parent] or {}
+				table.insert ( elementChildren[parent], element )
+			end
+
+			local creatorResource = getResourceName(edf.edfGetCreatorResource(element))
+			usedResources[creatorResource] = true
+		end
+		if (getTickCount() > tick + 200) then
+			setTimer(function()
+				if (coroutine.status(saveResourceCoroutine) == "suspended") then
+					coroutine.resume(saveResourceCoroutine)
+				elseif (coroutine.status(saveResourceCoroutine) == "dead") then
+					saveResourceCoroutine = nil
+				end
+			end, 200, 1)
+			coroutine.yield()
+			tick = getTickCount()
+		end
+	end
+	
+	-- Save in the map node the used definitions
+	local usedDefinitions = ""
+	for resource in pairs(usedResources) do
+		usedDefinitions = usedDefinitions .. resource .. ","
+	end
+	if usedDefinitions ~= "" then
+		usedDefinitions = string.sub(usedDefinitions, 1, #usedDefinitions - 1)
+		xmlNodeSetAttribute(xmlNode, "edf:definitions", usedDefinitions)
+	end
+	--dumpNodes ( xmlNode, rootElements, elementChildren )
+	local tick = getTickCount()
+	
+	for i, element in ipairs(rootElements) do
+		if (getTickCount() > tick + 200) then
+			setTimer(function()
+				if (coroutine.status(saveResourceCoroutine) == "suspended") then
+					coroutine.resume(saveResourceCoroutine)
+				elseif (coroutine.status(saveResourceCoroutine) == "dead") then
+					saveResourceCoroutine = nil
+				end
+			end, 200, 1)
+			coroutine.yield()
+			tick = getTickCount()
+		end
+		-- create element subnode
+		local elementNode = xmlCreateChild(xmlNode, getElementType(element))
+		--add an ID attribute first off
+		xmlNodeSetAttribute(elementNode, "id", getElementID(element))
+		--dump raw properties from the getters
+--		outputDebugString(tostring(loadedEDF).."["..tostring(edf.edfGetCreatorResource(element)).."]"..".elements["..tostring(getElementType(element)).."].data")
+		for dataField in pairs(loadedEDF[edf.edfGetCreatorResource(element)].elements[getElementType(element)].data) do
+			local value
+			if specialSyncers[dataField] then
+				value = specialSyncers[dataField](element)
+			else
+				value = edf.edfGetElementProperty(element, dataField)
+			end
+			if type(value) == "number" or type(value) == "string" then
+				xmlNodeSetAttribute(elementNode, dataField, value )
+			end
+		end
+		-- dump properties to attributes
+		for dataName, dataValue in orderedPairs(getMapElementData(element)) do
+			if dataName == "position" then
+				xmlNodeSetAttribute(elementNode, "posX", toAttribute(dataValue[1]))
+				xmlNodeSetAttribute(elementNode, "posY", toAttribute(dataValue[2]))
+				xmlNodeSetAttribute(elementNode, "posZ", toAttribute(dataValue[3]))
+			elseif dataName == "rotation" then
+				xmlNodeSetAttribute(elementNode, "rotX", toAttribute(dataValue[1]))
+				xmlNodeSetAttribute(elementNode, "rotY", toAttribute(dataValue[2]))
+				xmlNodeSetAttribute(elementNode, "rotZ", toAttribute(dataValue[3]))
+			elseif not specialSyncers[dataName] or dataValue ~= getWorkingDimension() then
+				xmlNodeSetAttribute(elementNode, dataName, toAttribute(dataValue))
+			end
+		end
+		dumpNodes ( elementNode, elementChildren[element], elementChildren )
+	end
+	
+	
+	local returnValue = xmlSaveFile(xmlNode)
 	clearResourceMeta ( resource, true )
 	local metaNode = xmlLoadFile ( ':' .. getResourceName(resource) .. '/' .. "meta.xml" )
 	dumpMeta ( metaNode, metaNodes, resource, resourceName..".map", test )
@@ -292,21 +404,33 @@ function saveResource ( resourceName, test )
 	if ( test ) then return returnValue end
 	if returnValue then
 		loadedMap = resourceName
-		editor_gui.outputMessage (getPlayerName(source).." saved to map resource \""..resourceName.."\".", root,255,0,0)
+		if (theSaver) then
+			editor_gui.outputMessage (getPlayerName(theSaver).." saved to map resource \""..resourceName.."\".", root,255,0,0)
+		end
 	end
-	triggerClientEvent ( client, "saveloadtest_return", client, "save", returnValue, resourceName )
+	if (theSaver) then
+		triggerClientEvent ( theSaver, "saveloadtest_return", theSaver, "save", returnValue, resourceName )
+	end
 	dumpSave()
+	outputDebugString("Full saving of map complete in "..math.floor(getTickCount() - iniTick).." ms")
 	return returnValue
 end
-addEventHandler ( "saveResource", rootElement, saveResource )
 
 function quickSave(saveAs, dump)
 	if client and not isPlayerAllowedToDoEditorAction(client,"save") then
 		editor_gui.outputMessage ("You don't have permissions to save the map!", client,255,0,0)
 		return
 	end
-	
+	quickSaveCoroutine = coroutine.create(quickSaveCoroutineFunction)
+	coroutine.resume(quickSaveCoroutine, saveAs, dump, client)
+end
+addEventHandler("quickSaveResource", rootElement, quickSave )
+
+function quickSaveCoroutineFunction(saveAs, dump, client)
 	if loadedMap then
+		editor_gui.outputMessage("The map is being saved... (this could take a while)", root,255,0,0)
+		local tick = getTickCount()
+		local iniTick = getTickCount()
 		local resource = getResourceFromName ( dump and DUMP_RESOURCE or loadedMap )
 		local mapTable = getResourceFiles ( resource,"map" )
 		if not mapTable then
@@ -327,13 +451,105 @@ function quickSave(saveAs, dump)
 			triggerClientEvent ( client, "saveloadtest_return", client, "quickSave", false, loadedMap )
 			return
 		end
-		dumpMap ( xmlNode, true )
+		--dumpMap ( xmlNode, true )
+		baseElement = baseElement or mapContainer
+		local elementChildren = {}
+		local rootElements = {}
+		local usedResources = {}
+
+		for i, element in ipairs(getElementChildren(baseElement)) do  --Find parents to start with
+			if (getTickCount() > tick + 200) then
+				setTimer(function()
+					if (coroutine.status(quickSaveCoroutine) == "suspended") then
+						coroutine.resume(quickSaveCoroutine)
+					elseif (coroutine.status(quickSaveCoroutine) == "dead") then
+						quickSaveCoroutine = nil
+					end
+				end, 200, 1)
+				coroutine.yield()
+				tick = getTickCount()
+			end
+			--ignore representations and destroyed elements
+			if not edf.edfIsRepresentation(element) and getElementDimension(element) ~= DESTROYED_ELEMENT_DIMENSION then
+				local parent = getElementData ( element, "me:parent" )
+				if not parent or getElementType(parent) == "map" then
+					table.insert ( rootElements, element )
+					elementChildren[element] = elementChildren[element] or {}
+				else
+					elementChildren[element] = elementChildren[element] or {}
+					elementChildren[parent] = elementChildren[parent] or {}
+					table.insert ( elementChildren[parent], element )
+				end
+
+				local creatorResource = getResourceName(edf.edfGetCreatorResource(element))
+				usedResources[creatorResource] = true
+			end
+		end
+		-- Save in the map node the used definitions
+		local usedDefinitions = ""
+		for resource in pairs(usedResources) do
+			usedDefinitions = usedDefinitions .. resource .. ","
+		end
+		if usedDefinitions ~= "" then
+			usedDefinitions = string.sub(usedDefinitions, 1, #usedDefinitions - 1)
+			xmlNodeSetAttribute(xmlNode, "edf:definitions", usedDefinitions)
+		end
+		--dumpNodes ( xmlNode, rootElements, elementChildren )
+		
+		for i, element in ipairs(rootElements) do
+			if (getTickCount() > tick + 200) then
+				setTimer(function()
+					if (coroutine.status(quickSaveCoroutine) == "suspended") then
+						coroutine.resume(quickSaveCoroutine)
+					elseif (coroutine.status(quickSaveCoroutine) == "dead") then
+						quickSaveCoroutine = nil
+					end
+				end, 200, 1)
+				coroutine.yield()
+				tick = getTickCount()
+			end
+			-- create element subnode
+			local elementNode = xmlCreateChild(xmlNode, getElementType(element))
+			--add an ID attribute first off
+			xmlNodeSetAttribute(elementNode, "id", getElementID(element))
+			--dump raw properties from the getters
+	--		outputDebugString(tostring(loadedEDF).."["..tostring(edf.edfGetCreatorResource(element)).."]"..".elements["..tostring(getElementType(element)).."].data")
+			for dataField in pairs(loadedEDF[edf.edfGetCreatorResource(element)].elements[getElementType(element)].data) do
+				local value
+				if specialSyncers[dataField] then
+					value = specialSyncers[dataField](element)
+				else
+					value = edf.edfGetElementProperty(element, dataField)
+				end
+				if type(value) == "number" or type(value) == "string" then
+					xmlNodeSetAttribute(elementNode, dataField, value )
+				end
+			end
+			-- dump properties to attributes
+			for dataName, dataValue in orderedPairs(getMapElementData(element)) do
+				if dataName == "position" then
+					xmlNodeSetAttribute(elementNode, "posX", toAttribute(dataValue[1]))
+					xmlNodeSetAttribute(elementNode, "posY", toAttribute(dataValue[2]))
+					xmlNodeSetAttribute(elementNode, "posZ", toAttribute(dataValue[3]))
+				elseif dataName == "rotation" then
+					xmlNodeSetAttribute(elementNode, "rotX", toAttribute(dataValue[1]))
+					xmlNodeSetAttribute(elementNode, "rotY", toAttribute(dataValue[2]))
+					xmlNodeSetAttribute(elementNode, "rotZ", toAttribute(dataValue[3]))
+				elseif not specialSyncers[dataName] or dataValue ~= getWorkingDimension() then
+					xmlNodeSetAttribute(elementNode, dataName, toAttribute(dataValue))
+				end
+			end
+			dumpNodes ( elementNode, elementChildren[element], elementChildren )
+		end
+		xmlSaveFile(xmlNode)
+		
 		xmlUnloadFile ( xmlNode )
 		local metaNode = xmlLoadFile ( ':' .. getResourceName(resource) .. '/' .. "meta.xml" )
 		dumpMeta ( metaNode, {}, resource, loadedMap..".map" )
 		xmlUnloadFile ( metaNode )
+		outputDebugString("Quick save complete in "..math.floor(getTickCount() - iniTick).." ms")
 		if not dump and loadedMap == DUMP_RESOURCE then
-			editor_gui.loadsave_getResources("saveAs",source)
+			editor_gui.loadsave_getResources("saveAs",client)
 			return
 		end
 		if saveAs then
@@ -344,11 +560,9 @@ function quickSave(saveAs, dump)
 			dumpSave()
 		end
 	else
-		editor_gui.loadsave_getResources("saveAs",source)
+		editor_gui.loadsave_getResources("saveAs",client)
 	end
 end
-addEventHandler("quickSaveResource", rootElement, quickSave )
-
 
 ------TESTING
 local testBackupNodes = {}
@@ -366,7 +580,9 @@ function (gamemodeName)
 	end
 	g_restoreEDF = nil
 	triggerClientEvent ( root, "suspendGUI", client )
-	local success = saveResource ( TEST_RESOURCE, true )
+	--local success = saveResource ( TEST_RESOURCE, true )
+	saveResourceCoroutine = coroutine.create(saveResourceCoroutineFunction)
+	local success = coroutine.resume(saveResourceCoroutine, TEST_RESOURCE, true)
 	if ( success ) then
 		beginTest(client,gamemodeName)
 	else
