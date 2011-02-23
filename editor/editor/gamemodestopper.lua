69ba82d47998709479fe100dfc6b54d52bad0b60
arrantuna@hotmail.co.uk <arrantuna@hotmail.co.uk>
2011-02-23 10:55:28 +0000

    Map Editor: Instead of getting debug warnings saying access denied (and there could be multiple warnings) output one message informing the user of the situation.

--- editor/editor/gamemodestopper.lua
+++ editor/editor/gamemodestopper.lua
@@ -14,7 +14,12 @@ addEventHandler("onResourceStart", getResourceRootElement(),
 	function()
 		for index,resource in ipairs(getResources()) do
 			if isResourceRunning(resource) and (isGamemode(resource) or isMap(resource)) then
-				stopResource(resource)
+				if hasObjectPermissionTo(getThisResource(), "function.stopResource") then
+					stopResource(resource)
+				else
+					outputDebugString("Editor: Unable to stop running gamemodes (no access to function.stopResource)")
+					return
+				end
 			end
 		end
 	end
