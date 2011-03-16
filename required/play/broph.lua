d2f7ba0a657fcda22c604b4ae94a20c5f95022c6
arrantuna@hotmail.co.uk <arrantuna@hotmail.co.uk>
2011-03-16 17:58:38 +0000

    play: Fixed #5946 (play: Some crucial gameplay elements are not defaulted)

--- required/play/broph.lua
+++ required/play/broph.lua
@@ -1,28 +1,15 @@
 addEventHandler("onResourceStart", resourceRoot,
 	function()
+		resetMapInfo()
 		for i,player in ipairs(getElementsByType("player")) do
 			spawn(player)
 		end
-		setGravity(0.008)
-		setGameSpeed(1)
-		setTime(12, 0)
-		setWeather(0)
 	end
 )
 
 function spawn(player)
 	if not isElement(player) then return end
-	setElementInterior(player, 0)
-	setElementDimension(player, 0)
-	showPlayerHudComponent(player, "ammo", true)
-	showPlayerHudComponent(player, "armour", true)
-	showPlayerHudComponent(player, "breath", true)
-	showPlayerHudComponent(player, "clock", true)
-	showPlayerHudComponent(player, "health", true)
-	showPlayerHudComponent(player, "money", true)
-	showPlayerHudComponent(player, "radar", true)
-	showPlayerHudComponent(player, "weapon", true)
-	repeat until spawnPlayer ( player, -711+math.random(1,5), 957+math.random(5,9), 12.4, 90, math.random(9,288) )
+	repeat until spawnPlayer ( player, -711+math.random(1,5), 957+math.random(5,9), 12.4, 90, math.random(9,288), 0, 0)
 	fadeCamera(player, true)
 	setCameraTarget(player, player)
 	showChat(player, true)
