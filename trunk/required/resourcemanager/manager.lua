810b5cae4bc34b11647ad8a0e78790ffd9111dea
arrantuna@hotmail.co.uk <arrantuna@hotmail.co.uk@126a8462-9353-1474-c972-ef1b1113b2d6>
2011-04-05 09:50:08 +0000

    resourcemanager: Added #6025 (Normal date in resource manager)

--- trunk/required/resourcemanager/manager.lua
+++ trunk/required/resourcemanager/manager.lua
@@ -22,9 +22,21 @@ function getResourceInfo ( resource )
 
     local failreason =  getResourceLoadFailureReason ( resource )
     local state = getResourceState ( resource )
-    local starttime = getResourceLastStartTime ( resource )
-    local loadtime = getResourceLoadTime ( resource )
+    local starttime = getRealTimes(getResourceLastStartTime ( resource ))
+    local loadtime = getRealTimes(getResourceLoadTime ( resource ))
 	local author =  mta_getResourceInfo ( resource, "author" )
 	local version = mta_getResourceInfo ( resource, "version" )
     return {state=state, failurereason=failreason, starttime=starttime, loadtime=loadtime, author=author, version=version}
+end
+
+function getRealTimes(sek)
+	if sek == "never" then return "Never" end
+	local time = getRealTime(sek)
+	if time.hour < 10 then time.hour = "0"..time.hour end
+	if time.minute < 10 then time.minute = "0"..time.minute end
+	if time.second < 10 then time.second = "0"..time.second end
+	if time.month+1 < 10 then time.month = "0"..(time.month+1) end
+	if time.monthday < 10 then time.monthday = "0"..time.monthday end
+	
+	return (time.year+1900).."-"..time.month.."-"..time.monthday.." "..time.hour..":"..time.minute..":"..time.second
 end
