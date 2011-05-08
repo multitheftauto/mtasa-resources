53b9c02d9bf803ad5fe39f67cf772da4a264a991
Talidan <danthetaliban@gmail.com>
2011-05-08 17:21:07 +0000

    Updated voice resource for use with the NewVoice branch.
    EXPORTED FUNCTIONS:
    
    --MUTING--
    bool setPlayerMuted ( player, bool muted ) --CLIENT/SERVER
    bool isPlayerMuted ( player, bool muted ) --CLIENT/SERVER
    --Where using this clientside mutes only for the local player
    
    --Get an array of muted players for a specific player
    table getPlayerMutedByList ( player ) --SERVER
    
    --CHANNELS--
    --[[Set the player's channel ID.  Players can only hear people in their own channel.
    Not providing the channel argument will restore default behaviour
    Default behaviour is either being in a channel associated to the player's team if he has one
    Or, being in the 'root' channel.  The root channel broadcasts to every player in the server.
    ]]
    bool setPlayerChannel ( player, [ int channelID ] ) --SERVER
    
    int getPlayerChannel ( player ) --SERVER
    table getPlayersInChannel ( int channelID ) --SERVER
    int getNextEmptyChannel () --SERVER

--- required/voice/sDefaultChannelManager.lua
+++ required/voice/sDefaultChannelManager.lua
@@ -1,35 +1,43 @@
-addEventHandler ( "onPlayerJoin", root, 
-	function()
-		setPlayerInternalChannel ( player, root )
-	end
-)
+if isVoiceEnabled() then
+	addEventHandler ( "onPlayerJoin", root, 
+		function()
+			setPlayerInternalChannel ( source, root )
+		end
+	)
 
-function refreshPlayers()
-	for i,player in ipairs(getElementsByType"player") do
-		if not tonumber(getPlayerChannel ( player )) then --If he's not in a scripted channel
-			setPlayerDefaultChannel ( player )
+	addEventHandler ( "onResourceStart", getResourceRootElement(), 
+		function()
+			refreshPlayers()
+		end
+	)
+
+	function refreshPlayers()
+		for i,player in ipairs(getElementsByType"player") do
+			if not tonumber(getPlayerChannel ( player )) then --If he's not in a scripted channel
+				setPlayerDefaultChannel ( player )
+			end
 		end
 	end
-end
 
-setTimer ( refreshPlayers, TEAM_REFRESH, 0 )
+	setTimer ( refreshPlayers, TEAM_REFRESH, 0 )
 
-function setPlayerDefaultChannel ( player )
-	local team = getPlayerTeam(player)
-	if team and settings.autoassign_to_teams then --He has a team, so let's put him in that team's voice channel
-		return setPlayerInternalChannel ( player, team, getPlayerMutedByList(player) )
-	else --If he doesn't have a team, stick him in the root
-		return setPlayerInternalChannel ( player, root, getPlayerMutedByList(player) )
+	function setPlayerDefaultChannel ( player )
+		local team = getPlayerTeam(player)
+		if team and settings.autoassign_to_teams then --He has a team, so let's put him in that team's voice channel
+			return setPlayerInternalChannel ( player, team, getPlayerMutedByList(player) )
+		else --If he doesn't have a team, stick him in the root
+			return setPlayerInternalChannel ( player, root, getPlayerMutedByList(player) )
+		end
 	end
-end
 
 
-function setPlayerInternalChannel ( player, element, muted )
-	if playerChannels[player] == element then
-		return false
+	function setPlayerInternalChannel ( player, element, muted )
+		if playerChannels[player] == element then
+			return false
+		end
+		playerChannels[player] = element
+		channels[element] = player
+		setPlayerVoiceBroadcastTo ( player, element, muted )
+		return true
 	end
-	playerChannels[player] = element
-	channels[element] = player
-	setPlayerVoiceBroadcastTo ( player, element, muted )
-	return true
 end
--- required/voice/sGlobals.lua
+++ required/voice/sGlobals.lua
@@ -1,50 +1,57 @@
-TEAM_REFRESH = 4000 -- Interval in which team channels are refreshed, in MS.
-SETTINGS_REFRESH = 5000 -- Interval in which team channels are refreshed, in MS.
-resourceRoot = getResourceRootElement(getThisResource())
+if isVoiceEnabled() then
+	TEAM_REFRESH = 4000 -- Interval in which team channels are refreshed, in MS.
+	SETTINGS_REFRESH = 5000 -- Interval in which team channels are refreshed, in MS.
+	resourceRoot = getResourceRootElement(getThisResource())
 
-------------------
-playerChannels = {}
-channels = {}
-globalMuted = {}
-mutedBy = {}
-settings = {}
+	------------------
+	playerChannels = {}
+	channels = {}
+	globalMuted = {}
+	mutedBy = {}
+	settings = {}
 
-------------------
-addEvent("voice_mutePlayerForPlayer", true)
-addEvent("voice_unmutePlayerForPlayer", true)
+	------------------
+	addEvent("voice_mutePlayerForPlayer", true)
+	addEvent("voice_unmutePlayerForPlayer", true)
 
-------------------
---Function to convert { moo=true,boo=true } into { moo,boo }
-function tableToArray (tbl) 
-	local newtable = {}
-	for k,v in pairs(tbl) do
-		table.insert ( newtable, k )
+	------------------
+	--Function to convert { moo=true,boo=true } into { moo,boo }
+	function tableToArray (tbl) 
+		local newtable = {}
+		for k,v in pairs(tbl) do
+			table.insert ( newtable, k )
+		end
+		return newtable
 	end
-	return newtable
+
+	function checkValidPlayer ( player )
+		if not isElement(player) or getElementType(player) ~= "player" then
+			outputDebugString ( "setPlayerMuted: Bad 'player' argument", 2 )
+			return false
+		end
+		return true
+	end
+
+	------------------
+	--Monitor our settings so they dynamically update
+	setTimer ( 
+		function()
+			local show_chat_icon = get ( "show_chat_icon" )
+			if show_chat_icon ~= settings.voice_show_chat_icon then
+				settings.show_chat_icon = show_chat_icon
+				setElementData ( resourceRoot, "show_chat_icon", show_chat_icon )
+			end
+			local autoassign_to_teams = get ( "autoassign_to_teams" )
+			if autoassign_to_teams ~= settings.autoassign_to_teams then
+				settings.autoassign_to_teams = autoassign_to_teams
+				refreshPlayers()
+			end
+			
+		end,
+	SETTINGS_REFRESH, 0 )
 end
 
-function checkValidPlayer ( player )
-	if not isElement(player) or getElementType(player) ~= "player" then
-		outputDebugString ( "setPlayerMuted: Bad 'player' argument", 2 )
-		return false
-	end
-	return true
+function outputVoiceNotLoaded ()
+	outputDebugString ( "Voice is not enabled on this server!", 1 )
+	return false
 end
-
-------------------
---Monitor our settings so they dynamically update
-setTimer ( 
-	function()
-		local show_chat_icon = get ( "show_chat_icon" )
-		if show_chat_icon ~= settings.voice_show_chat_icon then
-			settings.show_chat_icon = show_chat_icon
-			setElementData ( resourceRoot, "show_chat_icon", show_chat_icon )
-		end
-		local autoassign_to_teams = get ( "show_chat_icon" )
-		if autoassign_to_teams ~= settings.autoassign_to_teams then
-			settings.autoassign_to_teams = autoassign_to_teams
-			refreshPlayers()
-		end
-		
-	end,
-SETTINGS_REFRESH, 0 )
