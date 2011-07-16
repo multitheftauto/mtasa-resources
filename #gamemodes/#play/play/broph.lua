470b0f8655aa8ccf89a10313191c228c3eaddee7
arrantuna@hotmail.co.uk <arrantuna@hotmail.co.uk>
2011-07-16 13:30:16 +0000

    Play: Give out new skins too.

--- #gamemodes/#play/play/broph.lua
+++ #gamemodes/#play/play/broph.lua
@@ -9,7 +9,7 @@
 
 function spawn(player)
 	if not isElement(player) then return end
-	repeat until spawnPlayer ( player, -711+math.random(1,5), 957+math.random(5,9), 12.4, 90, math.random(9,288), 0, 0)
+	repeat until spawnPlayer ( player, -711+math.random(1,5), 957+math.random(5,9), 12.4, 90, math.random(312), 0, 0)
 	fadeCamera(player, true)
 	setCameraTarget(player, player)
 	showChat(player, true)
