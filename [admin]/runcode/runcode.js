901cc3d47b74f8a5c097588400e7c549aa874b4b
Gamesnert <Gamesnert@gmail.com>
2011-07-23 15:01:14 +0000

    Fixed #6273: Patch: runcode http interface
    This one actually seems to work.

--- [admin]/runcode/runcode.js
+++ [admin]/runcode/runcode.js
@@ -1,38 +1,13 @@
 var resultstext;
 
 function onBodyLoad () {
-	resultstext = document.getElementById("results");
+	resultstext = CodeMirror.fromTextArea(document.getElementById("code"),{tabMode: "indent",matchBrackets: true,theme: "neat"});
 }
 
 function webRun () {
-	httpRun (
-		codefield.getCode(),
+	httpRun(resultstext.getValue(),
 		function () {
-			var tempString = "";
-			if (arguments[0] == true) {
-				var first = true;
-				if (arguments.length > 1) {
-					tempString = "Result: ";
-					for (var i = 1; i < arguments.length; i++) {
-						if (first == true) {
-							first = false;
-						}
-						else {
-							tempString = tempString + ", ";
-						}
-						tempString = tempString + arguments[i];
-					}
-				}
-				else {
-					tempString = "No results."
-				}
-				resultstext.setAttribute("class", "result");
-			}
-			else {
-				tempString = "Error: " + arguments[1];
-				resultstext.setAttribute("class", "error");
-			}
-			resultstext.innerHTML = tempString;
+			alert(arguments[0])
 		}
 	);
 }
