db57e019da932fe48d96c7ed08d7e197d417bc49
Gamesnert <Gamesnert@126a8462-9353-1474-c972-ef1b1113b2d6>
2011-07-23 15:01:14 +0000

    Fixed #6273: Patch: runcode http interface
    This one actually seems to work.

--- trunk/[admin]/runcode/server.lua
+++ trunk/[admin]/runcode/server.lua
@@ -79,5 +79,40 @@ addCommandHandler("crun",
 
 -- http interface run export
 function httpRun(commandstring)
-	return pcall(loadstring(commandstring))
+	local notReturned
+	--First we test with return
+	local commandFunction,errorMsg = loadstring("return "..commandstring)
+	if errorMsg then
+		--It failed.  Lets try without "return"
+		notReturned = true
+		commandFunction, errorMsg = loadstring(commandstring)
+	end
+	if errorMsg then
+		--It still failed.  Print the error message and stop the function
+		return "Error: "..errorMsg
+	end
+	--Finally, lets execute our function
+	results = { pcall(commandFunction) }
+	if not results[1] then
+		--It failed.
+		return "Error: "..results[2]
+	end
+	if not notReturned then
+		local resultsString = ""
+		local first = true
+		for i = 2, #results do
+			if first then
+				first = false
+			else
+				resultsString = resultsString..", "
+			end
+			local resultType = type(results[i])
+			if isElement(results[i]) then
+				resultType = "element:"..getElementType(results[i])
+			end
+			resultsString = resultsString..tostring(results[i]).." ["..resultType.."]"
+		end
+		return "Command results: "..resultsString
+	end
+	return "Command executed!"
 end
