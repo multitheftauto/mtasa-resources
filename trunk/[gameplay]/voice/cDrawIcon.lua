3c46f248f4c54dca7e37f0b0219c703199f8f511
arrantuna@hotmail.co.uk <arrantuna@hotmail.co.uk@126a8462-9353-1474-c972-ef1b1113b2d6>
2011-08-24 20:32:23 +0000

    Voice: Adding changes as requested by Talidan.

--- trunk/[gameplay]/voice/cDrawIcon.lua
+++ trunk/[gameplay]/voice/cDrawIcon.lua
@@ -1,4 +1,4 @@
-﻿local g_screenX,g_screenY = guiGetScreenSize()
+local g_screenX,g_screenY = guiGetScreenSize()
 local BONE_ID = 8
 local WORLD_OFFSET = 0.4
 local ICON_PATH = "images/voice.png"
@@ -8,11 +8,19 @@ local ICON_WIDTH = 0.35*g_screenX
 local iconHalfWidth = ICON_WIDTH/2
 -- local iconHalfHeight = ICON_HEIGHT/2
 
+local ICON_DIMENSIONS = 16
+local ICON_LINE = 20
+local ICON_TEXT_SHADOW = tocolor ( 0, 0, 0, 255 )
+
 --Draw the voice image
 addEventHandler ( "onClientRender", root,
 	function()
+		local index = 0
 		if not bShowChatIcons then return end
 		for player in pairs(voicePlayers) do
+			local color = tocolor(getPlayerNametagColor ( player ))
+			dxDrawVoiceLabel ( player, index, color )
+			index = index + 1
 			while true do
 				--is he streamed in?
 				if not isElementStreamedIn(player) then
@@ -34,7 +42,6 @@ addEventHandler ( "onClientRender", root,
 				if not isLineOfSightClear ( camX, camY, camZ, headX, headY, headZ, true, false, false, true, false, true, false, player ) then
 					break
 				end
-				local color = tocolor(getPlayerNametagColor ( player ))
 				dxDrawVoice ( absX, absY, color, getDistanceBetweenPoints3D(camX, camY, camZ, headX, headY, headZ) )
 				break
 			end
@@ -45,5 +52,22 @@ addEventHandler ( "onClientRender", root,
 function dxDrawVoice ( posX, posY, color, distance )
 	distance = 1/distance
 	dxDrawImage ( posX - iconHalfWidth*distance, posY - iconHalfWidth*distance, ICON_WIDTH*distance, ICON_WIDTH*distance, ICON_PATH, 0, 0, 0, color, false )
+end
+
+function dxDrawVoiceLabel ( player, index, color )
+	local sx, sy = guiGetScreenSize ()
+	local scale = sy / 800
+	local spacing = ( ICON_LINE * scale )
+	local px, py = sx - 200, sy * 0.7 + spacing * index
+	local icon = ICON_DIMENSIONS * scale
+
+	dxDrawImage ( px, py, icon, icon, ICON_PATH, 0, 0, 0, color, false )
+
+	px = px + spacing
+
+	-- shadows
+	dxDrawText ( getPlayerName ( player ), px + 1, py + 1, px, py, ICON_TEXT_SHADOW, scale )
+	-- text
+	dxDrawText ( getPlayerName ( player ), px, py, px, py, color, scale )
 end
 
