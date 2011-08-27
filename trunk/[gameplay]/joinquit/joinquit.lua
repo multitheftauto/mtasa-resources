e3bacfbc980c8cc013c57b66de6812397fdbe7c1
arrantuna@hotmail.co.uk <arrantuna@hotmail.co.uk@126a8462-9353-1474-c972-ef1b1113b2d6>
2011-08-27 15:09:40 +0000

    Removing redundant variable. Patch from Callum.

--- trunk/[gameplay]/joinquit/joinquit.lua
+++ trunk/[gameplay]/joinquit/joinquit.lua
@@ -1,18 +1,16 @@
-﻿g_Root = getRootElement()
-
-addEventHandler('onClientPlayerJoin', g_Root,
+﻿addEventHandler('onClientPlayerJoin', root,
 	function()
 		outputChatBox('* ' .. getPlayerName(source) .. ' has joined the game', 255, 100, 100)
 	end
 )
 
-addEventHandler('onClientPlayerChangeNick', g_Root,
+addEventHandler('onClientPlayerChangeNick', root,
 	function(oldNick, newNick)
 		outputChatBox('* ' .. oldNick .. ' is now known as ' .. newNick, 255, 100, 100)
 	end
 )
 
-addEventHandler('onClientPlayerQuit', g_Root,
+addEventHandler('onClientPlayerQuit', root,
 	function(reason)
 		outputChatBox('* ' .. getPlayerName(source) .. ' has left the game [' .. reason .. ']', 255, 100, 100)
 	end
