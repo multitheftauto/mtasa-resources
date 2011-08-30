792ccdcf4cccf3ef9043986604b4ce201b2cabac
Cazomino05 <Cazomino05@126a8462-9353-1474-c972-ef1b1113b2d6>
2011-08-30 18:36:55 +0000

    Added GPS resource. Thanks arc_

---
+++ trunk/[gameplay]/gps/client.lua
@@ -0,0 +1,60 @@
+local floor = math.floor
+
+addCommandHandler('path', 
+	function(command, node1, node2)
+		local path = server.calculatePathByNodeIDs(tonumber(node1), tonumber(node2))
+		if not path then
+			outputConsole('No path found')
+			return
+		end
+		server.spawnPlayer(getLocalPlayer(), path[1].x, path[1].y, path[1].z)
+		fadeCamera(true)
+		setCameraTarget(getLocalPlayer())
+		
+		table.each(getElementsByType('marker'), destroyElement)
+		for i,node in ipairs(path) do
+			createMarker(node.x, node.y, node.z, 'corona', 5, 50, 0, 255, 200)
+		end
+	end
+)
+
+local function getAreaID(x, y)
+	return math.floor((y + 3000)/750)*8 + math.floor((x + 3000)/750)
+end
+
+local function getNodeByID(db, nodeID)
+	local areaID = floor(nodeID / 65536)
+	return db[areaID][nodeID]
+end
+
+--[[
+addEventHandler('onClientRender', getRootElement(),
+	function()
+		local db = vehicleNodes
+		
+		local camX, camY, camZ = getCameraMatrix()
+		local x, y, z = getElementPosition(getLocalPlayer())
+		local areaID = getAreaID(x, y)
+		local drawn = {}
+		for id,node in pairs(db[areaID]) do
+			if getDistanceBetweenPoints3D(x, y, z, node.x, node.y, z) < 300 then
+				--[/[
+				local screenX, screenY = getScreenFromWorldPosition(node.x, node.y, node.z)
+				if screenX then
+					dxDrawText(tostring(id), screenX - 10, screenY - 5)
+				end
+				--]/]
+				--[/[
+				for neighbourid,distance in pairs(node.neighbours) do
+					if not drawn[neighbourid .. '-' .. id] then
+						local neighbour = getNodeByID(db, neighbourid)
+						dxDrawLine3D(node.x, node.y, node.z + 1, neighbour.x, neighbour.y, neighbour.z + 1, tocolor(0, 0, 200, 255), 3)
+						drawn[id .. '-' .. neighbourid] = true
+					end
+				end
+				--]/]
+			end
+		end
+	end
+)
+--]]
