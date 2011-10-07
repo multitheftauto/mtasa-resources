29f45d955361138bc11f9aef5cf2f432c76b15bb
Cazomino05 <Cazomino05@126a8462-9353-1474-c972-ef1b1113b2d6>
2011-10-07 19:33:26 +0000

    Added fastrope script Events: onFastRopeDestroy - Called when a fast rope is destroyed with an ID and heli if created by createFastRopeHeli onFastRopeCreate - Called when a fast rope is created with an ID and heli if created by createFastRopeHeli onPlayerFastRope - Called when a fast rope is destroyed with an ID and heli if created by createFastRopeHeli. The source of this is the player. Exports: int createFastRopeOnHeli(player thePlayer, vehicle theHeli [ , string side, int timeInMS, float offset ] ) int createFastRope(player thePlayer, float x, float y, float z [ , int timeInMS, float offset ] ) everything in [] is optional and both functions return a rope ID or false if an error occurred
    addPlayerToFastRope( player thePlayer, int ropeID )
    add a player to the rope ID (only works if the rope hasn't expired yet and he's not the driver of the helicopter if one was specified when creating the rope)
    
    hasRopeExpired ( ropeID )
    check if a ropeID has expired yet.

---
+++ trunk/[gameplay]/fastrope/fastrope.lua
@@ -0,0 +1,93 @@
+-- defines
+local localplayer = getLocalPlayer()
+local onrope = false
+local colshape
+local timer
+-- disable falling damage when they land
+local function disableFallDamage(attacker, weapon, bodypart, loss)
+	if ( source == localPlayer and weapon == 54 and onrope == true ) then
+		-- Disable falling damage while on the rope
+		cancelEvent()
+	end
+end
+-- set on rope to false
+local function setOffRope ( )
+	if ( source == localPlayer and onrope == true ) then
+		triggerServerEvent("frope_animoff", localPlayer)
+		setTimer(cleanUp, 1000, 1)
+	end
+end
+-- handle ground hit
+local function groundhit(ped)
+	if ( ped == localPlayer and onrope == true) then
+		triggerServerEvent("frope_animoff", localPlayer)
+		setTimer(cleanUp, 1000, 1)
+	end
+end
+-- reduced code duplication here
+function cleanUp ( )
+	outputChatBox("cleanup")
+	-- no longer on the rope so unset our variables and remove our handlers
+	onrope = false
+	removeEventHandler("onClientColShapeHit", colshape, groundhit)
+	if ( timer ) then
+		killTimer ( timer )
+	end
+end
+
+-- remote calls
+
+function create_FastRope ( x, y, z, time )
+	-- simple wrapper
+	createSWATRope(x,y,z,time)
+end
+addEvent( "frope_createFastRope", true )
+addEventHandler("frope_createFastRope", getRootElement(), create_FastRope)
+local function createLandingSphere( x, y, z, forceWait )
+	-- get the ground position
+	local groundPos = getGroundPosition ( x, y, z )
+	-- get the water position
+	local waterPos = getWaterLevel ( x, y, z, true )
+	-- ground not loaded... wait
+	if ( forceWait ) then
+		-- wait 500ms for it to load then try again ( should only take 1 attempt but even so it'l be handled if not)
+		setTimer(createLandingSphere, 500, 1, x, y, z)
+		outputChatBox("Waiting.")
+		return
+	end
+	-- if the ground position is < 0 it's likely we are about to land in water though make sure the water level here is higher as well (area 51 e.g.)
+	if ( groundPos < 0 and waterPos > groundPos ) then
+		-- Create our sphere here and setup a hit event so we can cleanup
+		colshape = createColSphere(x,y,waterPos, 2)
+		outputChatBox("Water")
+		outputChatBox(waterPos)
+	else
+		-- Create our sphere here and setup a hit event so we can cleanup
+		colshape = createColSphere(x,y,groundPos, 2)
+		outputChatBox("Ground")
+		outputChatBox(groundPos)
+	end
+	addEventHandler("onClientColShapeHit", colshape, groundhit)
+end
+function smartanimbreak ( x, y, z )
+	-- don't create 2 colspheres or we end up with a leak
+	if ( onrope == true ) then
+		outputChatBox("Already on the rope!")
+		return
+	end
+	-- check position against rope position.
+	local x2, y2, z2 = getElementPosition(localPlayer)
+	-- force a 500ms wait so we load collisions
+	local forceWait = true
+	-- create our colsphere for the landing.
+	createLandingSphere ( x, y, z, forceWait )
+	-- We are on the ropes now!
+	onrope = true
+	-- Disable damage and make sure we cleanup if we die on the rope
+	-- Allow 4 seconds then force them to disable in case we don't hit the landing sphere
+	--timer = setTimer(groundhit, 4000, 1, localPlayer)
+end
+addEvent("frope_smartAnimBreak", true)
+addEventHandler("onClientPlayerDamage", getRootElement(), disableFallDamage)
+addEventHandler("onClientPlayerWasted", getRootElement(), setOffRope)
+addEventHandler("frope_smartAnimBreak", getRootElement(), smartanimbreak)
