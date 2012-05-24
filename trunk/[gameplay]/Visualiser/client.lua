f2603f08e8fc0c60d6fcfa62453d24e54021aa0e
Cazomino05 <Cazomino05@126a8462-9353-1474-c972-ef1b1113b2d6>
2012-05-24 16:49:33 +0000

    Added example visualiser using Gamesnerts cinema shader resource.

---
+++ trunk/[gameplay]/Visualiser/client.lua
@@ -0,0 +1,292 @@
+local startx = 112
+local starty = 1026
+local startz = 14
+local screenStartX = guiGetScreenSize()
+local SPECWIDTH = screenStartX
+local screenStartX = screenStartX * 0
+local SPECHEIGHT = (SPECWIDTH / 16) * 7  -- height (changing requires palette adjustments too)
+local screenStartY = SPECHEIGHT / 2
+local BANDS = 40
+
+
+local peakData, ticks, maxbpm, startTime, release, peak, peaks
+function reset ( )
+	peaks = {}
+	for k=0, BANDS - 1 do
+		peaks[k] = {}
+	end
+	peakData = {}
+	ticks = getTickCount()
+	maxbpm = 1
+	bpmcount = 1
+	startTime = 0
+	release = { }
+	peak = 0
+end
+
+addEvent("playmus", true)
+addEventHandler("playmus", root, function ( url )
+	if ( stream ) then
+		destroyElement(stream)
+	end
+	stream = playSound3D(url, startx, starty, startz, true)
+	setSoundMinDistance(stream, 1)
+	setSoundMaxDistance(stream, 10000)
+	setTimer(setSoundPanningEnabled, 1000, 1, stream, false)
+	startTicks = getTickCount()
+	ticks = getTickCount()
+	reset ( )
+	
+	-- Create shader
+	shader_cinema, tec = dxCreateShader ( "texreptransform.fx" )
+	if not shader_cinema then return end
+	-- If the image is too bright, you can darken it
+	-- If the image is too bright, you can darken it
+	dxSetShaderValue ( shader_cinema, "gBrighten", -0.25 )
+	-- Set the angle, grayscaled, rgb
+	local radian=math.rad(0)
+	dxSetShaderValue ( shader_cinema, "gRotAngle", radian )
+	dxSetShaderValue ( shader_cinema, "gGrayScale", 0 )
+	dxSetShaderValue ( shader_cinema, "gRedColor", 0 )
+	dxSetShaderValue ( shader_cinema, "gGrnColor", 0 )
+	dxSetShaderValue ( shader_cinema, "gBluColor", 0 )
+	-- Set image alpha (1 max)
+	dxSetShaderValue ( shader_cinema, "gAlpha", 1 )
+	-- Set scrolling (san set negative and positive values)
+	dxSetShaderValue ( shader_cinema, "gScrRig",  0)
+       	dxSetShaderValue ( shader_cinema, "gScrDow", 0)
+	-- Scale and offset (don't need to change that)
+        dxSetShaderValue ( shader_cinema, "gHScale", 1 )
+	dxSetShaderValue ( shader_cinema, "gVScale", 1 )
+	dxSetShaderValue ( shader_cinema, "gHOffset", 0 )
+	dxSetShaderValue ( shader_cinema, "gVOffset", 0 ) 
+	if not shader_cinema then
+		outputChatBox( "Could not create shader. Please use debugscript 3" )
+		return
+	else
+		tar = dxCreateRenderTarget ( SPECWIDTH, SPECHEIGHT )
+		SPECWIDTH = SPECWIDTH - 6
+		engineApplyShaderToWorldTexture ( shader_cinema, "drvin_screen" )
+	end
+	addEventHandler("onClientRender", root, function ( )
+		local fftData = getSoundFFTData(stream, 2048)
+		local w, h = guiGetScreenSize()
+		if ( fftData == false ) then
+			dxDrawText("Stream not loaded yet.", w-300, h-150)
+			return
+		end
+		if ( getSoundMetaTags(stream).stream_name ~= nil ) then
+			local len = string.len(getSoundMetaTags(stream).stream_name)
+			dxDrawText("Now Playing: " .. getSoundMetaTags(stream).stream_name, w-(270+(len*2.8)), h-150)
+		else
+			dxDrawText("Now Playing: -", w-(270), h-150)
+		end
+		calc ( fftData, stream )
+	end)
+		
+end)
+
+function timetostring ( input, input2 )
+	local minutes = input / 60
+	local seconds = input % 60
+	local minutes2 = input2 / 60
+	local seconds2 = input2 % 60
+	return string.format("%2.2i:%2.2i", minutes2, seconds2)
+end
+function avg ( num )
+	return maxbpm / bpmcount
+end
+function avg2 ( num1, num2, num )
+	return (num1+num2)/num
+end
+function round(num, idp)
+  return tonumber(string.format("%." .. (idp or 0) .. "f", num))
+end
+function getAverageBPM ( )
+	return maxbpm / bpmcount
+end
+function min ( num1, num2 ) 
+	return num1 <= num2 and num1 or num2
+end
+function max ( num1, num2 ) 
+	return num1 >= num2 and num1 or num2
+end
+t1 = getTickCount()
+myRenderTarget = dxCreateRenderTarget( SPECWIDTH, SPECHEIGHT )
+function calc ( fft, stream )
+	dxSetRenderTarget( tar, true )
+	-- background rect.
+	--dxDrawRectangle( screenStartX, screenStartY+150, SPECWIDTH, 0-(SPECHEIGHT+75+2),  tocolor(0, 0, 0, 255 ))
+
+	math.randomseed ( getTickCount ( ) )
+	local bpm = getAverageBPM()
+	local realbpm = getSoundBPM ( stream )
+	--if ( realbpm ~= false and realbpm > 20 and (getTickCount ( ) - startTime <= 20000 or startTime == 0) ) then
+	--if ( realbpm ~= false and realbpm > bpm-20 ) then
+	if ( maxbpm == 1 and realbpm ~= false ) then
+		maxbpm = realbpm
+		bpmcount = 1
+		if ( startTime == 0 ) then
+			startTime = getTickCount ( )
+		end
+	end
+	if ( realbpm == false or realbpm == nil or realbpm == 0  ) then
+		realbpm = 1
+	end
+	bpm = getAverageBPM ( )
+	if ( bpm <= 1 and getSoundBPM ( stream ) == false and getSoundPosition ( stream ) <= 20000 ) then
+		--dxDrawText(string.format("Loading... %i%%",  (startTime)), screenStartX+20, screenStartY-40, screenStartX+40, screenStartY-20, tocolor(255, 255, 255, 255 ), 1, "arial")
+		
+		--dxDrawText(string.format("Loading... %i%%",  0.005 * (getSoundPosition ( stream ) * 1000)), screenStartX+20, screenStartY-20, screenStartX+40, screenStartY-20, tocolor(255, 255, 255, 255 ), 1, "arial")
+	end
+	local colours = 32768 / 4
+	local calced = {}
+	local y = 0
+	local bC=0
+	local specbuf = 0
+	local w, h = guiGetScreenSize()
+	local l,r = getSoundLevelData(stream)
+	if ( bpm == false ) then
+		bpm = 0
+	end
+	local avgv = getAverageBPM ( )
+	local r,g,b = 0,0,0
+	local var = (avgv) + 37
+
+	if ( var <= 56 ) then
+		r,g,b = 99, 184, 255
+	end
+	if ( var >= 57 and var < 83 ) then
+		r,g,b = 238, 174, 238
+	end
+	if ( var >= 83 and var < 146 ) then
+		r,g,b = 238, 174, 238
+	end
+
+	if ( var >= 146 and var < 166 ) then
+		r,g,b = 238, 174, 238
+	end
+	if ( var > 166 and var <= 200 ) then
+		r,g,b = 238, 201, 0
+	end
+
+	if ( var >= 200 ) then
+		r,g,b = var, 0, 0
+	end
+	--if ( var >= 200 ) then
+	--	--green
+	--	r,g,b = var, 0, 0
+	--else
+	--	--yellow/orange/red
+	--	r,g,b = var - 249, 255 - var, 0
+	--end
+
+	local tags = getSoundMetaTags(stream)
+	local bSpawnParticles = true
+	if ( bpm <= 1 and getSoundBPM ( stream ) == false and getSoundPosition ( stream ) <= 20000 ) then
+		r,g,b = 255, 255, 255
+		dxDrawImage ( 0, 00, SPECWIDTH, SPECHEIGHT+100, "bg.png", 0, 0,0, tocolor(r, g, b, 255) )
+		dxDrawText(string.format("Learning...", avgv), screenStartX+10, screenStartY-30, screenStartX+10, screenStartY-30, tocolor(255, 255, 255, 255 ), 1.5, "arial")
+		bSpawnParticles = false
+	else
+		-- always make this bigger because when you tint it the image will look smaller.
+		local var = 600
+		local var2 = 400
+		dxDrawImage ( -var2, -var, SPECWIDTH+(var2*2), SPECHEIGHT+(var*2)+100, "bg.png", 0, 0,0, tocolor(r, g, b, 255) )
+	end
+	local movespeed = (1 * (bpm / 180)) + 1
+	local dir = bpm <= 100 and "down" or "up"
+	local prevcalced = calced
+	for x=1, BANDS - 1 do
+		local posx = x - 1
+		local peak=0
+		local bB=math.pow(2,x*10.0/(BANDS-1))
+		if (bB>1023) then 
+			bB=1023
+		end
+		if (bB<=bC) then 
+			bB=bC+1 
+		end -- make sure it uses at least 1 FFT bin
+		while bC < bB do	
+			if (peak < fft[ 1+bC ]) then 
+				peak=fft[ 1+bC ] 
+			end
+			bC = bC + 1
+		end	
+		y=math.sqrt(peak)*3*(SPECHEIGHT-4); -- scale it (sqrt to make low values more visible)
+		if (y > 200+SPECHEIGHT) then
+			y=SPECHEIGHT+200
+		end -- cap it
+		calced[x] = y
+		if ( peakData[x] == nil ) then
+			peakData[x] = y
+			bSpawn = false
+		else 
+			if ( peakData[x] < y ) then
+				peakData[x] = y
+				bSpawn = true
+
+			else
+				bSpawn = false
+			end
+		end
+		if ( use_dx == true ) then
+			y = y - 1
+			if ( y >= -1 ) then
+				--dxDrawRectangle((x*(SPECWIDTH/BANDS))+10+screenStartX, screenStartY, 10, (y+1), tocolor(15, 185, 6, 255 ))
+				dxDrawRectangle((posx*(SPECWIDTH/BANDS))+10+screenStartX, screenStartY, 10, max((y+1)/4, 1), tocolor(r, g, b, 255 ))
+				--dxDrawImage ( (x*(SPECWIDTH/BANDS))+10+screenStartX, screenStartY, 10, (y+1)/4, "bar.png", 0, 0,0, tocolor(r, g, b, 255) )
+				--dxDrawRectangle((x*(SPECWIDTH/BANDS))+12+screenStartX, screenStartY, 10, (y+1), tocolor(119, 228, 114, 255 ))
+			end
+			if ( bSpawnParticles == true ) then
+				for key = 0, 20 do
+					if ( peaks[x][key] == nil ) then
+						if ( #peaks[x] <= 20 and prevcalced[x] <= calced[x] and ( release[x] == true or release[x] == nil ) and y > 1 ) then
+							local rnd = math.random(0, 0)
+							peaks[x][key] = {}
+							if ( dir == "up" ) then
+								peaks[x][key]["pos"] = screenStartY
+							else
+								peaks[x][key]["pos"] = screenStartY+((y+1)/4)
+							end
+							peaks[x][key]["posx"] = (posx*(SPECWIDTH/BANDS))+12+screenStartX+(2-key)
+							peaks[x][key]["alpha"] = 128
+							peaks[x][key]["dirx"] = 0
+							release[x] = false
+							setTimer(function ( ) release[x] = true end, 100, 1)
+						end
+					else
+						if ( bpm > 0 ) then
+							local maxScreenPos = 90
+							local AlphaMulti = 255 / maxScreenPos
+							value = peaks[x][key]
+							if ( value ~= nil ) then
+								local sX = value.posx
+								dxDrawRectangle( value.posx, value.pos, 2, 2, tocolor(r, g, b, value.alpha))
+								--dxDrawImage ( value.posx, value.pos, 2, 2, "bar.png", 0, 0,0, tocolor(r, g, b, value.alpha) )
+								value.pos = dir == "down" and value.pos + movespeed or value.pos - movespeed
+								value.posx = value.posx + (movespeed <= 2 and math.random(-movespeed,movespeed) or math.random(-1, 1))
+								value.alpha = value.alpha - (AlphaMulti) - math.random(1, 4)
+								
+								if ( value.alpha <= 0 ) then
+									peaks[x][key] = nil
+									peakData[x] = 0
+								end
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+	if ( bSpawnParticles == true ) then
+		dxDrawText(string.format((tags.artist ~= nil and tags.artist .. ", " or "") .."BPM: %i", avgv), screenStartX+10, screenStartY-30, screenStartX+20, screenStartY-30, tocolor(255, 255, 255, 255 ), 1.5, "arial")
+	end
+	dxDrawText(tags.title or tags.stream_name or "Unknown", screenStartX+10, screenStartY-60, screenStartX+10, screenStartY-60, tocolor(255, 255, 255, 255 ), 2, "arial")
+	dxDrawText(timetostring(getSoundLength(stream), getSoundPosition(stream)), SPECWIDTH-50, screenStartY-40, SPECWIDTH-80, screenStartY-40, tocolor(255, 255, 255, 255 ), 1.5, "arial")
+	-- debug
+	--dxDrawText(tostring(bpm), SPECWIDTH-40, screenStartY-10, SPECWIDTH-80, screenStartY-10, tocolor(255, 255, 255, 255 ), 1.5, "arial")
+	--release = true
+	dxSetRenderTarget()
+	dxSetShaderValue ( shader_cinema, "gTexture", tar )
+end
---
+++ trunk/[gameplay]/Visualiser/server.lua
@@ -0,0 +1,3 @@
+addCommandHandler("startmusic", function ( plr, commandName, url ) 
+	setTimer(triggerClientEvent, 1000, 1, "playmus", root, url)
+end)
