1312b9f52535d75c1461183f31340e4569d0c281
arrantuna@hotmail.co.uk <arrantuna@hotmail.co.uk@126a8462-9353-1474-c972-ef1b1113b2d6>
2012-08-26 12:38:22 +0000

    [editor] Fixed a debug warning when adding an EDF and the resource wasn't loaded yet. Editor already tries to adds them again once it's loaded.

--- trunk/[editor]/editor_gui/client/me_gui.lua
+++ trunk/[editor]/editor_gui/client/me_gui.lua
@@ -169,7 +169,9 @@ function refreshElementIcons()
 				theIcon = guiCreateStaticImage ( posX, screenY - guiConfig.iconSize, guiConfig.iconSize, guiConfig.iconSize, iconPath.."elementmenu/generic.png", false) --, getResourceFromName(key)) 
 			else
 				--otherwise create edf's icon
-				theIcon = guiCreateStaticImage ( posX, screenY - guiConfig.iconSize, guiConfig.iconSize, guiConfig.iconSize, ":"..resource.."/"..iconDir, false)
+				if (getResourceFromName(resource)) then -- To prevent debug when trying to load from a not yet started resource
+					theIcon = guiCreateStaticImage ( posX, screenY - guiConfig.iconSize, guiConfig.iconSize, guiConfig.iconSize, ":"..resource.."/"..iconDir, false)
+				end
 			end
 			if not theIcon then
 				--if edf's icon was not made successfully, then create a generic one.
