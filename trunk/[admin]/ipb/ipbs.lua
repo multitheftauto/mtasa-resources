1c6cf72af95535eae8d53286a89d50540dc2f0fc
arrantuna@hotmail.co.uk <arrantuna@hotmail.co.uk@126a8462-9353-1474-c972-ef1b1113b2d6>
2012-10-30 16:29:13 +0000

    [ipb] Enabled options and filter feature.

--- trunk/[admin]/ipb/ipbs.lua
+++ trunk/[admin]/ipb/ipbs.lua
@@ -6,7 +6,7 @@ function startListening(player)
 		outputChatBox("Error: access denied", player, 255, 0, 0)
 		return
 	end
-	listeners[player] = "Server info"
+	listeners[player] = {"Server info", "", ""}
 	outputChatBox("IPB: Started listening", player, 0, 255, 0)
 	
 	local stat1, stat2 = getPerformanceStats("Server info")
@@ -19,17 +19,30 @@ function changeCategory(newCategory)
 		outputChatBox("Error: access denied", client, 255, 0, 0)
 		return
 	end
-	listeners[client] = newCategory
+	listeners[client][1] = newCategory
 	local stat1, stat2 = getPerformanceStats(newCategory)
 	triggerClientEvent(client, "ipb.recStats", client, 2, stat1, stat2)
 end
 addEvent("ipb.changeCat", true)
 addEventHandler("ipb.changeCat", root, changeCategory)
 
+function changeOptionsAndFilter(options, filter)
+	if (not hasObjectPermissionTo(client, "general.http", false)) then
+		outputChatBox("Error: access denied", client, 255, 0, 0)
+		return
+	end
+	listeners[client][2] = options
+	listeners[client][3] = filter
+	local stat1, stat2 = getPerformanceStats(listeners[client][1], listeners[client][2], listeners[client][3])
+	triggerClientEvent(client, "ipb.recStats", client, 2, stat1, stat2)
+end
+addEvent("ipb.changeOptionsAndFilter", true)
+addEventHandler("ipb.changeOptionsAndFilter", root, changeOptionsAndFilter)
+
 function updateListeners()
 	for player, data in pairs(listeners) do
 		if (isElement(player)) then
-			local stat1, stat2 = getPerformanceStats(data)
+			local stat1, stat2 = getPerformanceStats(data[1], data[2], data[3])
 			triggerClientEvent(player, "ipb.recStats", player, 3, stat1, stat2)
 		else
 			listeners[player] = nil
