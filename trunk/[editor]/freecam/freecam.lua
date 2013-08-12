97cd1b7b0ec7b17501a4953d4c7e6989e51ce978
arrantuna@hotmail.co.uk <arrantuna@hotmail.co.uk@126a8462-9353-1474-c972-ef1b1113b2d6>
2013-08-12 20:23:11 +0000

    [freecam] Fixed server side exports not working.

--- trunk/[editor]/freecam/freecam.lua
+++ trunk/[editor]/freecam/freecam.lua
@@ -287,10 +287,10 @@ function setFreecamOption(theOption, value)
 	end
 end
 
-addEvent("doSetFreecamEnabled")
+addEvent("doSetFreecamEnabled", true)
 addEventHandler("doSetFreecamEnabled", rootElement, setFreecamEnabled)
 
-addEvent("doSetFreecamDisabled")
+addEvent("doSetFreecamDisabled", true)
 addEventHandler("doSetFreecamDisabled", rootElement, setFreecamDisabled)
 
 addEvent("doSetFreecamOption")
