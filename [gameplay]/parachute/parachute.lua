5548f16a38933db4385c7245d02240b0ef6eff7f
arrantuna@hotmail.co.uk <arrantuna@hotmail.co.uk>
2013-11-25 22:03:42 +0000

    [parachute] Slightly reduced server CPU usage by making use of triggerClientEvent's ability to take a table of players which is about 30 times more efficient than calling triggerClientEvent for every player.

--- [gameplay]/parachute/parachute.lua
+++ [gameplay]/parachute/parachute.lua
@@ -1,30 +1,33 @@
-﻿root = getRootElement ()
-
+﻿
 local function onResourceStart ( resource )
-  local players = getElementsByType ( "player" )
-  for k, v in pairs ( players ) do
-    setElementData ( v, "parachuting", false )
-  end
+	local players = getElementsByType ( "player" )
+	for k, v in pairs ( players ) do
+		setElementData ( v, "parachuting", false )
+	end
 end
-addEventHandler ( "onResourceStart", getResourceRootElement ( getThisResource () ), onResourceStart )
+addEventHandler ( "onResourceStart", resourceRoot, onResourceStart )
 
 function requestAddParachute ()
+	local plrs = {}
 	for key,player in ipairs(getElementsByType"player") do
 		if player ~= source then
-			triggerClientEvent ( player, "doAddParachuteToPlayer", source )
+			table.insert(plrs, player)
 		end
 	end
+	triggerClientEvent(plrs, "doAddParachuteToPlayer", source)
 end
 addEvent ( "requestAddParachute", true )
 addEventHandler ( "requestAddParachute", root, requestAddParachute )
 
 function requestRemoveParachute ()
 	takeWeapon ( source, 46 )
+	local plrs = {}
 	for key,player in ipairs(getElementsByType"player") do
 		if player ~= source then
-			triggerClientEvent ( player, "doRemoveParachuteFromPlayer", source )
+			table.insert(plrs, player)
 		end
 	end
+	triggerClientEvent(plrs, "doAddParachuteToPlayer", source)
 end
 addEvent ( "requestRemoveParachute", true )
 addEventHandler ( "requestRemoveParachute", root, requestRemoveParachute )
