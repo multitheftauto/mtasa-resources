5a4ee132fb5247bcd071b0d5bb41bef6b44cda1d
Qais Patankar <qaisjp@gmail.com>
2016-07-31 01:59:52 +0100

    [runcode] Improve support for return statements

--- [admin]/runcode/client.lua
+++ [admin]/runcode/client.lua
@@ -7,7 +7,6 @@ local function runString (commandstring)
 	local commandFunction,errorMsg = loadstring("return "..commandstring)
 	if errorMsg then
 		--It failed.  Lets try without "return"
-		notReturned = true
 		commandFunction, errorMsg = loadstring(commandstring)
 	end
 	if errorMsg then
@@ -22,26 +21,29 @@ local function runString (commandstring)
 		outputChatBoxR("Error: "..results[2])
 		return
 	end
-	if not notReturned then
-		local resultsString = ""
-		local first = true
-		for i = 2, #results do
-			if first then
-				first = false
-			else
-				resultsString = resultsString..", "
-			end
-			local resultType = type(results[i])
-			if isElement(results[i]) then
-				resultType = "element:"..getElementType(results[i])
-			end
-			resultsString = resultsString..tostring(results[i]).." ["..resultType.."]"
+	
+	local resultsString = ""
+	local first = true
+	for i = 2, #results do
+		if first then
+			first = false
+		else
+			resultsString = resultsString..", "
 		end
-		outputChatBoxR("Command results: "..resultsString)
-	elseif not errorMsg then
-		outputChatBoxR("Command executed!")
+		local resultType = type(results[i])
+		if isElement(results[i]) then
+			resultType = "element:"..getElementType(results[i])
+		end
+		resultsString = resultsString..tostring(results[i]).." ["..resultType.."]"
 	end
+
+	if #results > 1 then
+		outputChatBoxR("Command results: " ..resultsString)
+		return
+	end
+	
+	outputChatBoxR("Command executed!")
 end
 
 addEvent("doCrun", true)
-addEventHandler("doCrun", getRootElement(), runString)
+addEventHandler("doCrun", root, runString)
--- [admin]/runcode/server.lua
+++ [admin]/runcode/server.lua
@@ -2,19 +2,14 @@
 
 function runString (commandstring, outputTo, source)
 	me = source
-	local sourceName
-	if source then
-		sourceName = getPlayerName(source)
-	else
-		sourceName = "Console"
-	end
+	local sourceName = source and getPlayerName(source) or "Console"
+	
 	outputChatBoxR(sourceName.." executed command: "..commandstring, outputTo, true)
 	local notReturned
 	--First we test with return
 	local commandFunction,errorMsg = loadstring("return "..commandstring)
 	if errorMsg then
 		--It failed.  Lets try without "return"
-		notReturned = true
 		commandFunction, errorMsg = loadstring(commandstring)
 	end
 	if errorMsg then
@@ -29,25 +24,28 @@ function runString (commandstring, outputTo, source)
 		outputChatBoxR("Error: "..results[2], outputTo)
 		return
 	end
-	if not notReturned then
-		local resultsString = ""
-		local first = true
-		for i = 2, #results do
-			if first then
-				first = false
-			else
-				resultsString = resultsString..", "
-			end
-			local resultType = type(results[i])
-			if isElement(results[i]) then
-				resultType = "element:"..getElementType(results[i])
-			end
-			resultsString = resultsString..tostring(results[i]).." ["..resultType.."]"
+	
+	local resultsString = ""
+	local first = true
+	for i = 2, #results do
+		if first then
+			first = false
+		else
+			resultsString = resultsString..", "
 		end
-		outputChatBoxR("Command results: "..resultsString, outputTo)
-	elseif not errorMsg then
-		outputChatBoxR("Command executed!", outputTo)
+		local resultType = type(results[i])
+		if isElement(results[i]) then
+			resultType = "element:"..getElementType(results[i])
+		end
+		resultsString = resultsString..tostring(results[i]).." ["..resultType.."]"
 	end
+	
+	if #results > 1 then
+		outputChatBoxR("Command results: " ..resultsString)
+		return
+	end
+	
+	outputChatBoxR("Command executed!")
 end
 
 -- run command
