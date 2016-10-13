7de6f71247d597fdcaf281a0b98d74a716557832
Jusonex <jusonex96@gmail.com>
2016-10-13 12:10:08 +0200

    [webbrowser] Remove Browser.isSupported check

--- [gameplay]/webbrowser/client.lua
+++ [gameplay]/webbrowser/client.lua
@@ -10,12 +10,6 @@ addEventHandler("onClientResourceStart", resourceRoot,
 		addEventHandler("onClientGUIClick", icon,
 			function(button, state)
 				if button == "left" and state == "up" then
-					-- Check if browser is supported
-					if not Browser.isSupported() then
-						outputChatBox("Your operating system does not support the browser. Please consider upgrading to a newer OS version", 255, 0, 0)
-						return
-					end
-					
 					-- Create the browser now
 					if WebBrowserGUI.instance ~= nil then return end
 					WebBrowserGUI.instance = WebBrowserGUI:new()
