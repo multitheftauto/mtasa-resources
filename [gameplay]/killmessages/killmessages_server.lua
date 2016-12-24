0c3abe578c8f49d18d765dd21b96693f0b153654
Arran <arrantuna@hotmail.co.uk>
2016-12-24 17:16:18 +0000

    [killmessages] Fixed vehicle kills not being displayed properly
    1. Explosive kills like Hydra didn't show killer and displayed a fist or
    explosion symbol now they display the killer and Hydra symbol.
    2. Rhino kills were displaying as fist kills, they now display as Rhino
    kills. For some reason onClientPlayerWasted is returning the driver when
    using Rhino but most other vehicles would return the vehicle and that
    was preventing it from getting the used vehicle.

--- [gameplay]/killmessages/killmessages_server.lua
+++ [gameplay]/killmessages/killmessages_server.lua
@@ -23,59 +23,63 @@ local default = {
 ["startX"] = -10
 }
 
-local vehicleIDs = { [50]=true,[49]=true,[31]=true,[38]=true,[52]=true }
-function KillMessages_onPlayerWasted ( totalammo, killer, killerweapon, bodypart )
-	---These are special checks for certain kill types
-	local usedVehicle
-	if killerweapon == 19 and isElement(killer) then --rockets
-		killerweapon = killer and getElementType ( killer ) == "player" and getPedWeapon(killer)
-		if not killerweapon then
-			killerweapon = 51
-		end
-	elseif vehicleIDs[killerweapon] then --heliblades/rammed
-		if ( isElement(killer) and getElementType ( killer ) == "vehicle" ) then
-			usedVehicle = getElementModel ( killer )
-			killer = getVehicleOccupant ( killer, 0 )
-		end
-	elseif ( killerweapon == 59 and isElement(killer) ) then
-		if ( getElementType ( killer ) == "player" ) then
-			local vehicle = getPedOccupiedVehicle(killer)
-			if ( vehicle ) then
-				usedVehicle = getElementModel ( vehicle )
-			end
+function KillMessages_onPlayerWasted(totalammo, killer, killerweapon, bodypart)
+	local usedVehicle = nil
+	local killerType = nil
+	local killerweapon = killerweapon or 255
+	-- In very rare cases killer isn't nil or false but isn't an element.
+	if (killer and not isElement(killer)) then
+		killer = nil
+	end
+	if (killer) then
+		killerType = getElementType(killer)
+	end
+	-- Sometimes the killer is returned as the driver instead of vehicle, like when driving a Rhino but we need the vehicle ID
+	if (killerType == "player") then
+		local vehicle = getPedOccupiedVehicle(killer)
+		if (vehicle and getPedWeapon(killer) == 0) then
+			usedVehicle = getElementModel(vehicle)
 		end
 	end
-	--finish this
+	-- Change the killer into the vehicle controller
+	if (killer and killerType == "vehicle") then
+		usedVehicle = getElementModel(killer)
+		killer = getVehicleController(killer) or killer
+		killerType = getElementType(killer)
+	end
+	-- If killed by rocket and was on foot player turn it into their current weapon
+	if (killerweapon == 19 and killerType == "player" and not isPedInVehicle(killer)) then
+		killerweapon = getPedWeapon(killer) or 51
+	end
 	-- Got a killer? Print the normal "* X died" if not
-	if ( killer and isElement(killer) and getElementType ( killer ) == "player" ) then
-		local kr,kg,kb = getPlayerNametagColor	( killer )
-		if getPlayerTeam ( killer ) then
-			kr,kg,kb = getTeamColor ( getPlayerTeam ( killer ) )
+	if (killer and killerType == "player") then
+		local kr, kg, kb = getPlayerNametagColor(killer)
+		if (getPlayerTeam(killer)) then
+			kr, kg, kb = getTeamColor(getPlayerTeam(killer))
 		end
 		-- Suicide?
 		if (source == killer) then
-			if not killerweapon then killerweapon = 255 end
-			local triggered = triggerEvent ( "onPlayerKillMessage", source,false,killerweapon,bodypart )
-			--outputDebugString ( "Cancelled: "..tostring(triggered) )
-			if ( triggered ) then
-				eventTriggered ( source,false,killerweapon,bodypart,true,usedVehicle)
+			local triggered = triggerEvent("onPlayerKillMessage", source, false, killerweapon, bodypart)
+			--outputDebugString("Cancelled: "..tostring(triggered))
+			if (triggered) then
+				eventTriggered(source, false, killerweapon, bodypart, true, usedVehicle)
 				return
 			end
 		end
-		local triggered = triggerEvent ( "onPlayerKillMessage", source,killer,killerweapon,bodypart )
-				--outputDebugString ( "Cancelled: "..tostring(triggered) )
-		if ( triggered ) then
-			eventTriggered ( source,killer,killerweapon,bodypart,false,usedVehicle)
+		local triggered = triggerEvent("onPlayerKillMessage", source, killer, killerweapon, bodypart)
+		--outputDebugString("Cancelled: "..tostring(triggered))
+		if (triggered) then
+			eventTriggered(source, killer, killerweapon, bodypart, false, usedVehicle)
 		end
 	else
-		local triggered = triggerEvent ( "onPlayerKillMessage", source,false,killerweapon,bodypart )
-		--outputDebugString ( "Cancelled: "..tostring(triggered) )
-		if ( triggered ) then
-			eventTriggered ( source,false,killerweapon,bodypart,false,usedVehicle)
+		local triggered = triggerEvent("onPlayerKillMessage", source, false, killerweapon, bodypart)
+		--outputDebugString("Cancelled: "..tostring(triggered))
+		if (triggered) then
+			eventTriggered(source, false, killerweapon, bodypart, false, usedVehicle)
 		end
 	end
 end
-addEventHandler ( "onPlayerWasted", getRootElement(), KillMessages_onPlayerWasted )
+addEventHandler("onPlayerWasted", root, KillMessages_onPlayerWasted)
 
 addEvent ( "onPlayerKillMessage" )
 function eventTriggered ( source,killer,weapon,bodypart,suicide,usedVehicle )
