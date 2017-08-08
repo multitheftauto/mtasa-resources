4d73e766c09d3b082de5c54f283d3db1d9df61a0
Gabrielius <Drakath13@gmail.com>
2017-08-08 03:23:07 +0300

    Admin reporting system improvements (#61)
    * Add new setting; maxchatmsgs setting.
    
    * Improve reporting system
    
    - Added player reports, custom report categories and chat log storage.
    
    * Add report config
    
    - Added a configuration file for report categories. The same file also store reports.
    
    Don't force player selection and add a close button.
    
    * Add autofill for ban nick and reason
    
    * Move report categories from XML to settings
    
    * Clean up some code

--- [admin]/admin/client/gui/admin_message.lua
+++ [admin]/admin/client/gui/admin_message.lua
@@ -9,36 +9,74 @@
 **************************************]]
 
 aViewMessageForm = nil
+local aSuspectInfo = nil
+local viewID
 
 function aViewMessage ( id )
+	if ( aSuspectInfo and viewID and viewID ~= id ) then
+		destroyElement(aSuspectInfo)
+		aSuspectInfo = nil
+	end
+	viewID = id
 	if ( aViewMessageForm == nil ) then
 		local x, y = guiGetScreenSize()
-		aViewMessageForm	= guiCreateWindow ( x / 2 - 150, y / 2 - 125, 300, 250, "", false )
+		aViewMessageForm	= guiCreateWindow ( x / 2 - 150, y / 2 - 129, 300, 258, "", false )
 					   guiCreateLabel ( 0.05, 0.10, 0.30, 0.09, "Category:", true, aViewMessageForm )
-					   guiCreateLabel ( 0.05, 0.18, 0.30, 0.09, "Subject:", true, aViewMessageForm )
-					   guiCreateLabel ( 0.05, 0.26, 0.30, 0.09, "Time:", true, aViewMessageForm )
-					   guiCreateLabel ( 0.05, 0.34, 0.30, 0.09, "By:", true, aViewMessageForm )
+					   guiCreateLabel ( 0.05, 0.18, 0.30, 0.09, "Time:", true, aViewMessageForm )
+					   guiCreateLabel ( 0.05, 0.26, 0.30, 0.09, "By:", true, aViewMessageForm )
+		aViewMessageLabelSuspect = guiCreateLabel ( 0.05, 0.34, 0.30, 0.09, "Suspect:", true, aViewMessageForm )
+		aViewMessageSuspect	= guiCreateButton ( 0.40, 0.34, 0.55, 0.07, "View info", true, aViewMessageForm )
 		aViewMessageCategory	= guiCreateLabel ( 0.40, 0.10, 0.55, 0.09, "", true, aViewMessageForm )
-		aViewMessageSubject	= guiCreateLabel ( 0.40, 0.18, 0.55, 0.09, "", true, aViewMessageForm )
-		aViewMessageTime	= guiCreateLabel ( 0.40, 0.26, 0.55, 0.09, "", true, aViewMessageForm )
-		aViewMessageAuthor	= guiCreateLabel ( 0.40, 0.34, 0.55, 0.09, "", true, aViewMessageForm )
-		aViewMessageText	= guiCreateMemo ( 0.05, 0.41, 0.90, 0.45, "", true, aViewMessageForm )
+		aViewMessageTime	= guiCreateLabel ( 0.40, 0.18, 0.55, 0.09, "", true, aViewMessageForm )
+		aViewMessageAuthor	= guiCreateLabel ( 0.40, 0.26, 0.55, 0.09, "", true, aViewMessageForm )
+		aViewMessageText	= guiCreateMemo ( 0.05, 0.42, 0.90, 0.44, "", true, aViewMessageForm )
 					   guiMemoSetReadOnly ( aViewMessageText, true )
-		aViewMessageCloseB	= guiCreateButton ( 0.77, 0.88, 0.20, 0.09, "Close", true, aViewMessageForm )
-
+		aViewMessageCloseB	= guiCreateButton ( 0.77, 0.86, 0.20, 0.09, "Close", true, aViewMessageForm )
+		
 		addEventHandler ( "onClientGUIClick", aViewMessageForm, aClientMessageClick )
+		addEventHandler ( "onClientGUIClick", aViewMessageSuspect, aViewSuspectInfo )
+		
 		--Register With Admin Form
 		aRegister ( "Message", aViewMessageForm, aViewMessage, aViewMessageClose )
 	end
 	if ( _messages[id] ) then
 		guiSetText ( aViewMessageCategory, _messages[id].category )
-		guiSetText ( aViewMessageSubject, _messages[id].subject )
+		guiSetText ( aViewMessageForm, _messages[id].subject )
 		guiSetText ( aViewMessageTime, _messages[id].time )
 		guiSetText ( aViewMessageAuthor, _messages[id].author )
 		guiSetText ( aViewMessageText, _messages[id].text )
 		guiSetVisible ( aViewMessageForm, true )
+		local isVisible = _messages[id].suspect ~= nil
+		guiSetVisible ( aViewMessageLabelSuspect, isVisible )
+		guiSetVisible ( aViewMessageSuspect, isVisible )
 		guiBringToFront ( aViewMessageForm )
 		triggerServerEvent ( "aMessage", getLocalPlayer(), "read", id )
+	end
+end
+
+function aViewSuspectInfo ( button )
+	if ( button == "left" ) then
+		if ( source == aViewMessageSuspect ) then
+			if ( aSuspectInfo == nil ) then
+				local suspectInfo = _messages[viewID].suspect
+				if ( suspectInfo ) then
+					local x, y = guiGetScreenSize()
+					aSuspectInfo = guiCreateWindow(x / 2 - 145, y / 2 - 192.5, 290, 385, "Player information", false)
+					local btnClose = guiCreateButton(0.365, 0.88, 0.27, 0.10, "Close", true, aSuspectInfo)
+					addEventHandler("onClientGUIClick", btnClose, function()
+						destroyElement(aSuspectInfo)
+						aSuspectInfo = nil
+					end, false)
+
+					local infoMemo = guiCreateMemo(0.04, 0.1, 0.96, 0.75, "Nickname: "..suspectInfo.name.."\nAccount name: "..suspectInfo.username.."\nIP: "
+						..suspectInfo.ip.."\nSerial: "..suspectInfo.serial.."\nMTA version: "..suspectInfo.version.."\n\nChat log:\n"..suspectInfo.chatLog,
+					true, aSuspectInfo)
+					guiMemoSetReadOnly(infoMemo, true)
+				else
+					aMessageBox ( "error", "This report does have any suspect information." )
+				end
+			end
+		end
 	end
 end
 
@@ -46,11 +84,20 @@ function aViewMessageClose ( destroy )
 	if ( ( destroy ) or ( guiCheckBoxGetSelected ( aPerformanceMessage ) ) ) then
 		if ( aViewMessageForm ) then
 			removeEventHandler ( "onClientGUIClick", aViewMessageForm, aClientMessageClick )
+			if ( aViewMessageSuspect ) then
+				removeEventHandler ( "onClientGUIClick", aViewMessageSuspect, aViewSuspectInfo )
+			end
 			destroyElement ( aViewMessageForm )
 			aViewMessageForm = nil
+			destroyElement(aSuspectInfo)
+			aSuspectInfo = nil
 		end
 	else
 		if aViewMessageForm then guiSetVisible ( aViewMessageForm, false ) end
+		if aSuspectInfo then
+			destroyElement ( aSuspectInfo )
+			aSuspectInfo = nil
+		end
 	end
 end
 
