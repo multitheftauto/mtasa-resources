e6ea2ade3c347393995dda3a8527f5567d0872df
Qais Patankar <qaisjp@gmail.com>
2018-09-27 12:39:22 +0100

    admin2: fix reporting flow

--- [admin]/admin2/client/widgets/admin_report.lua
+++ [admin]/admin2/client/widgets/admin_report.lua
@@ -18,13 +18,12 @@ function aReport.Open()
         guiCreateLabel(0.05, 0.11, 0.20, 0.09, "Category:", true, aReport.Form)
         guiCreateLabel(0.05, 0.21, 0.20, 0.09, "Subject:", true, aReport.Form)
         guiCreateLabel(0.05, 0.30, 0.20, 0.07, "Message:", true, aReport.Form)
-        aReport.Category = guiCreateComboBox(0.30, 0.10, 0.65, 0.09, "Question", true, aReport.Form)
-        guiEditSetReadOnly(aReport.Category, true)
-        guiComboBoxAddItem(aReport.Categories "Question")
-        guiComboBoxAddItem(aReport.Categories "Suggestion")
-        guiComboBoxAddItem(aReport.Categories "Abuse")
-        guiComboBoxAddItem(aReport.Categories "Player")
-        guiComboBoxAddItem(aReport.Categories "Other")
+        aReport.Categories = guiCreateComboBox(0.30, 0.10, 0.65, 0.4, "Question", true, aReport.Form)
+        guiComboBoxAddItem(aReport.Categories, "Question")
+        guiComboBoxAddItem(aReport.Categories, "Suggestion")
+        guiComboBoxAddItem(aReport.Categories, "Abuse")
+        guiComboBoxAddItem(aReport.Categories, "Player")
+        guiComboBoxAddItem(aReport.Categories, "Other")
         aReport.Subject = guiCreateEdit(0.30, 0.20, 0.65, 0.09, "", true, aReport.Form)
         guiHandleInput(aReport.Subject)
         aReport.Message = guiCreateMemo(0.05, 0.38, 0.90, 0.45, "", true, aReport.Form)
@@ -32,53 +31,59 @@ function aReport.Open()
         aReport.Accept = guiCreateButton(0.40, 0.88, 0.25, 0.09, "Send", true, aReport.Form)
         aReport.Cancel = guiCreateButton(0.70, 0.88, 0.25, 0.09, "Cancel", true, aReport.Form)
 
-        addEventHandler("onClientGUIClick", aReportForm, aClientReportClick)
+        addEventHandler("onClientGUIClick", aReport.Form, aClientReportClick)
     end
     guiBringToFront(aReport.Form)
     showCursor(true)
 end
 addCommandHandler("report", aReport.Open)
 
-function aReportClose()
+function aReport.CloseCursor()
     guiSetInputEnabled(false)
-    if (aReportForm) then
-        removeEventHandler("onClientGUIClick", aReportForm, aClientReportClick)
-        destroyElement(aReportForm)
-        aReportForm = nil
-        showCursor(false)
+    showCursor(false)
+end
+
+function aReport.Close(closeCursor)
+    if (aReport.Form) then
+        removeEventHandler("onClientGUIClick", aReport.Form, aClientReportClick)
+        destroyElement(aReport.Form)
+        aReport.Form = nil
+
+        if closeCursor then
+            aReport.CloseCursor()
+        end
     end
 end
 
 function aClientReportClick(button)
-    if (source == aReportCategory) then
-        guiBringToFront(aReportDropDown)
-    end
-    if (source ~= aReportCategories) then
-        guiSetVisible(aReportCategories, false)
-    end
     if (button == "left") then
-        if (source == aReportAccept) then
-            if ((string.len(guiGetText(aReportSubject)) < 1) or (string.len(guiGetText(aReportMessage)) < 5)) then
-                messageBox("Subject/Message missing.", MB_ERROR)
+        if (source == aReport.Accept) then
+            if ((string.len(guiGetText(aReport.Subject)) < 1) or (string.len(guiGetText(aReport.Message)) < 5)) then
+                messageBox("Subject/Message missing or too short.", MB_ERROR)
             else
                 local tableOut = {}
-                tableOut.category = guiGetText(aReportCategory)
-                tableOut.subject = guiGetText(aReportSubject)
-                tableOut.message = guiGetText(aReportMessage)
-                triggerServerEvent("aMessage", getLocalPlayer(), "new", tableOut)
-                aReportClose()
+                tableOut.category = aReport.Categories:getItemText(aReport.Categories.selected)
+                tableOut.subject = guiGetText(aReport.Subject)
+                tableOut.message = guiGetText(aReport.Message)
+                triggerServerEvent("aMessage", localPlayer, "new", tableOut)
+                
+                -- Hide report window, but don't hide cursor
+                aReport.Close(false)
+                
+                -- Show messageBox and pause
                 messageBox("Your message has been submited and will be processed as soon as possible.", MB_INFO)
-                setTimer(aMessageBox.Close, 3000, 1, true)
+                
+                -- hide the cursor now that the messageBox window has been done
+                aReport.CloseCursor()
+
+                -- setTimer(aMessageBox.Close, 3000, 1, true)
             end
-        elseif (source == aReportSubject) then
+        elseif (source == aReport.Subject) then
             guiSetInputEnabled(true)
-        elseif (source == aReportMessage) then
+        elseif (source == aReport.Message) then
             guiSetInputEnabled(true)
-        elseif (source == aReportCancel) then
-            aReportClose()
-        elseif (source == aReportDropDown) then
-            guiBringToFront(aReportCategories)
-            guiSetVisible(aReportCategories, true)
+        elseif (source == aReport.Cancel) then
+            aReport.Close(true)
         end
     end
 end
