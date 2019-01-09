d47e42af0e1a4b19adbe95cd39965bc9cea7188f
Patrik Juvonen <patrik@scope.studio>
2019-01-09 15:47:11 +0200

    Fix #137: Add exported functions and setting to toggle driveby This commit, once applied, enables toggling of the *realdriveby* feature for a specific player.
    Export new server functions:
      - `bool isDrivebyEnabled ( player thePlayer )`
      - `bool setDrivebyEnabled ( player thePlayer, bool enabled )`
    
    Export new client functions:
      - `bool isDrivebyEnabled ( )`
      - `bool setDrivebyEnabled ( bool enabled [, bool syncToServer ] )`
    
    Add new `meta.xml` setting:
      - `driveby_enabled` (default value: `[true]`)

---
+++ [gameplay]/realdriveby/exports_server.lua
@@ -0,0 +1,16 @@
+function isDrivebyEnabled(player)
+	assert(isElement(player) and getElementType(player) == "player", "Bad argument @ 'isDrivebyEnabled' [Expected player at argument 1, got " .. type(player) .. "]")
+
+	return syncedPlayerStates[player] == nil and settings.enabled or syncedPlayerStates[player]
+end
+
+function setDrivebyEnabled(player, state)
+	assert(isElement(player) and getElementType(player) == "player", "Bad argument @ 'setDrivebyEnabled' [Expected player at argument 1, got " .. type(player) .. "]")
+
+	local stateType = type(state)
+	assert(stateType == "boolean", "Bad argument @ 'setDrivebyEnabled' [Expected boolean at argument 2, got " .. stateType .. "]")
+
+	syncedPlayerStates[player] = state
+
+	triggerClientEvent(player, "driveby_setDrivebyEnabled", player, state, false)
+end
