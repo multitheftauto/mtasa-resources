5d4ebffe1a7443ecf6fea85a694840946e043cc3
Patrik Juvonen <patrik@scope.studio>
2019-01-09 22:37:37 +0200

    Improve readability of the previously expanded expression

--- [gameplay]/realdriveby/exports_server.lua
+++ [gameplay]/realdriveby/exports_server.lua
@@ -1,11 +1,11 @@
 function isDrivebyEnabled(player)
 	assert(isElement(player) and getElementType(player) == "player", "Bad argument @ 'isDrivebyEnabled' [Expected player at argument 1, got " .. type(player) .. "]")
 
-	if syncedPlayerStates[player] == nil then
-		return settings.enabled
+	if syncedPlayerStates[player] ~= nil then
+		return syncedPlayerStates[player]
 	end
 
-	return syncedPlayerStates[player] or false
+	return settings.enabled
 end
 
 function setDrivebyEnabled(player, state)
