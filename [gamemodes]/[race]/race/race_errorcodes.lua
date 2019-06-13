00ffc1d108423fa5f45776584edeca8649256d21
Patrik Juvonen <patrik@scope.studio>
2019-06-13 09:24:00 +0300

    Remove duplicate errorCode in race and use new votemanager errorCode export

--- [gamemodes]/[race]/race/race_errorcodes.lua
+++ [gamemodes]/[race]/race/race_errorcodes.lua
@@ -1,20 +1,30 @@
-errorCode = {
-	--startPoll error codes: 1x
-	pollAlreadyRunning = 10,
-	lessThanTwoOptions = 11,
-	invalidTitle = 12,
-	invalidVisibleTo = 13,
-	noVoters = 14,
-	startCancelled = 15,
-	--stopPoll error codes: 2x
-	noPollRunning = 20,
-	stopCancelled = 21,
-	--premade poll error codes: 3x
-	invalidMap = 30,
-	invalidPlayer = 31,
-	twoMapsNeeded = 32,
-	noGamemodeRunning = 33,
-	mapIsntCompatible = 34,
-	twoModesNeeded = 35,
-	onlyOneCompatibleMap = 36
-}
+errorCode = {}
+
+local function loadAdditionalErrorCodes()
+	-- Load error codes from votemanager resource if it's running
+	local votemanagerResource = getResourceFromName("votemanager")
+	if (votemanagerResource) then
+		if (getResourceState(votemanagerResource) == "running") then
+			local votemanagerErrorCodes = exports.votemanager:getErrorCodes()
+			if (votemanagerErrorCodes) and (type(votemanagerErrorCodes) == "table") then
+				for key, code in pairs(votemanagerErrorCodes) do
+					-- If the error code is not defined here already, then set it
+					if (errorCode[key] == nil) then
+						errorCode[key] = code
+					end
+				end
+			else
+				outputDebugString("Could not load error codes from votemanager because it returned '" .. tostring(votemanagerErrorCodes) .. "' [" .. type(votemanagerErrorCodes) .. "]. Expected a table.", 1)
+			end
+		else
+			outputDebugString("Could not load error codes from votemanager because it is not running.", 1)
+		end
+	else
+		outputDebugString("Could not load error codes from votemanager because it is not available.", 1)
+	end
+end
+if (localPlayer) then
+	addEventHandler("onClientResourceStart", resourceRoot, loadAdditionalErrorCodes)
+else
+	addEventHandler("onResourceStart", resourceRoot, loadAdditionalErrorCodes)
+end
