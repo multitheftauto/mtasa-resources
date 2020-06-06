multitheftauto/mtasa-resources

commit 83884881f1a171ac5f77d76ed2da2f280fcd264b

- Fixed project image files quality issues introduced in 99a39b7 - Addendum to trailing fixes in same commit

[admin]/admin/client/colorpicker/colorpicker.lua
@@ -1,4 +1,4 @@
-﻿sw, sh = guiGetScreenSize()
+sw, sh = guiGetScreenSize()
 
 pickerTable = {}
 
@@ -44,7 +44,7 @@ function colorPicker.create(id, start, title)
   cp.gui.svmap = guiCreateStaticImage(16, 32, 256, 256, "client/colorpicker/blank.png", false, cp.gui.window)
   cp.gui.hbar = guiCreateStaticImage(288, 32, 32, 256, "client/colorpicker/blank.png", false, cp.gui.window)
   cp.gui.blank = guiCreateStaticImage(336, 32, 64, 64, "client/colorpicker/blank.png", false, cp.gui.window)
-  cp.gui.edith = guiCreateLabel(338, 106, 64, 20, "H: 0°", false, cp.gui.window)
+  cp.gui.edith = guiCreateLabel(338, 106, 64, 20, "H: 0Â°", false, cp.gui.window)
   cp.gui.edits = guiCreateLabel(338, 126, 64, 20, "S: 100%", false, cp.gui.window)
   cp.gui.editv = guiCreateLabel(338, 146, 64, 20, "V: 100%", false, cp.gui.window)
   cp.gui.editr = guiCreateLabel(338, 171, 64, 20, "R: 255", false, cp.gui.window)
@@ -64,7 +64,7 @@ function colorPicker.create(id, start, title)
   cp.handlers.guiBlur = function() cp:guiBlur() end
   cp.handlers.pickColor = function() cp:pickColor() end
   cp.handlers.destroy = function() cp:destroy() end
-  
+
   addEventHandler("onClientGUIMouseDown", cp.gui.svmap, cp.handlers.mouseDown, false)
   addEventHandler("onClientMouseLeave", cp.gui.svmap, cp.handlers.mouseSnap, false)
   addEventHandler("onClientMouseMove", cp.gui.svmap, cp.handlers.mouseMove, false)
@@ -138,7 +138,7 @@ function colorPicker:updateColor()
   self.color.current = tocolor(self.color.r, self.color.g, self.color.b,255)
   self.color.huecurrent = tocolor(hsv2rgb(self.color.h, 1, 1))
   self.color.hex = string.format("#%02X%02X%02X", self.color.r, self.color.g, self.color.b)
-  guiSetText(self.gui.edith, "H: "..tostring(math.round(self.color.h*360)).."°")
+  guiSetText(self.gui.edith, "H: "..tostring(math.round(self.color.h*360)).."Â°")
   guiSetText(self.gui.edits, "S: "..tostring(math.round(self.color.s*100)).."%")
   guiSetText(self.gui.editv, "V: "..tostring(math.round(self.color.v*100)).."%")
   guiSetText(self.gui.editr, "R: "..tostring(self.color.r))

[editor]/editor_main/client/collisions.lua
@@ -1,7 +1,7 @@
-﻿--[[
+--[[
 	Collection of functions to detect if a line hits a shape
 
-	Special thanks to Rayco "XeNMaX" Hernandez García for his BIG help.
+	Special thanks to Rayco "XeNMaX" Hernandez GarcÃ­a for his BIG help.
 ]]
 
 local ZERO_TOLERANCE = 0.000001
@@ -60,11 +60,11 @@ collisionTest = { }
 collisionTest.Rectangle = function(lineStart, lineEnd, rectangleCenter, sizeX, sizeY )
     -- check if line intersects rectangle around element
 	local ratio = rectangleCenter.z / lineEnd.z
-	
+
 	lineEnd.x = rectangleCenter.x * lineEnd.x
 	lineEnd.y = rectangleCenter.y * lineEnd.y
 	lineEnd.z = rectangleCenter.z
-	
+
 	if lineEnd.x < (rectangleCenter.x + sizeX/2) and lineEnd.x > (rectangleCenter.x - sizeX/2) then
 		if lineEnd.y < (rectangleCenter.y + sizeY/2) and lineEnd.y > (rectangleCenter.y - sizeY/2) then
 			return lineEnd
@@ -141,7 +141,7 @@ collisionTest.Cylinder = function(lineStart, lineEnd, cylCenter, cylRadius, cylH
 
 		if (fDiscr < 0.0) then
 			return nil
-		
+
 		elseif (fDiscr > ZERO_TOLERANCE) then
 			fRoot = math.sqrt(fDiscr)
 			fInv = 1.0 / fA2

[gameplay]/webbrowser/WebBrowserGUI.lua
@@ -13,13 +13,13 @@ function WebBrowserGUI:constructor()
 	self.m_ForwardButton = GuiButton(42, 25, 32, 32, ">", false, self.m_Window)
 	self.m_ForwardButton:setEnabled(false)
 	self.m_EditAddress = GuiEdit(77, 25, sizeX - 192, 32, "Please enter an address", false, self.m_Window)
-	self.m_DevButton = GuiButton(sizeX - 110, 25, 32, 32, "✎", false, self.m_Window)
-	self.m_LoadButton = GuiButton(sizeX - 75, 25, 32, 32, "➽", false, self.m_Window)
-	self.m_ButtonClose = GuiButton(sizeX - 38, 25, 24, 24, "✖", false, self.m_Window)
+	self.m_DevButton = GuiButton(sizeX - 110, 25, 32, 32, "â", false, self.m_Window)
+	self.m_LoadButton = GuiButton(sizeX - 75, 25, 32, 32, "â½", false, self.m_Window)
+	self.m_ButtonClose = GuiButton(sizeX - 38, 25, 24, 24, "â", false, self.m_Window)
 	self.m_ButtonClose:setProperty("NormalTextColour", "FFFF2929")
 	self.m_ButtonClose:setProperty("HoverTextColour", "FF990909")
 	self.m_ButtonClose:setFont("default-bold-small")
-	
+
 	self.m_Browser = GuiBrowser(5, 62, sizeX - 10, sizeY - 67, false, false, false, self.m_Window)
 
 	if not self.m_Browser then
@@ -27,15 +27,15 @@ function WebBrowserGUI:constructor()
 		self.m_Window:destroy()
 		return
 	end
-	
+
 	local browser = self.m_Browser:getBrowser()
 	addEventHandler("onClientBrowserCreated", browser, function(...) self:Browser_Created(...) end)
 	addEventHandler("onClientBrowserNavigate", browser, function(...) self:Browser_Navigate(...) end)
 	addEventHandler("onClientBrowserWhitelistChange", root, function(...) self:Browser_WhitelistChange(...) end)
 	addEventHandler("onClientBrowserDocumentReady", browser, function(...) self:Browser_DocumentReady(...) end)
-	
+
 	self.m_RequestedURL = ""
-	
+
 	showCursor(true)
 	GuiElement.setInputMode("no_binds_when_editing")
 end
@@ -71,7 +71,7 @@ end
 function WebBrowserGUI:Browser_DocumentReady()
 	self.m_Window:setText("Web browser: " .. tostring(self.m_Browser:getBrowser():getTitle()))
 	self.m_EditAddress:setText(tostring(self.m_Browser:getBrowser():getURL()))
-	
+
 	self.m_BackButton:setEnabled(self.m_Browser:getBrowser():canNavigateBack())
 	self.m_ForwardButton:setEnabled(self.m_Browser:getBrowser():canNavigateForward())
 end
@@ -110,7 +110,7 @@ function WebBrowserGUI:DevButton_Click(button, state)
 			exports.msgbox:guiShowMessageBox("You have to enable the development using setDevelopmentMode", "error", "Development mode required", false)
 			return
 		end
-		
+
 		self.m_Browser:getBrowser():toggleDevTools(true)
 	end
 end
@@ -128,13 +128,13 @@ function WebBrowserGUI:loadURL(url)
 	elseif url:sub(0, 7)  ~= "http://" and url:sub(0, 8) ~= "https://" then
 		url = "http://"..url
 	end
-	
+
 	if Browser.isDomainBlocked(url, true) then
 		self.m_RequestedURL = url
 		Browser.requestDomains({url}, true)
 		return
 	end
-	
+
 	self.m_EditAddress:setText(url)
 	self.m_Browser:getBrowser():loadURL(url)
 end

[gameplay]/freeroam/colorpicker/colorpicker.lua
@@ -60,7 +60,7 @@ colorPicker = {
 	constructor = function( info )
 		info = info or colorPicker.default
 		colorPicker.value = colorPicker.convertColorToTable(info.value)
-		
+
 		colorPicker.buttonWidth = info.width * colorPicker.default.buttonWidth
 
 		local offset = 1 --px
@@ -73,10 +73,10 @@ colorPicker = {
 			offset = offset / sizeX
 			height = height / sizeY
 		end
-	 
+
 		colorPicker.GUI = {}
 		colorPicker.children = {}
-		
+
 		-- Create the color selection window
 		local screenW, screenH = guiGetScreenSize()
 		colorPicker.selectWindow = info.selectWindow
@@ -134,7 +134,7 @@ colorPicker = {
 		colorPicker.GUI.labelH = guiCreateLabel(info.selectWindow.hslX, info.selectWindow.hslY + 3,
 		                                 10, 20, "H", false, colorPicker.GUI.selectWindow)
 		guiSetFont(colorPicker.GUI.labelH, "default-bold-small")
-		
+
 		colorPicker.children.S = guiCreateEdit(info.selectWindow.hslX + 10, info.selectWindow.hslY + info.selectWindow.hslHeight, info.selectWindow.hslWidth, info.selectWindow.hslHeight, tostring( math.floor(colorPicker.s * 255) ), false, colorPicker.GUI.selectWindow)
 		guiEditSetMaxLength(colorPicker.children.S, 3)
 		addEventHandler("onClientGUIChanged", colorPicker.children.S, colorPicker.forceNaturalAndRange)
@@ -161,23 +161,23 @@ colorPicker = {
 
 		colorPicker.GUI.historyLabel = guiCreateLabel(info.selectWindow.historyX, info.selectWindow.historyY,
 		                                       150, 15, "Recently used colors:", false, colorPicker.GUI.selectWindow)
-											   
+
 		colorPicker.GUI.noteLabel = guiCreateLabel(info.selectWindow.noteX, info.selectWindow.noteY,
 		                                       190, 15, "Click outside the window to close.", false, colorPicker.GUI.selectWindow)
 		guiSetFont( colorPicker.GUI.noteLabel, "default-small" )
-		
+
 		checkColor1 = guiCreateCheckBox(info.selectWindow.ra1X, info.selectWindow.ra1Y,
 												70, 15, "Color 1", true, false, colorPicker.GUI.selectWindow)
-		
+
 		checkColor2 = guiCreateCheckBox(info.selectWindow.ra2X, info.selectWindow.ra2Y,
 												70, 15, "Color 2", true, false, colorPicker.GUI.selectWindow)
-		
+
 		checkColor3 = guiCreateCheckBox(info.selectWindow.ra3X, info.selectWindow.ra3Y,
 												70, 15, "Color 3", true, false, colorPicker.GUI.selectWindow)
-		
+
 		checkColor4 = guiCreateCheckBox(info.selectWindow.ra4X, info.selectWindow.ra4Y,
 												70, 15, "Color 4", true, false, colorPicker.GUI.selectWindow)
-		
+
 		checkColor5 = guiCreateCheckBox(info.selectWindow.ra5X, info.selectWindow.ra5Y,
 												75, 15, "Headlights", true, false, colorPicker.GUI.selectWindow)
 
@@ -191,7 +191,7 @@ colorPicker = {
 				guiSetText(source, changedText)
 			end
 		end
-		
+
 		local inputNumber = tonumber(guiGetText( source ))
 		if inputNumber then
 			local clampedNumber = inputNumber
@@ -261,7 +261,7 @@ colorPicker = {
 	updateTempColors = function()
 		local r, g, b, a = colorPicker.value[1], colorPicker.value[2], colorPicker.value[3], colorPicker.value[4]
 		return r, g, b
-		
+
 	end,
 	openSelect = function( currentColor )
 		if colorPicker.isSelectOpen then return end
@@ -367,7 +367,7 @@ colorPicker = {
 		else
 			r, g, b, a = colorPicker.value[1] / 255, colorPicker.value[2] / 255, colorPicker.value[3] / 255, colorPicker.value[4] / 255
 		end
-		
+
 		-- Draw the lines pointing to the current selected color
 		local x = paletteX + (colorPicker.h * 255)
 		local y = paletteY + ((1 - colorPicker.s) * 255)

[gameplay]/scoreboard/colorpicker/colorpicker.lua
@@ -50,7 +50,7 @@ colorPicker = {
 	constructor = function( info )
 		info = info or colorPicker.default
 		colorPicker.value = colorPicker.convertColorToTable(info.value)
-		
+
 		colorPicker.buttonWidth = info.width * colorPicker.default.buttonWidth
 
 		local offset = 1 --px
@@ -69,7 +69,7 @@ colorPicker = {
 
 		colorPicker.GUI = {}
 		colorPicker.children = {}
-		
+
 		-- Create the color selection window
 		local screenW, screenH = guiGetScreenSize()
 		colorPicker.selectWindow = info.selectWindow
@@ -127,7 +127,7 @@ colorPicker = {
 		colorPicker.GUI.labelH = guiCreateLabel(info.selectWindow.hslX, info.selectWindow.hslY + 3,
 		                                 10, 20, "H", false, colorPicker.GUI.selectWindow)
 		guiSetFont(colorPicker.GUI.labelH, "default-bold-small")
-		
+
 		colorPicker.children.S = guiCreateEdit(info.selectWindow.hslX + 10, info.selectWindow.hslY + info.selectWindow.hslHeight, info.selectWindow.hslWidth, info.selectWindow.hslHeight, tostring( math.floor(colorPicker.s * 255) ), false, colorPicker.GUI.selectWindow)
 		guiEditSetMaxLength(colorPicker.children.S, 3)
 		addEventHandler("onClientGUIChanged", colorPicker.children.S, colorPicker.forceNaturalAndRange)
@@ -154,7 +154,7 @@ colorPicker = {
 
 		colorPicker.GUI.historyLabel = guiCreateLabel(info.selectWindow.historyX, info.selectWindow.historyY,
 		                                       150, 15, "Recently used colors:", false, colorPicker.GUI.selectWindow)
-											   
+
 		colorPicker.GUI.noteLabel = guiCreateLabel(info.selectWindow.noteX, info.selectWindow.noteY,
 		                                       190, 15, "Click outside the window to close.", false, colorPicker.GUI.selectWindow)
 		guiSetFont( colorPicker.GUI.noteLabel, "default-small" )
@@ -169,7 +169,7 @@ colorPicker = {
 				guiSetText(source, changedText)
 			end
 		end
-		
+
 		local inputNumber = tonumber(guiGetText( source ))
 		if inputNumber then
 			local clampedNumber = inputNumber
@@ -341,7 +341,7 @@ colorPicker = {
 		else
 			r, g, b, a = colorPicker.value[1] / 255, colorPicker.value[2] / 255, colorPicker.value[3] / 255, colorPicker.value[4] / 255
 		end
-		
+
 		-- Draw the lines pointing to the current selected color
 		local x = paletteX + (colorPicker.h * 255)
 		local y = paletteY + ((1 - colorPicker.s) * 255)

commit 36ff1895eda7bb3e06114f81ccc2dd6afdfd5a24

Fix broken special characters
Introduced in 83884881f1

[admin]/admin/client/colorpicker/colorpicker.lua
@@ -44,7 +44,7 @@ function colorPicker.create(id, start, title)
   cp.gui.svmap = guiCreateStaticImage(16, 32, 256, 256, "client/colorpicker/blank.png", false, cp.gui.window)
   cp.gui.hbar = guiCreateStaticImage(288, 32, 32, 256, "client/colorpicker/blank.png", false, cp.gui.window)
   cp.gui.blank = guiCreateStaticImage(336, 32, 64, 64, "client/colorpicker/blank.png", false, cp.gui.window)
-  cp.gui.edith = guiCreateLabel(338, 106, 64, 20, "H: 0Â°", false, cp.gui.window)
+  cp.gui.edith = guiCreateLabel(338, 106, 64, 20, "H: 0°", false, cp.gui.window)
   cp.gui.edits = guiCreateLabel(338, 126, 64, 20, "S: 100%", false, cp.gui.window)
   cp.gui.editv = guiCreateLabel(338, 146, 64, 20, "V: 100%", false, cp.gui.window)
   cp.gui.editr = guiCreateLabel(338, 171, 64, 20, "R: 255", false, cp.gui.window)
@@ -138,7 +138,7 @@ function colorPicker:updateColor()
   self.color.current = tocolor(self.color.r, self.color.g, self.color.b,255)
   self.color.huecurrent = tocolor(hsv2rgb(self.color.h, 1, 1))
   self.color.hex = string.format("#%02X%02X%02X", self.color.r, self.color.g, self.color.b)
-  guiSetText(self.gui.edith, "H: "..tostring(math.round(self.color.h*360)).."Â°")
+  guiSetText(self.gui.edith, "H: "..tostring(math.round(self.color.h*360)).."°")
   guiSetText(self.gui.edits, "S: "..tostring(math.round(self.color.s*100)).."%")
   guiSetText(self.gui.editv, "V: "..tostring(math.round(self.color.v*100)).."%")
   guiSetText(self.gui.editr, "R: "..tostring(self.color.r))

[editor]/editor_main/client/collisions.lua
@@ -1,7 +1,7 @@
 --[[
 	Collection of functions to detect if a line hits a shape
 
-	Special thanks to Rayco "XeNMaX" Hernandez GarcÃ­a for his BIG help.
+	Special thanks to Rayco "XeNMaX" Hernandez García for his BIG help.
 ]]
 
 local ZERO_TOLERANCE = 0.000001

[gameplay]/webbrowser/WebBrowserGUI.lua
@@ -13,9 +13,9 @@ function WebBrowserGUI:constructor()
 	self.m_ForwardButton = GuiButton(42, 25, 32, 32, ">", false, self.m_Window)
 	self.m_ForwardButton:setEnabled(false)
 	self.m_EditAddress = GuiEdit(77, 25, sizeX - 192, 32, "Please enter an address", false, self.m_Window)
-	self.m_DevButton = GuiButton(sizeX - 110, 25, 32, 32, "â", false, self.m_Window)
-	self.m_LoadButton = GuiButton(sizeX - 75, 25, 32, 32, "â½", false, self.m_Window)
-	self.m_ButtonClose = GuiButton(sizeX - 38, 25, 24, 24, "â", false, self.m_Window)
+	self.m_DevButton = GuiButton(sizeX - 110, 25, 32, 32, "✎", false, self.m_Window)
+	self.m_LoadButton = GuiButton(sizeX - 75, 25, 32, 32, "➽", false, self.m_Window)
+	self.m_ButtonClose = GuiButton(sizeX - 38, 25, 24, 24, "✖", false, self.m_Window)
 	self.m_ButtonClose:setProperty("NormalTextColour", "FFFF2929")
 	self.m_ButtonClose:setProperty("HoverTextColour", "FF990909")
 	self.m_ButtonClose:setFont("default-bold-small")