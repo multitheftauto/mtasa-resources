a0eccd08c2d9a7e81ffe1cc1ee03142c62dd6bcb
John Lillis <johnmichaelffs@gmail.com>
2020-06-16 17:19:57 -0400

    Clean up code, randomize player colors onGamemodeMapStart

--- [gameplay]/playercolors/playercolors.lua
+++ [gameplay]/playercolors/playercolors.lua
@@ -1,20 +1,19 @@
-local lowerBound,upperBound = unpack(get"color_range")
+local lowerBound, upperBound = unpack(get("color_range"))
 
-addEventHandler ( "onResourceStart", resourceRoot,
-	function()
-		for i,player in ipairs(getElementsByType"player") do
-			processPlayer ( player )
-		end
-	end
-)
-
-function processPlayer ( player )
+function randomizePlayerColor(player)
 	player = player or source
 	local r, g, b = math.random(lowerBound, upperBound), math.random(lowerBound, upperBound), math.random(lowerBound, upperBound)
 	setPlayerNametagColor(player, r, g, b)
 end
-addEventHandler ( "onPlayerJoin", root, processPlayer )
+addEventHandler("onPlayerJoin", root, randomizePlayerColor)
 
+local function randomizeAllPlayerColors()
+	for _, player in ipairs(getElementsByType("player")) do
+		randomizePlayerColor(player)
+	end
+end
+addEventHandler("onResourceStart", resourceRoot, randomizeAllPlayerColors)
+addEventHandler("onGamemodeMapStart", root, randomizeAllPlayerColors) -- mapmanager resets player colors to white when the map ends
 
 addEventHandler('onPlayerChat', root,
 	function(msg, type)
