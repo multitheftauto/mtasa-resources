c764042f2dc2c4295befcf981b0206aeec851469
xLive <30420446+xLive@users.noreply.github.com>
2021-01-23 02:03:36 +0300

    admin2: remove a blank line in the admin chat memo

--- [admin]/admin2/client/main/admin_chat.lua
+++ [admin]/admin2/client/main/admin_chat.lua
@@ -123,8 +123,9 @@ function aChatTab.onClientGUIAccepted(element)
 end
 
 function aChatTab.onClientAdminChat(message)
-    guiSetText(aChatTab.AdminChat, guiGetText(aChatTab.AdminChat) .. "" .. getPlayerName(source) .. ": " .. message)
-    guiSetProperty(aChatTab.AdminChat, "CaratIndex", tostring(string.len(guiGetText(aChatTab.AdminChat))))
+    local chat = guiGetText(aChatTab.AdminChat)
+    guiSetText(aChatTab.AdminChat, (chat ~= "\n" and chat or "") .. getPlayerName(source) .. ": " .. message)
+    guiSetProperty(aChatTab.AdminChat, "CaratIndex", tostring(string.len(chat)))
     if (aGetSetting("adminChatOutput")) then
         outputChatBox("ADMIN> " .. getPlayerName(source) .. ": " .. message, 255, 0, 0)
     end
