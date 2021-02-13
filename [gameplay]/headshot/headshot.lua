a1ce4e55b79d2aacdbc05737c9be261f2496fe2c
majqqq <>
2021-02-13 09:09:02 +0100

    Main commit

--- [gameplay]/headshot/headshot.lua
+++ [gameplay]/headshot/headshot.lua
@@ -1,26 +1,19 @@
-local removeHeadOnHeadshot = get("removeHeadOnHeadshot");
+local removeHeadOnHeadshot = get("removeHeadOnHeadshot")
 
-addEvent "onPlayerHeadshot"
+function onPlayerDamage(attacker, weapon, bodypart, loss)
+	if bodypart == 9 then
+		killPed(source, attacker, weapon, bodypart)
 
-addEventHandler("onPlayerDamage", getRootElement(),
-	function (attacker, weapon, bodypart, loss)
-		if bodypart == 9 then
-			local result = triggerEvent("onPlayerHeadshot", source, attacker, weapon, loss)
-			if result == true then
-				killPed(source, attacker, weapon, bodypart)
-				if removeHeadOnHeadshot then
-					setPedHeadless(source, true)
-				end
-			end
+		if removeHeadOnHeadshot then
+			setPedHeadless(source, true)
 		end
 	end
-)
+end
+addEventHandler("onPlayerDamage", root, onPlayerDamage)
 
-addEventHandler("onPlayerSpawn", getRootElement(),
-	function()
-		-- Restore head if it got blown off
-		if removeHeadOnHeadshot and isPedHeadless(source) then
-			setPedHeadless(source, false)
-		end
+function onPlayerSpawn()
+	if removeHeadOnHeadshot and isPedHeadless(source) then
+		setPedHeadless(source, false) -- Restore head if it got blown off
 	end
-)
+end
+addEventHandler("onPlayerSpawn", root, onPlayerSpawn)
