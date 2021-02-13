316972996e1cd38d639aeb6e7a6ce685dcc4fcc4
majqqq <>
2021-02-13 12:55:19 +0100

    Main commit.

--- [gameplay]/reload/reload.lua
+++ [gameplay]/reload/reload.lua
@@ -1,5 +1,7 @@
-function reloadWeapon()
-	reloadPedWeapon(client)
+local function reloadWeapon()
+	if client then
+		reloadPedWeapon(client)
+	end
 end
 addEvent("relWep", true)
-addEventHandler("relWep", resourceRoot, reloadWeapon)
+addEventHandler("relWep", root, reloadWeapon)
--- [gameplay]/reload/reload_c.lua
+++ [gameplay]/reload/reload_c.lua
@@ -13,17 +13,20 @@ local blockedTasks = {
     ["TASK_SIMPLE_CAR_OPEN_DOOR_FROM_OUTSIDE"] = true,
 }
 
-local function reloadWeapon()
+local function reloadTimer()
     if blockedTasks[getPedSimplestTask(localPlayer)] then
         return
     end
-    triggerServerEvent("relWep", resourceRoot)
+
+    triggerServerEvent("relWep", localPlayer)
 end
 
 -- The jump task is not instantly detectable and bindKey works quicker than getControlState
 -- If you try to reload and jump at the same time, you will be able to instant reload.
 -- We work around this by adding an unnoticable delay to foil this exploit.
-addCommandHandler("Reload weapon", function()
-    setTimer(reloadWeapon, 50, 1)
-end)
+
+local function reloadWeapon()
+	setTimer(reloadTimer, 50, 1)
+end
+addCommandHandler("Reload weapon", reloadWeapon)
 bindKey("r", "down", "Reload weapon")
