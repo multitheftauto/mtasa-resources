f14920ac6f32c836f206965b2d7994fef5a581b3
patrikjuvonen <22572159+patrikjuvonen@users.noreply.github.com>
2021-02-16 01:37:42 +0200

    webstats: refactor code (#293)
    - Use Chart.js
    - Refactor code
    - Reduce frequency to 5 seconds from 60 seconds
    * Fixes #278

--- [web]/webstats/internal_stats.lua
+++ [web]/webstats/internal_stats.lua
@@ -1,89 +1,87 @@
-call(getThisResource(), "registerStat", getThisResource(), "getVehicleCount", "Vehicles", "The number of vehicles")
+local publichatcount = 0
+local teamchatcount = 0
+local totalchatcount = 0
+local vehicleswasted = 0
+local damagecount = 0
+
+registerStat(resource, "getVehicleCount", "Vehicles", "The number of vehicles")
 function getVehicleCount()
-	return #getElementsByType("vehicle");
+	return #getElementsByType("vehicle")
 end
 
-call(getThisResource(), "registerStat", getThisResource(), "getObjectCount", "Objects", "The number of objects")
+registerStat(resource, "getObjectCount", "Objects", "The number of objects")
 function getObjectCount()
-	return #getElementsByType("object");
+	return #getElementsByType("object")
 end
 
-call(getThisResource(), "registerStat", getThisResource(), "getPlayerCount", "Players", "The number of players")
+registerStat(resource, "getPlayerCount", "Players", "The number of players")
+function getPlayerCount()
+	return #getElementsByType("player")
+end
 
-call(getThisResource(), "registerStat", getThisResource(), "getBlipCount", "Blips", "The number of blips")
+registerStat(resource, "getBlipCount", "Blips", "The number of blips")
 function getBlipCount()
-	return #getElementsByType("blip");
+	return #getElementsByType("blip")
 end
 
-call(getThisResource(), "registerStat", getThisResource(), "getMarkerCount", "Markers", "The number of markers")
+registerStat(resource, "getMarkerCount", "Markers", "The number of markers")
 function getMarkerCount()
-	return #getElementsByType("marker");
+	return #getElementsByType("marker")
 end
 
-call(getThisResource(), "registerStat", getThisResource(), "getPickupCount", "Pickups", "The number of pickups")
+registerStat(resource, "getPickupCount", "Pickups", "The number of pickups")
 function getPickupCount()
-	return #getElementsByType("pickup");
+	return #getElementsByType("pickup")
 end
 
-publichatcount = 0
-teamchatcount = 0
-totalchatcount = 0
-addEventHandler ( "onPlayerChat",  getRootElement(),
-	function(message,messageType)
-		if messageType == 0 or messageType == 1 then
-			publichatcount = publichatcount + 1
-		else
-			teamchatcount = teamchatcount + 1
-		end
-		totalchatcount = totalchatcount + 1
+registerStat(resource, "getPublicChatCount", "Public Chat Count", "The number of lines of public chat")
+addEventHandler("onPlayerChat", root, function (message, messageType)
+	if (messageType == 0) or (messageType == 1) then
+		publichatcount = publichatcount + 1
+	else
+		teamchatcount = teamchatcount + 1
 	end
-)
+	totalchatcount = totalchatcount + 1
+end)
 
-call(getThisResource(), "registerStat", getThisResource(), "getPublicChatCount", "Public Chat Count", "The number of lines of public chat")
 function getPublicChatCount()
-	local ret = publichatcount;
-	publichatcount = 0;
-	return ret;
+	local ret = publichatcount
+	publichatcount = 0
+	return ret
 end
 
-call(getThisResource(), "registerStat", getThisResource(), "getTeamChatCount", "Team Chat Count", "The number of lines of team chat")
+registerStat(resource, "getTeamChatCount", "Team Chat Count", "The number of lines of team chat")
 function getTeamChatCount()
-	local ret = teamchatcount;
-	teamchatcount = 0;
-	return ret;
+	local ret = teamchatcount
+	teamchatcount = 0
+	return ret
 end
 
-call(getThisResource(), "registerStat", getThisResource(), "getChatCount", "Chat Count", "The number of lines of chat")
+registerStat(resource, "getChatCount", "Chat Count", "The number of lines of chat")
 function getChatCount()
-	local ret = totalchatcount;
-	totalchatcount = 0;
-	return ret;
+	local ret = totalchatcount
+	totalchatcount = 0
+	return ret
 end
 
-call(getThisResource(), "registerStat", getThisResource(), "getDamageCount", "Damage Given", "The amount of damage players have taken")
-damagecount = 0
-addEventHandler ( "onPlayerDamage",  getRootElement(),
-	function( attacker, attackerweapon, bodypart, loss )
-		damagecount = damagecount + loss
-	end
-)
+registerStat(resource, "getDamageCount", "Damage Given", "The amount of damage players have taken")
+addEventHandler("onPlayerDamage", root, function (_, _, _, loss)
+	damagecount = damagecount + loss
+end)
 
 function getDamageCount()
-	local ret = damagecount;
-	damagecount = 0;
-	return ret;
+	local ret = damagecount
+	damagecount = 0
+	return ret
 end
 
-call(getThisResource(), "registerStat", getThisResource(), "getVehiclesWastedCount", "Vehicles Wasted", "The number of vehicles destroyed")
-vehicleswasted = 0
-addEventHandler ( "onVehicleExplode",  getRootElement(),
-	function(  )
-		vehicleswasted = vehicleswasted + 1
-	end
-)
+registerStat(resource, "getVehiclesWastedCount", "Vehicles Wasted", "The number of vehicles destroyed")
+addEventHandler("onVehicleExplode", root, function ()
+	vehicleswasted = vehicleswasted + 1
+end)
 
 function getVehiclesWastedCount()
-	local ret = vehicleswasted;
-	vehicleswasted = 0;
-	return ret;
+	local ret = vehicleswasted
+	vehicleswasted = 0
+	return ret
 end
