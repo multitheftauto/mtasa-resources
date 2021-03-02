multitheftauto/mtasa-resources

commit eef212edcc6596c3e5300d9aa2f00b41dd6ffda1

admin: fix output message on actions (#285)
admin: fix output message on actions

[admin]/admin/server/admin_server.lua
@@ -522,13 +522,16 @@ addCommandHandler ( "admin", aAdminMenu )
 
 function aAction ( type, action, admin, player, data, more )
 	if ( aLogMessages[type] ) then
+		local function aEscapeNickname( name )
+			return string.gsub( name, "%%", "%%%%" )
+		end
 		function aStripString ( string )
 			string = tostring ( string )
-			string = string.gsub ( string, "$admin", getPlayerName ( admin ) )
-			string = string.gsub ( string, "$by_admin_4all", isAnonAdmin4All( admin )    and "" or " by " .. getPlayerName ( admin ) )
-			string = string.gsub ( string, "$by_admin_4plr", isAnonAdmin4Victim( admin ) and "" or " by " .. getPlayerName ( admin ) )
+			string = string.gsub ( string, "$admin", aEscapeNickname( getPlayerName ( admin ) ) )
+			string = string.gsub ( string, "$by_admin_4all", isAnonAdmin4All( admin )    and "" or " by " .. aEscapeNickname( getPlayerName ( admin ) ) )
+			string = string.gsub ( string, "$by_admin_4plr", isAnonAdmin4Victim( admin ) and "" or " by " .. aEscapeNickname( getPlayerName ( admin ) ) )
 			string = string.gsub ( string, "$data2", more or "" )
-			if ( player ) then string = string.gsub ( string, "$player", getPlayerName ( player ) ) end
+			if ( player ) then string = string.gsub ( string, "$player", aEscapeNickname( getPlayerName ( player ) ) ) end
 			return tostring ( string.gsub ( string, "$data", data or "" ) )
 		end
 		local node = aLogMessages[type][action]

commit bb1428593a102254c0ce563ffdd74e3c16bb2f0e

Fixed bugs introduced by eef212e (admin: fix output message on actions)

[admin]/admin/server/admin_server.lua
@@ -521,28 +521,38 @@ end
 addCommandHandler ( "admin", aAdminMenu )
 
 function aAction ( type, action, admin, player, data, more )
-	if ( aLogMessages[type] ) then
-		local function aEscapeNickname( name )
-			return string.gsub( name, "%%", "%%%%" )
-		end
-		function aStripString ( string )
-			string = tostring ( string )
-			string = string.gsub ( string, "$admin", aEscapeNickname( getPlayerName ( admin ) ) )
-			string = string.gsub ( string, "$by_admin_4all", isAnonAdmin4All( admin )    and "" or " by " .. aEscapeNickname( getPlayerName ( admin ) ) )
-			string = string.gsub ( string, "$by_admin_4plr", isAnonAdmin4Victim( admin ) and "" or " by " .. aEscapeNickname( getPlayerName ( admin ) ) )
-			string = string.gsub ( string, "$data2", more or "" )
-			if ( player ) then string = string.gsub ( string, "$player", aEscapeNickname( getPlayerName ( player ) ) ) end
-			return tostring ( string.gsub ( string, "$data", data or "" ) )
-		end
-		local node = aLogMessages[type][action]
-		if ( node ) then
-			local r, g, b = node["r"], node["g"], node["b"]
-			if ( node["all"] ) then outputChatBox ( aStripString ( node["all"] ), _root, r, g, b ) end
-			if ( node["admin"] ) and ( admin ~= player ) then outputChatBox ( aStripString ( node["admin"] ), admin, r, g, b ) end
-			if ( node["player"] ) then outputChatBox ( aStripString ( node["player"] ), player, r, g, b ) end
-			if ( node["log"] ) then outputServerLog ( aStripString ( node["log"] ) ) end
-		end
-	end
+    if ( aLogMessages[type] ) then
+
+        local function aEscapeNickname( name )
+            return string.gsub( name, "%%", "%%%%" )
+        end
+
+        local function aStripString ( string )
+            local adminName = aEscapeNickname( getPlayerName ( admin ) )
+            string = tostring ( string )
+            string = string.gsub ( string, "$admin",  adminName)
+            string = string.gsub ( string, "$by_admin_4all", isAnonAdmin4All( admin )    and "" or " by " .. adminName )
+            string = string.gsub ( string, "$by_admin_4plr", isAnonAdmin4Victim( admin ) and "" or " by " .. adminName )
+            string = string.gsub ( string, "$data2", more or "" )
+
+            if ( player ) then 
+                local playerName = aEscapeNickname( getPlayerName( player ) )
+                string = string.gsub ( string, "$player", playerName) 
+            end
+
+            return tostring ( string.gsub ( string, "$data", data or "" ) )
+        end
+
+        local node = aLogMessages[type][action]
+
+        if ( node ) then
+            local r, g, b = node["r"], node["g"], node["b"]
+            if ( node["all"] ) then outputChatBox ( aStripString ( node["all"] ), _root, r, g, b ) end
+            if ( node["admin"] ) and ( admin ~= player ) then outputChatBox ( aStripString ( node["admin"] ), admin, r, g, b ) end
+            if ( node["player"] ) then outputChatBox ( aStripString ( node["player"] ), player, r, g, b ) end
+            if ( node["log"] ) then outputServerLog ( aStripString ( node["log"] ) ) end
+        end
+    end
 end
 
 -- Should admin name be hidden from public chatbox message?