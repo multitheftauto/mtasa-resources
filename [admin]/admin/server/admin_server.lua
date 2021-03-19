519cd11ec181e9b4dc573f285bbb83d0b8355f56
Ricky Claven <ricksterhd123@gmail.com>
2021-03-19 18:25:00 +0000

    admin: fix slap error #302 (#303)
    * Fix #302
    
    * Fix output default valued slaps

--- [admin]/admin/server/admin_server.lua
+++ [admin]/admin/server/admin_server.lua
@@ -1129,11 +1129,11 @@ addEventHandler ( "aPlayer", _root, function ( player, action, data, additional,
 				if ( slap >= 0 ) then
 					if ( slap > 0 ) then
 						if ( slap > getElementHealth ( player ) ) then setTimer ( killPed, 50, 1, player )
-						else setElementHealth ( player, getElementHealth ( player ) - data ) end
+						else setElementHealth ( player, getElementHealth ( player ) - slap ) end
 					end
 					local x, y, z = getElementVelocity ( player )
 					setElementVelocity ( player, x , y, z + 0.2 )
-					mdata = data
+					mdata = slap
 				else
 					action = nil
 				end
