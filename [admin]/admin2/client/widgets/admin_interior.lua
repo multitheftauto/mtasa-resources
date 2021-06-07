4ce2de49f8efb92a8972d738898edbfa2386db98
iDannz <bdls1704@hotmail.com>
2021-06-07 17:42:42 -0300

    admin2: add filter to some lists (#324)
    * add filter to "give weapon"
    
    * add filter to "give vehicle"
    
    * add filter to "set skin"
    
    * add filter to "define interior" and refactor to match the style of the code with other scripts

--- [admin]/admin2/client/widgets/admin_interior.lua
+++ [admin]/admin2/client/widgets/admin_interior.lua
@@ -7,84 +7,118 @@
 *	Original File by lil_Toady
 *
 **************************************]]
-aInteriorForm = nil
+aInterior = {
+    Form = nil,
+    interiors = {}
+}
 
-function aPlayerInterior(player)
-    if (aInteriorForm == nil) then
+function aInterior.Show(player)
+    if (aInterior.Form == nil) then
         local x, y = guiGetScreenSize()
-        aInteriorForm = guiCreateWindow(x / 2 - 110, y / 2 - 150, 220, 300, "Player Interior Management", false)
-        aInteriorList = guiCreateGridList(0.03, 0.08, 0.94, 0.73, true, aInteriorForm)
-        guiGridListAddColumn(aInteriorList, "World", 0.2)
-        guiGridListAddColumn(aInteriorList, "Description", 0.75)
-        aInteriorSelect = guiCreateButton(0.03, 0.82, 0.94, 0.075, "Select", true, aInteriorForm)
-        aInteriorCancel = guiCreateButton(0.03, 0.90, 0.94, 0.075, "Cancel", true, aInteriorForm)
-
-        local node = xmlLoadFile("conf\\interiors.xml")
-        if (node) then
-            local interiors = 0
-            while (xmlFindChild(node, "interior", interiors) ~= false) do
-                local interior = xmlFindChild(node, "interior", interiors)
-                local row = guiGridListAddRow(aInteriorList)
-                guiGridListSetItemText(aInteriorList, row, 1, xmlNodeGetAttribute(interior, "world"), false, true)
-                guiGridListSetItemText(aInteriorList, row, 2, xmlNodeGetAttribute(interior, "id"), false, false)
-                interiors = interiors + 1
-            end
-        end
-        addEventHandler("onClientGUIDoubleClick", aInteriorForm, aClientInteriorDoubleClick)
-        addEventHandler("onClientGUIClick", aInteriorForm, aClientInteriorClick)
+        aInterior.Form = guiCreateWindow(x / 2 - 110, y / 2 - 150, 220, 300, "Player Interior Management", false)
+        guiSetAlpha(aInterior.Form, 1)
+        guiSetProperty(aInterior.Form, 'AlwaysOnTop', 'True')   
+        aInterior.List = guiCreateGridList(0.03, 0.17, 0.94, 0.65, true, aInterior.Form)
+        guiGridListAddColumn(aInterior.List, "World", 0.2)
+        guiGridListAddColumn(aInterior.List, "Description", 0.75)
+        aInterior.Edit = guiCreateEdit(0.03, 0.08, 0.94, 0.08, '', true, aInterior.Form)
+        aInterior.Select = guiCreateButton(0.03, 0.82, 0.94, 0.075, "Select", true, aInterior.Form)
+        aInterior.Cancel = guiCreateButton(0.03, 0.90, 0.94, 0.075, "Cancel", true, aInterior.Form)
+        
+        aInterior.Load()
+        aInterior.Refresh()
+        
+        addEventHandler("onClientGUIDoubleClick", aInterior.Form, aInterior.onDoubleClick)
+        addEventHandler("onClientGUIClick", aInterior.Form, aInterior.onClick)
+        addEventHandler("onClientGUIChanged", aInterior.Form, aInterior.onGUIChange)
+        
         --Register With Admin Form
-        aRegister("PlayerInterior", aInteriorForm, aPlayerInterior, aPlayerInteriorClose)
+        aRegister("PlayerInterior", aInterior.Form, aInterior.Show, aInterior.Close)
     end
-    aInteriorSelectPointer = player
-    guiSetVisible(aInteriorForm, true)
-    guiBringToFront(aInteriorForm)
+    aInterior.SelectPointer = player
+    guiSetVisible(aInterior.Form, true)
+    guiBringToFront(aInterior.Form)
 end
 
-function aPlayerInteriorClose(destroy)
-    if ((destroy) or (guiCheckBoxGetSelected(aPerformanceInterior))) then
-        if (aInteriorForm) then
-            removeEventHandler("onClientGUIDoubleClick", aInteriorForm, aClientInteriorDoubleClick)
-            removeEventHandler("onClientGUIClick", aInteriorForm, aClientInteriorClick)
-            destroyElement(aInteriorForm)
-            aInteriorForm = nil
+function aInterior.Close(destroy)
+    if (destroy) then
+        aInterior.interiors = {}
+        if (aInterior.Form) then
+            removeEventHandler("onClientGUIDoubleClick", aInterior.Form, aInterior.onDoubleClick)
+            removeEventHandler("onClientGUIClick", aInterior.Form, aInterior.onClick)
+            destroyElement(aInterior.Form)
+            aInterior.Form = nil
         end
     else
-        guiSetVisible(aInteriorForm, false)
+        guiSetVisible(aInterior.Form, false)
     end
 end
 
-function aClientInteriorDoubleClick(button)
+function aInterior.onDoubleClick(button)
     if (button == "left") then
-        if (source == aInteriorList) then
-            if (guiGridListGetSelectedItem(aInteriorList) ~= -1) then
+        if (source == aInterior.List) then
+            if (guiGridListGetSelectedItem(aInterior.List) ~= -1) then
                 triggerServerEvent(
                     "aPlayer",
                     getLocalPlayer(),
-                    aInteriorSelectPointer,
+                    aInterior.SelectPointer,
                     "setinterior",
-                    guiGridListGetItemText(aInteriorList, guiGridListGetSelectedItem(aInteriorList), 2)
+                    guiGridListGetItemText(aInterior.List, guiGridListGetSelectedItem(aInterior.List), 2)
                 )
-                aPlayerInteriorClose(false)
+                aInterior.Close(false)
             end
         end
     end
 end
 
-function aClientInteriorClick(button)
+function aInterior.onGUIChange()
+    aInterior.Refresh()
+end
+
+function aInterior.onClick(button)
     if (button == "left") then
-        if (source == aInteriorSelect) then
-            if (guiGridListGetSelectedItem(aInteriorList) ~= -1) then
+        if (source == aInterior.Select) then
+            if (guiGridListGetSelectedItem(aInterior.List) ~= -1) then
                 triggerServerEvent(
                     "aPlayer",
                     getLocalPlayer(),
-                    aInteriorSelectPointer,
+                    aInterior.SelectPointer,
                     "setinterior",
-                    guiGridListGetItemText(aInteriorList, guiGridListGetSelectedItem(aInteriorList), 2)
+                    guiGridListGetItemText(aInterior.List, guiGridListGetSelectedItem(aInterior.List), 2)
                 )
-                guiSetVisible(aInteriorForm, false)
+                guiSetVisible(aInterior.Form, false)
             end
-        elseif (source == aInteriorCancel) then
-            aPlayerInteriorClose(false)
+        elseif (source == aInterior.Cancel) then
+            aInterior.Close(false)
+        end
+    end
+end
+
+function aInterior.Load()
+    local table = {}
+    local node = xmlLoadFile("conf\\interiors.xml")
+    if (node) then
+        local interiors = 0
+        while (xmlFindChild(node, "interior", interiors) ~= false) do
+            local interior = xmlFindChild(node, "interior", interiors)
+            local id = #table + 1
+            table[id] = {}
+            table[id]["id"] = xmlNodeGetAttribute(interior, "id")
+            table[id]["world"] = xmlNodeGetAttribute(interior, "world")
+            interiors = interiors + 1
+        end
+    end
+    aInterior.interiors = table
+end
+
+function aInterior.Refresh()
+    local filter = guiGetText(aInterior.Edit):lower()
+    guiGridListClear(aInterior.List)
+    for k, v in ipairs(aInterior.interiors) do
+        if v.world:find(filter) or v.id:lower():find(filter) then
+            local row = guiGridListAddRow(aInterior.List)
+            guiGridListSetItemText(aInterior.List, row, 1, v.world, false, true)
+            guiGridListSetItemText(aInterior.List, row, 2, v.id, false, false)
         end
     end
 end
