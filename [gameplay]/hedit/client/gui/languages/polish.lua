703da786849e553e9fe23eb8193860669b01a7e7
Inder00 <inder00@wp.pl>
2021-10-31 01:06:27 +0200

    Update polish.lua

--- [gameplay]/hedit/client/gui/languages/polish.lua
+++ [gameplay]/hedit/client/gui/languages/polish.lua
@@ -36,7 +36,7 @@ guiLanguage.polish = {
 
     successSave = "Pomyslnie zapisano twoje ustawienia",
     successLoad = "Pomyslnie wczytano twoje ustawienia!",
-    sucessDelete = "",
+    sucessDelete = "Pomyślnie usunięto Twoje ustawienia!",
     
     wantTheSettings = "Czy na pewno chcesz zastosowac te ustawienia? Edytor uruchomi sie ponownie.",
 
