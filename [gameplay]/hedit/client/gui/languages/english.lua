multitheftauto/mtasa-resources

commit 0835ecd1f77fe98b1dcc84130d068f8fadb6af0d

hedit: add button to delete handlings (#360)
* Make grid a little smaller to add delete button and shift other buttons slightly upwards

* Add message for successful delete (only english)

* Create function to delete node from save file

* Correct the debug messages

* Add Arabic translation by [SW]Falcon

* Add Turkish translation by ᴏᴠ | Gürkan

* Update polish.lua

* Added Russian translation by Google Translate

* Add Spanish translation by Fantome

* Refactor variable

* Update russian.lua

* ru fix typo

* Revert "Add Spanish translation by Fantome"

This reverts commit 76388a65c3dabfd9e9a48a2b8a14ca467e9431d7.

* Revert "Added Russian translation by Google Translate"

This reverts commit e5ec614b0c0d11567ac56bb1a632b6373b631444.

* Revert "Update polish.lua"

This reverts commit 703da786849e553e9fe23eb8193860669b01a7e7.

* Revert "Add Turkish translation by ᴏᴠ | Gürkan"

This reverts commit 3653216f1bb0d715471e011820d8c0fd205564f3.

* Revert "Add Arabic translation by [SW]Falcon"

This reverts commit a318b2b562b29057e38fa922ac7843ed4f1b082a.

* Revert "Update russian.lua"

This reverts commit 62919c98fd7a979d2f99c74b1441b07f368f768e.

Co-authored-by: Inder00 <inder00@wp.pl>
Co-authored-by: Konstantin <apnkel@gmail.com>

[gameplay]/hedit/client/gui/languages/english.lua
@@ -36,6 +36,7 @@ guiLanguage.english = {
 
     successSave = "Successfully saved your handling settings!",
     successLoad = "Successfully loaded your handling settings!",
+    sucessDelete = "Successfully deleted your handling settings!",
 
     wantTheSettings = "Are you sure you would like to apply these settings? The handling editor will restart.",
 
@@ -154,6 +155,7 @@ guiLanguage.english = {
                 descriptionLabel = "Description",
                 saveButton = "Save",
                 loadButton = "Load",
+                deleteButton = "Delete",
                 grid = "",
                 nameEdit = "",
                 descriptionEdit = ""

[gameplay]/hedit/client/gui/languages/polish.lua
@@ -36,7 +36,8 @@ guiLanguage.polish = {
 
     successSave = "Pomyslnie zapisano twoje ustawienia",
     successLoad = "Pomyslnie wczytano twoje ustawienia!",
-
+    sucessDelete = "",
+    
     wantTheSettings = "Czy na pewno chcesz zastosowac te ustawienia? Edytor uruchomi sie ponownie.",
 
     vehicle = "Pojazd",

[gameplay]/hedit/client/gui/languages/russian.lua
@@ -36,6 +36,7 @@
 
     successSave = "Вы успешно сохранили ваш хэндлинг!",
     successLoad = "Вы успешно загрузили ваш хэндлинг!",
+    sucessDelete = "",
 
     wantTheSettings = "Вы уверены, что хотите применить эти настройки? Редактор хэндлинга будет перезагружен.",
 

[gameplay]/hedit/client/gui/languages/spanish.lua
@@ -40,6 +40,7 @@
 
     successSave = "Se guardo el handling con exito!",
     successLoad = "Se cargo el handling con exito!",
+    sucessDelete = "",
 
 	confirmVersionReset = "Estas seguro de que deseas establecer la version del editor a la de este servidor? Tus handlings guardados pueden llegar a ser incompatibles.",
 	successVersionReset = "La version de editor se ha actualizado.",

[gameplay]/hedit/client/gui/languages/turkish.lua
@@ -36,6 +36,7 @@ guiLanguage.turkish = {
 
     successSave = "Başarıyla işleme ayarlarını kurtardı!",
     successLoad = "Başarıyla senin kullanımı ayarlarını yüklenen!",
+    sucessDelete = "",
 
     wantTheSettings = "Bu ayarları uygulamak istiyorum emin misiniz? Taşıma editörü yeniden başlar.",
 

[gameplay]/hedit/client/gui/templates/default.lua
@@ -658,7 +658,7 @@ template.viewcontents = {
             grid = {
                 type = "gridlist",
                 pos = { 72, 83 },
-                size = { 285, 246 },
+                size = { 285, 220 },
                 runfunction = function ( this )
                     guiGridListAddColumn ( this, "Name",  0.5 )
                     guiGridListAddColumn ( this, "Model", 0.4 )
@@ -782,7 +782,7 @@ template.viewcontents = {
             },
             saveButton = {
                 type = "button",
-                pos = { 289, 334 },
+                pos = { 289, 309 },
                 size = { 68, 25 },
                 events = {
                     onClick = function ( this )
@@ -813,7 +813,7 @@ template.viewcontents = {
 
             loadButton = {
                 type = "button",
-                pos = { 289, 359 },
+                pos = { 289, 334 },
                 size = { 68, 25 },
                 events = {
                     onClick = function ( this )
@@ -835,6 +835,31 @@ template.viewcontents = {
                             end
 
                             func ( )
+                        end
+                    end
+                }
+            },
+
+            deleteButton = {
+                type = "button",
+                pos = { 289, 359 },
+                size = { 68, 25 },
+                events = {
+                    onClick = function ( this )
+                        local content = heditGUI.viewItems.save.guiItems
+                        local row,col = guiGridListGetSelectedItem ( content.grid )
+
+                        if row ~= -1 and col ~= -1 then
+                            local name = string.lower ( guiGridListGetItemText ( content.grid, row, col ) )
+
+                            local function func ( )
+                                if deleteClientHandling ( pVehicle, name ) then
+                                    guiCreateWarningMessage ( getText ( "sucessDelete" ), 3 )
+                                end
+                            end
+
+                            guiCreateWarningMessage ( getText ( "confirmDelete" ), 2, {func} )
+                            return true
                         end
                     end
                 }

[gameplay]/hedit/client/gui/languages/arabic.lua
@@ -36,6 +36,7 @@ guiLanguage.arabic = {
 
     successSave = "تم حفظ اعدادات وزانيتك بنجاح",
     successLoad = "تم تحميل اعدادات وزانيتك بنجاح",
+    successDelete = "",
 
     wantTheSettings = "هل انت متاكد من انك تريد تطبيق هذه الاعدادات ? معدل وزانيات سيتم اعادة تشغيله",
 

[gameplay]/hedit/client/utils.lua
@@ -307,6 +307,33 @@ function loadClientHandling ( vehicle, name )
     return true
 end
 
+function deleteClientHandling ( vehicle, name )
+    if not isValidVehicle ( vehicle ) then
+        if DEBUGMODE then
+            error ( "Invalid vehicle element at 'deleteClientHandling'! ["..tostring(vehicle).."]", 2 )
+        end
+
+        return false
+    end
+
+    if not isClientHandlingExisting ( name ) then
+        if DEBUGMODE then
+            error ( "Handling name given at 'deleteClientHandling' does not exist! ["..tostring(name).."]", 2 )
+        end
+
+        return false
+    end
+
+    name = string.lower ( name )
+    local handling = xmlCache.clientsaves[name].saveNode
+
+    xmlDestroyNode ( handling )
+    xmlSaveFile ( xmlFile[client_handling_file] )
+
+    xmlCache.clientsaves[name] = nil
+
+    return true
+end
 
 -- Imports a handling line in handling.cfg format, given a proper method.
 -- Valid methods: III, VC, SA, and IV

commit 6509b7424c3c2d6b38054e86904cc5b9a9c7fa78

hedit: Fixed typo's introduced in PR #360
Note that translation lines from said PR (and also PR #369 as of speaking) use a mix of "success" and "sucess", so it wouldn't work

[gameplay]/hedit/client/gui/languages/english.lua
@@ -36,7 +36,7 @@ guiLanguage.english = {
 
     successSave = "Successfully saved your handling settings!",
     successLoad = "Successfully loaded your handling settings!",
-    sucessDelete = "Successfully deleted your handling settings!",
+    successDelete = "Successfully deleted your handling settings!",
 
     wantTheSettings = "Are you sure you would like to apply these settings? The handling editor will restart.",
 

[gameplay]/hedit/client/gui/languages/polish.lua
@@ -36,7 +36,7 @@ guiLanguage.polish = {
 
     successSave = "Pomyslnie zapisano twoje ustawienia",
     successLoad = "Pomyslnie wczytano twoje ustawienia!",
-    sucessDelete = "",
+    successDelete = "",
     
     wantTheSettings = "Czy na pewno chcesz zastosowac te ustawienia? Edytor uruchomi sie ponownie.",
 

[gameplay]/hedit/client/gui/languages/russian.lua
@@ -36,7 +36,7 @@
 
     successSave = "Вы успешно сохранили ваш хэндлинг!",
     successLoad = "Вы успешно загрузили ваш хэндлинг!",
-    sucessDelete = "",
+    successDelete = "",
 
     wantTheSettings = "Вы уверены, что хотите применить эти настройки? Редактор хэндлинга будет перезагружен.",
 

[gameplay]/hedit/client/gui/languages/spanish.lua
@@ -40,7 +40,7 @@
 
     successSave = "Se guardo el handling con exito!",
     successLoad = "Se cargo el handling con exito!",
-    sucessDelete = "",
+    successDelete = "",
 
 	confirmVersionReset = "Estas seguro de que deseas establecer la version del editor a la de este servidor? Tus handlings guardados pueden llegar a ser incompatibles.",
 	successVersionReset = "La version de editor se ha actualizado.",

[gameplay]/hedit/client/gui/languages/turkish.lua
@@ -36,7 +36,7 @@ guiLanguage.turkish = {
 
     successSave = "Başarıyla işleme ayarlarını kurtardı!",
     successLoad = "Başarıyla senin kullanımı ayarlarını yüklenen!",
-    sucessDelete = "",
+    successDelete = "",
 
     wantTheSettings = "Bu ayarları uygulamak istiyorum emin misiniz? Taşıma editörü yeniden başlar.",
 

[gameplay]/hedit/client/gui/templates/default.lua
@@ -854,7 +854,7 @@ template.viewcontents = {
 
                             local function func ( )
                                 if deleteClientHandling ( pVehicle, name ) then
-                                    guiCreateWarningMessage ( getText ( "sucessDelete" ), 3 )
+                                    guiCreateWarningMessage ( getText ( "successDelete" ), 3 )
                                 end
                             end
 