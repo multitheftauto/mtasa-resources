1049ff64452834cf4307f80e5aab016d1145bf0b
srslyyyy <51768772+srslyyyy@users.noreply.github.com>
2022-04-18 04:02:11 +0200

    Update mapEditorScriptingExtension_s.lua

--- [editor]/editor_main/server/mapEditorScriptingExtension_s.lua
+++ [editor]/editor_main/server/mapEditorScriptingExtension_s.lua
@@ -1,6 +1,6 @@
--- FILE: 	mapEditorScriptingExtension_s.lua
--- PURPOSE:	Prevent the map editor feature set being limited by what MTA can load from a map file by adding a script file to maps
--- VERSION:	RemoveWorldObjects (v1) AutoLOD (v1)
+-- FILE: mapEditorScriptingExtension_s.lua
+-- PURPOSE: Prevent the map editor feature set being limited by what MTA can load from a map file by adding a script file to maps
+-- VERSION: RemoveWorldObjects (v1) AutoLOD (v1)
 
 local usedLODModels = {}
 local LOD_MAP = {}
