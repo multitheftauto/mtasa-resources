782db1c6f7062a547115c03710f5e03e7d5c5ef4
lopezloo <lopez@multitheftauto.pl>
2022-06-28 00:53:25 +0200

    Add frameratefixes resource

---
+++ [gameplay]/frameratefixes/client.lua
@@ -0,0 +1,13 @@
+-- Fix for "Climbing over certain objects kills you, when you have high FPS"
+-- https://github.com/multitheftauto/mtasa-blue/issues/602
+addEventHandler("onClientPlayerDamage", localPlayer,
+	function(attacker, weapon, bodypart)
+		if not attacker and weapon == 54 and bodypart == 3 then
+			local task = {}
+			task[1], task[2], task[3] = getPedTask(localPlayer, "primary", 3)
+			if task[1] == "TASK_COMPLEX_JUMP" and task[2] == "TASK_COMPLEX_IN_AIR_AND_LAND" and task[3] == "TASK_SIMPLE_CLIMB" then
+				cancelEvent()
+			end
+		end
+	end
+)
