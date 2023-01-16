15fbc95f61f493e96a927f8f4fa483f25be2cd13
BerserkOrchestra <30158626+Haxardous@users.noreply.github.com>
2023-01-16 17:12:20 +0000

    Add a new "restore" button within Editor's "Current Elements" window. (#401)
    * Add a new "restore" button & fix current elements window not refreshing.
    
    * Fixed current browser's buttons position/sizing when resizing the window.
    
    Co-Authored-By: Fernando Rocha <34967844+Fernando-A-Rocha@users.noreply.github.com>
    
    * lint fix(?)
    
    * link fix (2?)
    
    Co-authored-by: Fernando Rocha <34967844+Fernando-A-Rocha@users.noreply.github.com>

--- [editor]/editor_main/client/main.lua
+++ [editor]/editor_main/client/main.lua
@@ -734,7 +734,7 @@ function disableGameHUD()
 end
 
 -- PUBLIC
-function selectElement(element, submode, shortcut, dropreleaseLock, dropclonedrop)
+function selectElement(element, submode, shortcut, dropreleaseLock, dropclonedrop, ignoreProperties)
 	local openProperties
 	submode = submode or g_submode
 
@@ -815,8 +815,10 @@ function selectElement(element, submode, shortcut, dropreleaseLock, dropclonedro
 			createArrowMarker(handle)
 		end
 	else
-		editor_gui.openPropertiesBox( element, false, shortcut )
-		openProperties = true
+		if not ignoreProperties then
+			editor_gui.openPropertiesBox( element, false, shortcut )
+			openProperties = true
+		end
 	end
 
 	triggerServerEvent("doLockElement", element)
@@ -967,7 +969,7 @@ end
 
 -- sets the maximum distance at which an element can be selected
 function setMaxSelectDistance(distance)
-	assert((distance >= 0), "Distance must be a positive number")
+	assert((distance >= 0), "Distance must be a positive number.")
 	g_maxSelectDistance = distance
 	return true
 end
@@ -988,7 +990,10 @@ function getMaxSelectDistance()
 	return g_maxSelectDistance
 end
 
-function destroySelectedElement()
+function destroySelectedElement(key)
+	if key then return
+		editor_gui.restoreSelectedElement()
+	 end
 	if g_selectedElement then
 		local element = g_selectedElement
 		dropElement(false)
