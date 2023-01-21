999c1ca26b20ea98adcd46d1bcdd935799815394
Fernando-A-Rocha <frocha2001@gmail.com>
2023-01-21 21:27:01 +0000

    Revert "ammend cffa943f4e92eb0e300d140d9df4b3323f3c091f"
    This reverts commit aeb79eb738cc360c86639b12af49a716a07f1ec0.

--- [admin]/admin/server/admin_ip2c.lua
+++
@@ -1,9 +0,0 @@
---[[
-    ip2c script has been moved to its own separate resource.
-    This exported function remains for backwards compatibility, in case any custom resources rely on it.
-]]
-function getPlayerCountry(player)
-    outputDebugString("exports.admin:getPlayerCountry(player) is deprecated. Get & use the new ip2c resource.", 2)
-    outputDebugString("  https://mirror-cdn.multitheftauto.com/mtasa/resources/mtasa-resources-latest.zip", 2)
-    return false
-end
