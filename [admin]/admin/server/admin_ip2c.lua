3fc924d4424bd587df368e7fac0b1af95e5e8e7c
Fernando-A-Rocha <frocha2001@gmail.com>
2023-01-23 17:18:45 +0000

    admin getPlayerCountry calls ip2c if it actually exists

--- [admin]/admin/server/admin_ip2c.lua
+++ [admin]/admin/server/admin_ip2c.lua
@@ -9,5 +9,8 @@ function getPlayerCountry(player)
     outputServerLog("  This function has been moved to the new ip2c standalone resource")
     outputServerLog("  Get it from the latest MTA:SA default resources package:")
     outputServerLog("  https://mirror-cdn.multitheftauto.com/mtasa/resources/mtasa-resources-latest.zip", 2)
-    return false
+
+    local isIP2CResourceRunning = getResourceFromName( "ip2c" )
+	isIP2CResourceRunning = isIP2CResourceRunning and getResourceState( isIP2CResourceRunning ) == "running"
+    return (isIP2CResourceRunning and exports.ip2c:getPlayerCountry(player)) or false
 end
