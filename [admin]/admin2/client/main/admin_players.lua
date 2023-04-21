0b81c8f9ebfccf41995872fcb49b7220d18de2fd
Dark-Dragon <Dark.Dragon@gmx.de>
2023-04-22 01:08:51 +0200

    admin2: forward-ported permissions widget
    initial commit to forward-port admin1's permission widget to admin2

--- [admin]/admin2/client/main/admin_players.lua
+++ [admin]/admin2/client/main/admin_players.lua
@@ -74,7 +74,7 @@ function aPlayersTab.Create(tab)
     aPlayersTab.Shout = guiCreateButton(0.74, 0.19, 0.12, 0.04, "Shout", true, tab, "shout")
     aPlayersTab.Spectate = guiCreateButton(0.87, 0.19, 0.12, 0.04, "Spectate", true, tab, "spectate")
     aPlayersTab.SetNick = guiCreateButton(0.74, 0.235, 0.12, 0.04, "Set nick", true, tab, "setnick")
-    aPlayersTab.Admin = guiCreateButton(0.87, 0.235, 0.12, 0.04, "Give admin", true, tab, "setgroup")
+    aPlayersTab.Permissions = guiCreateButton(0.87, 0.235, 0.12, 0.04, "Permissions", true, tab, "setgroup")
     aPlayersTab.SlapOptions = guiCreateComboBox(0.76, 0.28, 0.1, 0.04, "0", true, tab)
     local width, height = guiGetSize(aPlayersTab.SlapOptions, false)
     for i = 0, 200, 20 do
@@ -257,14 +257,11 @@ function aPlayersTab.onClientClick(button)
                     end
                 elseif (source == aPlayersTab.WarpPlayer) then
                     aPlayerWarp(player)
-                elseif (source == aPlayersTab.Admin) then
-                    if
-                        (aPlayers[player]["admin"] and
-                            messageBox("Revoke admin rights from " .. name .. "?", MB_WARNING))
-                     then
-                        triggerServerEvent("aPlayer", localPlayer, player, "setgroup", false)
-                    elseif (messageBox("Give admin rights to " .. name .. "?", MB_WARNING)) then
-                        triggerServerEvent("aPlayer", localPlayer, player, "setgroup", true)
+                elseif (source == aPlayersTab.Permissions) then
+                    if (aPlayers[player]['account'] ~= 'guest') then
+                        aPermissions.Show(player)
+                    else
+                        aMessageBox.Show("This player is not logged in!", MB_ERROR, MB_ERROR)
                     end
                 end
             end
@@ -298,7 +295,6 @@ function aPlayersTab.onClientClick(button)
                 guiSetText(aPlayersTab.Groups, "Groups: N/A")
                 guiSetText(aPlayersTab.Mute, "Mute")
                 guiSetText(aPlayersTab.Freeze, "Freeze")
-                guiSetText(aPlayersTab.Admin, "Give admin")
                 guiSetText(aPlayersTab.Health, "Health: 0%")
                 guiSetText(aPlayersTab.Armour, "Armour: 0%")
                 guiSetText(aPlayersTab.Skin, "Skin: N/A")
