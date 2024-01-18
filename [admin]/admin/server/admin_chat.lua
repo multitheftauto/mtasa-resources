92fd10e99525aab4a458bd526bbb080ca8b2fa3a
Nico <122193236+Nico8340@users.noreply.github.com>
2024-01-18 18:09:10 +0100

    Use named function for onPlayerChat

--- [admin]/admin/server/admin_chat.lua
+++ [admin]/admin/server/admin_chat.lua
@@ -32,84 +32,84 @@ addEventHandler("onPlayerQuit", root,
     end
 )
 
-addEventHandler("onPlayerChat", root,
-    function(messageContent, messageType)
-        if messageType ~= 0 then
-            return
-        end
-
-        cancelEvent()
-
-        if getConfig("antiSpamEnabled") then
-            if getConfig("antiSpamTimeout") > 0 then
-                if chatTimeouts[source] then
-                    if chatTimeouts[source] - getRealTime().timestamp <= 0 then
-                        chatTimeouts[source] = nil
-                    else
-                        outputChatBox("You need to wait " .. math.ceil(chatTimeouts[source] - getRealTime().timestamp) .. " seconds before sending an another message.", source, 255, 0, 0)
-                        return
-                    end
-                end
-            end
-
-            local lastMessage = getMessageLast(source)
-
-            if getConfig("antiSpamRepeat") then
-                if lastMessage and type(lastMessage) == "table" then
-                    local lastMessageContent = lastMessage[1]
-
-                    if lastMessageContent then
-                        local contentStart, contentStartIndex = string.find(lastMessageContent, "#ffffff")
-                        local contentTrim = string.sub(lastMessageContent, contentStartIndex + 1)
-
-                        if contentTrim == messageContent then
-                            outputChatBox("Stop repeating yourself!", source, 255, 0, 0)
-                            return
-                        end
-                    end
-                end
-            end
-
-            if lastMessage and type(lastMessage) == "table" then
-                local lastMessageTime = lastMessage[2]
-
-                if lastMessageTime then
-                    if math.abs(getRealTime().timestamp - lastMessageTime) < getConfig("antiSpamDelay") then
-                        if getConfig("antiSpamTimeout") > 0 then
-                            outputChatBox("Your message has been marked as spam. You need to wait " .. getConfig("antiSpamTimeout") .. " seconds before sending an another message.", source, 255, 0, 0)
-                            chatTimeouts[source] = getRealTime().timestamp + getConfig("antiSpamTimeout")
-                        else
-                            outputChatBox("Your message has been marked as spam.", source, 255, 0, 0)
-                        end
-
-                        return
-                    end
-                end
-            end
-        end
-
-        local playerName = getPlayerName(source)
-        local playerNameColor = {197, 232, 242}
-
-        if not getConfig("isCustomColorsEnabled") then
-            messageContent = messageContent:gsub("#%x%x%x%x%x%x", "")
-        end
-
-        if getConfig("isPlayercolorsEnabled") then
-            playerNameColor = {getPlayerNametagColor(source)}
-            playerNameColor = string.format("#%02X%02X%02X", playerNameColor[1], playerNameColor[2], playerNameColor[3])
-        else
-            playerNameColor = string.format("#%02X%02X%02X", playerNameColor[1], playerNameColor[2], playerNameColor[3])
-        end
-
-        messageContent = string.format("%s%s: #ffffff%s", playerNameColor, playerName, messageContent)
-        messageContent = messageContent:gsub("%s+", " ")
-
-        table.insert(chatMessages[source], {messageContent, getRealTime().timestamp})
-        outputChatBox(messageContent, root, 255, 255, 255, true)
-        outputServerLog(messageContent:gsub("#%x%x%x%x%x%x", ""))
+function onChatHandler(messageContent, messageType)
+    if messageType ~= 0 then
+        return
     end
-)
+
+    cancelEvent()
+
+    if getConfig("antiSpamEnabled") then
+        if getConfig("antiSpamTimeout") > 0 then
+            if chatTimeouts[source] then
+                if chatTimeouts[source] - getRealTime().timestamp <= 0 then
+                    chatTimeouts[source] = nil
+                else
+                    outputChatBox("You need to wait " .. math.ceil(chatTimeouts[source] - getRealTime().timestamp) .. " seconds before sending an another message.", source, 255, 0, 0)
+                    return
+                end
+            end
+        end
+
+        local lastMessage = getMessageLast(source)
+
+        if getConfig("antiSpamRepeat") then
+            if lastMessage and type(lastMessage) == "table" then
+                local lastMessageContent = lastMessage[1]
+
+                if lastMessageContent then
+                    local contentStart, contentStartIndex = string.find(lastMessageContent, "#ffffff")
+                    local contentTrim = string.sub(lastMessageContent, contentStartIndex + 1)
+
+                    if contentTrim == messageContent then
+                        outputChatBox("Stop repeating yourself!", source, 255, 0, 0)
+                        return
+                    end
+                end
+            end
+        end
+
+        if lastMessage and type(lastMessage) == "table" then
+            local lastMessageTime = lastMessage[2]
+
+            if lastMessageTime then
+                if math.abs(getRealTime().timestamp - lastMessageTime) < getConfig("antiSpamDelay") then
+                    if getConfig("antiSpamTimeout") > 0 then
+                        outputChatBox("Your message has been marked as spam. You need to wait " .. getConfig("antiSpamTimeout") .. " seconds before sending an another message.", source, 255, 0, 0)
+                        chatTimeouts[source] = getRealTime().timestamp + getConfig("antiSpamTimeout")
+                    else
+                        outputChatBox("Your message has been marked as spam.", source, 255, 0, 0)
+                    end
+
+                    return
+                end
+            end
+        end
+    end
+
+    local playerName = getPlayerName(source)
+    local playerNameColor = {197, 232, 242}
+
+    if not getConfig("isCustomColorsEnabled") then
+        messageContent = messageContent:gsub("#%x%x%x%x%x%x", "")
+    end
+
+    if getConfig("isPlayercolorsEnabled") then
+        playerNameColor = {getPlayerNametagColor(source)}
+        playerNameColor = string.format("#%02X%02X%02X", playerNameColor[1], playerNameColor[2], playerNameColor[3])
+    else
+        playerNameColor = string.format("#%02X%02X%02X", playerNameColor[1], playerNameColor[2], playerNameColor[3])
+    end
+
+    messageContent = string.format("%s%s: #ffffff%s", playerNameColor, playerName, messageContent)
+    messageContent = messageContent:gsub("%s+", " ")
+
+    table.insert(chatMessages[source], {messageContent, getRealTime().timestamp})
+    outputChatBox(messageContent, root, 255, 255, 255, true)
+    outputServerLog(messageContent:gsub("#%x%x%x%x%x%x", ""))
+end
+
+addEventHandler("onPlayerChat", root, onChatHandler)
 
 function getConfig(configName)
     local configData = get(configName)
