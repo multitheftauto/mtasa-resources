9e3cbb64de7c5e5f6dd7053b535ad805705749f2
Fernando-A-Rocha <frocha2001@gmail.com>
2024-06-21 01:18:06 +0100

    update comment in PLACE_BUILDINGS

--- [gameplay]/mapfixes/client.lua
+++ [gameplay]/mapfixes/client.lua
@@ -3,7 +3,7 @@
 local function loadObjects()
 
     local PLACE_BUILDINGS = {
-        -- Big Smoke Crack Den inside area
+        -- Fill the hole of Big Smoke's Crack Palace with vanilla open-world interior
         {17933, 2532.992188, -1289.789062, 39.281250, 0, 0, 0},
         {17946, 2533.820312, -1290.554688, 36.945312, 0, 0, 0},
     }
