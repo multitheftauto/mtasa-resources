afae4c59febc0be7608e809334fb94b3f7b5f6dc
Fernando-A-Rocha <frocha2001@gmail.com>
2024-06-21 16:51:07 +0100

    update map fix components table

--- [gameplay]/mapfixes/server.lua
+++ [gameplay]/mapfixes/server.lua
@@ -1,16 +1,5 @@
 -- MTA:SA Map Fixes
 
-local mapFixComponents = {
-    ["crack_palace_interior"] = {
-        spawnBuildings = {
-            -- Fill the hole of Big Smoke's Crack Palace with vanilla open-world interior
-            {17933, 2532.992188, -1289.789062, 39.281250, 0, 0, 0},
-            {17946, 2533.820312, -1290.554688, 36.945312, 0, 0, 0},
-        },
-    },
-}
-
--- Check resource settings
 for name, data in pairs(mapFixComponents) do
     data.enabled = get(name) == "true"
 end
---
+++ [gameplay]/mapfixes/server_data.lua
@@ -0,0 +1,11 @@
+-- MTA:SA Map Fixes
+
+mapFixComponents = {
+    ["crack_palace_interior"] = {
+        -- Fill the hole of Big Smoke's Crack Palace with objects from carter.ipl
+        spawnBuildings = {
+            {17933, 2532.992188, -1289.789062, 39.281250, 0, 0, 0}, -- carter-light15b
+            {17946, 2533.820312, -1290.554688, 36.945312, 0, 0, 0}, -- carter_ground
+        },
+    },
+}
