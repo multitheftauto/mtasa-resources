e1c06c3c2581c16a6e05401381263a47dd6ac5f0
ds1-e <51768772+ds1-e@users.noreply.github.com>
2024-11-24 02:44:43 +0100

    superman: more initial improvements (#577)

---
+++ [gameplay]/superman/SHandleSuperman.lua
@@ -0,0 +1,29 @@
+local function supermanCancelAirKill()
+	local supermanFlying = getSupermanData(source, SUPERMAN_FLY_DATA_KEY)
+	local supermanTakingOff = getSupermanData(source, SUPERMAN_TAKE_OFF_DATA_KEY)
+
+	if (not supermanFlying and not supermanTakingOff) then
+		return false
+	end
+
+	cancelEvent()
+end
+addEventHandler("onPlayerStealthKill", root, supermanCancelAirKill)
+
+-- Fix for players glitching other players' vehicles by warping into them while superman is active, causing them to flinch into air and get stuck.
+
+local function supermanEnterVehicle()
+	local supermanFlying = getSupermanData(source, SUPERMAN_FLY_DATA_KEY)
+	local supermanTakingOff = getSupermanData(source, SUPERMAN_TAKE_OFF_DATA_KEY)
+
+	if (not supermanFlying and not supermanTakingOff) then
+		return false
+	end
+
+	removePedFromVehicle(source)
+
+	local playerX, playerY, playerZ = getElementPosition(source)
+
+	setElementPosition(source, playerX, playerY, playerZ)
+end
+addEventHandler("onPlayerVehicleEnter", root, supermanEnterVehicle)
