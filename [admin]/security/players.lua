e75d7561f090eca1a76b2bc0bb7de9dc3f1d5081
-ffs-PLASMA <33094646+ffsPLASMA@users.noreply.github.com>
2025-02-08 01:52:39 +0100

    Add recently introduced events, name and explosion/projectile checking (#588)

--- [admin]/security/players.lua
+++ [admin]/security/players.lua
@@ -1,3 +1,14 @@
+-- used to check how many explosion/projectile sync packets a client sends overtime
+local iExplosionCheckInterval 		= 3000;	-- the interval in ms to check for players sending too many explosion and projectile sync packets
+local tblPlayerProjectiles 			= {};	-- store players sending projectile sync packets
+local tblRegularExplosions 			= {};	-- store players sending regular explosion sync packets
+local tblVehicleExplosions 			= {};	-- store players sending vehicle explosion sync packets
+local iPlayerProjectileThreshold 	= 10;	-- the threshold when we consider client suspicious for projectile creations
+local iRegularExplosionThreshold 	= 10;	-- the threshold when we consider client suspicious for regular explosions
+local iVehicleExplosionThreshold 	= 10;	-- the threshold when we consider client suspicious for vehicle explosions
+
+
+
 -- add the elementdatas you want to protect from client updates in here
 local tblProtectedElementDatas = {["Score"] = true};
 
@@ -13,6 +24,18 @@ function clientChangesElementData(strKey, varOldValue, varNewValue)
 	end
 end
 addEventHandler("onElementDataChange", root, clientChangesElementData);
+
+
+
+-- https://wiki.multitheftauto.com/wiki/OnPlayerChangesProtectedData
+-- gets triggered when a client tries to change protected element data
+-- see elementdata_whitelisted config https://wiki.multitheftauto.com/wiki/Server_mtaserver.conf#elementdata_whitelisted
+-- this needs to be setup in conjunction with your existing elementdatas to take necessary action!
+-- the key feature is to prevent the client from updating non synced server elementdatas if they know the key and attached element
+function clientChnagesProtectedData(uElement, strKey, unValue)
+	logViolation(source, "Tried to change protected elementdata for key "..tostring(strKey).." to value "..tostring(unValue).." for element "..tostring(uElement).." ("..getElementType(uElement)..")");
+end
+addEventHandler("onPlayerChangesProtectedData", root, clientChnagesProtectedData);
 
 
 
@@ -56,3 +79,79 @@ function clientNetworkStatus(iStatus, iTicks)
 	end
 end
 addEventHandler("onPlayerNetworkStatus", root, clientNetworkStatus);
+
+
+
+-- https://wiki.multitheftauto.com/wiki/OnPlayerProjectileCreation
+-- gets triggered when a player creates a projectile sync packets (eg. shoots a weapon, vehicle weapon or via createProjectile)
+function clientCreateProjectile(iWeaponType, fPX, fPY, fPZ, fForce, uTarget, fRX, fRY, fRZ, fVX, fVY, fVZ)
+	if(isElement(source)) then
+		if(tblPlayerProjectiles[source]) then
+			tblPlayerProjectiles[source] = tblPlayerProjectiles[source] + 1;
+		else
+			tblPlayerProjectiles[source] = 1;
+		end
+	end
+end
+addEventHandler("onPlayerProjectileCreation", root, clientCreateProjectile);
+
+
+
+-- https://wiki.multitheftauto.com/wiki/OnExplosion
+-- gets triggered when an explosion occurs, either via server script or client sync packet
+function clientCreateExplosion(fPX, fPY, fPZ, iType)
+	if(isElement(source)) then
+		if(getElementType(source) == "player") then
+			if(tblRegularExplosions[source]) then
+				tblRegularExplosions[source] = tblRegularExplosions[source] + 1;
+			else
+				tblRegularExplosions[source] = 1;
+			end
+		end
+	end
+end
+addEventHandler("onExplosion", root, clientCreateExplosion);
+
+
+
+-- https://wiki.multitheftauto.com/wiki/OnVehicleExplode
+-- gets triggered when a vehicle explodes, either via server script or client sync packet
+function clientCreateVehicleExplosion(bWithExplosion, uPlayer)
+	if(isElement(uPlayer)) then
+		if(tblVehicleExplosions[uPlayer]) then
+			tblVehicleExplosions[uPlayer] = tblVehicleExplosions[uPlayer] + 1;
+		else
+			tblVehicleExplosions[uPlayer] = 1;
+		end
+	end
+end
+addEventHandler("onVehicleExplode", root, clientCreateVehicleExplosion);
+
+
+
+-- setup a timer with specified interval above and check if any client sent too many sync packets in the given time
+-- thresholds need to be adjusted for your need and actions taken!
+setTimer(function()
+	for uPlayer, iCounter in pairs(tblPlayerProjectiles) do
+		if(iCounter >= iPlayerProjectileThreshold) then
+			logViolation(uPlayer, "Exceeded projectile threshold "..tostring(iPlayerProjectileThreshold).." - Count: "..tostring(iCounter));
+		end
+	end
+	
+	for uPlayer, iCounter in pairs(tblRegularExplosions) do
+		if(iCounter >= iRegularExplosionThreshold) then
+			logViolation(uPlayer, "Exceeded regular explosions threshold "..tostring(iRegularExplosionThreshold).." - Count: "..tostring(iCounter));
+		end
+	end
+	
+	for uPlayer, iCounter in pairs(tblVehicleExplosions) do
+		if(iCounter >= iVehicleExplosionThreshold) then
+			logViolation(uPlayer, "Exceeded vehicle explosions threshold "..tostring(iVehicleExplosionThreshold).." - Count: "..tostring(iCounter));
+		end
+	end
+	
+	tblPlayerProjectiles = {};
+	tblRegularExplosions = {};
+	tblVehicleExplosions = {};
+	
+end, iExplosionCheckInterval, 0);
