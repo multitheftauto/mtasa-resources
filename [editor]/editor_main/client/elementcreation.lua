9ca4abd3b26ee812ca59ec2a347264cafacf531d
PrimelPrime <Name9615@gmail.com>
2025-05-28 11:31:16 +0200

    Reintroduce if check for creationParameters.position
    Main problem was the if check before with (~= string) which caused the fix to fail

--- [editor]/editor_main/client/elementcreation.lua
+++ [editor]/editor_main/client/elementcreation.lua
@@ -39,7 +39,11 @@ function doCreateElement ( elementType, resourceName, creationParameters, attach
 	creationParameters = creationParameters or {}
 	if not creationParameters.position then
 		local targetX, targetY, targetZ = processCameraLineOfSight()
-		creationParameters.position = {targetX, targetY, targetZ + .5}
+		if not elementType == "object" or not elementType == "vehicle" then
+			creationParameters.position = nil
+		else
+			creationParameters.position = {targetX, targetY, targetZ + .5}
+		end
 		creationParameters.rotation = getRandomRotation()
 	end
 
