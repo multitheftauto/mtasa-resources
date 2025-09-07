f35325879ce3b52ffc00d5e2de5e529884a47fa0
Eren Yaman <68560906+shadylua@users.noreply.github.com>
2025-09-07 17:29:56 +0300

    Update reload.lua

--- [gameplay]/reload/reload.lua
+++ [gameplay]/reload/reload.lua
@@ -1,5 +1,37 @@
-local function reloadWeapon()
-	reloadPedWeapon(client)
+local spamTimer = {}
+
+function reloadWeapon(player)
+    if getControlState(player,"aimweapon") then return end
+    if getControlState(player,"fire") then return end
+    if isPedDead(player) then return end
+    if isPedInVehicle(player) then return end
+    if getPedWeapon(player) == 0 then return end
+
+    if getPedAmmoInClip(player) == getPedTotalAmmo(player) then return end
+
+    if spamTimer[player] and getTickCount() - spamTimer[player] < 3000 then 
+        return 
+    end
+
+    spamTimer[player] = getTickCount()
+
+    reloadPedWeapon(player)
 end
-addEvent("relWep", true)
-addEventHandler("relWep", root, reloadWeapon)
+
+addEventHandler("onPlayerJoin", root, function() 
+    bindKey(source, "r", "down", reloadWeapon) 
+end) 
+
+addEventHandler("onResourceStart", resourceRoot, function() 
+    for _, player in ipairs (getElementsByType("player")) do 
+        bindKey(player, "r", "down", reloadWeapon)
+    end 
+end)
+
+addEventHandler("onPlayerWeaponReload", root, function()
+    spamTimer[source] = getTickCount()
+end)
+
+addEventHandler("onPlayerQuit", root, function() 
+    spamTimer[source] = nil
+end)
