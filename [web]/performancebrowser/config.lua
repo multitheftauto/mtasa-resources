98a477ecbb72bc544496ccfe7b8e36248b27de36
justin <39979049+jjustns@users.noreply.github.com>
2026-03-03 20:19:33 -0400

    Move "Lua time recordings" functions to one shared file

--- [web]/performancebrowser/config.lua
+++
@@ -1,7 +0,0 @@
--- Lua time recordings config
-g_LuaTimingRecordings = {
-	Enabled = true,
-	Frequency = 5000, -- in milliseconds
-	HistoryLength = 100, -- number of records to keep
-	HighCPUResourcesAmount = 10, -- percentage threshold
-}
--- [web]/performancebrowser/performancebrowser_client.lua
+++ [web]/performancebrowser/performancebrowser_client.lua
@@ -6,7 +6,6 @@
 
 me = localPlayer
 local bSupportsStats = getPerformanceStats ~= nil
-g_HighUsageResources = {}
 
 addEventHandler("onClientResourceStart", resourceRoot,
 	function (resource)
@@ -45,40 +44,3 @@ addEventHandler('onClientRequestStats', me,
 		triggerServerEvent( "onNotifyStats", resourceRoot, a, b, username, queryCategoryName, queryOptionsText, queryFilterText )
 	end
 )
-
--- Date/time formatting function
-local function getDateTimeString()
-    local time = getRealTime()
-    local weekday = ({"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"})[time.weekday + 1]
-    -- Weekday, DD.MM.YYYY, hh:mm:ss
-    return ("%s, %02d.%02d.%d, %02d:%02d:%02d"):format(weekday, time.monthday, time.month + 1, time.year + 1900, time.hour, time.minute, time.second)
-end
-
--- Save high CPU resources function (based on IPB alarm.lua)
-function saveHighCPUResources()
-    local columns, rows = getPerformanceStats("Lua timing")
-	
-    if not rows then
-        return
-    end
-
-    for index, row in pairs(rows) do
-        local usageText = row[2]:gsub("[^0-9%.]", "")
-        local usage = math.floor(tonumber(usageText) or 0)
-
-        if (usage > g_LuaTimingRecordings.HighCPUResourcesAmount) then
-            -- Record this high usage to table
-            table.insert(g_HighUsageResources, 1, {row[1], row[2], getDateTimeString()})
-
-            -- Make sure it won't get too big
-            if #g_HighUsageResources > g_LuaTimingRecordings.HistoryLength then
-                table.remove(g_HighUsageResources, g_LuaTimingRecordings.HistoryLength)
-            end
-        end
-    end
-end
-
--- Start monitoring timer
-if (g_LuaTimingRecordings.Enabled) then
-	setTimer(saveHighCPUResources, g_LuaTimingRecordings.Frequency, 0)
-end
