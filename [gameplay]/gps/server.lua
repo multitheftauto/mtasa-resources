a1bcaabf2fbf7c40df8ed365ece1519d8e2af3f6
T-MaxWiese-T <65263210+T-MaxWiese-T@users.noreply.github.com>
2026-06-27 21:00:54 +0200

    Update server.lua

--- [gameplay]/gps/server.lua
+++ [gameplay]/gps/server.lua
@@ -8,6 +8,12 @@ addEvent('onServerCall', true)
 addEventHandler('onServerCall', root,
 	function(fnName, ...)
 		if allowedRPC[fnName] then
+			if fnName == "calculatePathByCoords" or fnName == "calculatePathByNodeIDs" then
+				if get("routeMode") ~= "server" then
+					outputDebugString("SECURITY: " .. getPlayerName(client) .. " wanted to force server GPS even though client mode is active!", 2)
+					return 
+				end
+			end
 			_G[fnName](...)
 		end
 	end
@@ -17,6 +23,13 @@ addEvent('onServerCallback', true)
 addEventHandler('onServerCallback', root,
 	function(crID, fnName, ...)
 		if allowedRPC[fnName] then
+			if fnName == "calculatePathByCoords" or fnName == "calculatePathByNodeIDs" then
+				if get("routeMode") ~= "server" then
+					outputDebugString("SECURITY: " .. getPlayerName(client) .. " wanted to force server GPS even though client mode is active", 2)
+					triggerClientEvent(source, 'onServerCallbackReply', resourceRoot, crID, false)
+					return
+				end
+			end
 			triggerClientEvent(source, 'onServerCallbackReply', resourceRoot, crID, _G[fnName](...))
 		end
 	end
