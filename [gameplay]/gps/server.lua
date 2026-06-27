fde659d2d58057d3bea02d64ad8f2321b9927db9
T-MaxWiese-T <65263210+T-MaxWiese-T@users.noreply.github.com>
2026-06-27 20:45:37 +0200

    Update server.lua

--- [gameplay]/gps/server.lua
+++ [gameplay]/gps/server.lua
@@ -1,3 +1,27 @@
+local allowedRPC = {
+	calculatePathByCoords = true,
+	calculatePathByNodeIDs = true,
+	spawnPlayer = true
+}
+
+addEvent('onServerCall', true)
+addEventHandler('onServerCall', root,
+	function(fnName, ...)
+		if allowedRPC[fnName] then
+			_G[fnName](...)
+		end
+	end
+)
+
+addEvent('onServerCallback', true)
+addEventHandler('onServerCallback', root,
+	function(crID, fnName, ...)
+		if allowedRPC[fnName] then
+			triggerClientEvent(source, 'onServerCallbackReply', resourceRoot, crID, _G[fnName](...))
+		end
+	end
+)
+
 local function sendSettingsToClient(player)
 	local target = player or root
     local mode = get("routeMode") or "client"
