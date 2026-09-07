2fdfb4b2d889d5a8f79ec4a098a767aef34bbff0
Arran <arrantuna@hotmail.co.uk>
2026-09-07 16:21:35 +0100

    Fix #747 use gridlist instead of tabs

--- [managers]/helpmanager/helpmanager_client.lua
+++ [managers]/helpmanager/helpmanager_client.lua
@@ -1,9 +1,35 @@
 local pagesXml
-local wndHelp, tPanel, btnClose
-local tab = {}
-local memo = {}
+local wndHelp, btnClose, pageList, pageColumn
+local pages = {}
+local selectedResource
 local popupQueue = {}
 
+function selectHelpPage(resource)
+	selectedResource = resource
+	for pageResource, page in pairs(pages) do
+		guiSetVisible(page.memo, pageResource == resource)
+	end
+end
+
+function refreshPageList()
+	guiGridListClear(pageList)
+	local pageResources = {}
+	for resource in pairs(pages) do
+		table.insert(pageResources, resource)
+	end
+	if not pages[selectedResource] then
+		selectedResource = pageResources[1]
+	end
+	for _, resource in ipairs(pageResources) do
+		local row = guiGridListAddRow(pageList)
+		guiGridListSetItemText(pageList, row, pageColumn, pages[resource].title, false, false)
+		guiGridListSetItemData(pageList, row, pageColumn, getResourceName(resource))
+		if resource == selectedResource then
+			guiGridListSetSelectedItem(pageList, row, pageColumn)
+		end
+	end
+	selectHelpPage(selectedResource)
+end
 local HELP_KEY = "F9"
 local HELP_COMMAND = "gamehelp"
 local POPUP_TIMEOUT = 15000 --ms
@@ -18,9 +44,21 @@ addEvent("onHelpHidden")
 addEventHandler("onClientResourceStart", resourceRoot,
 	function ()
 		wndHelp  = guiCreateWindow(.2, .2, .6, .6, "Help", true)
-		tPanel   = guiCreateTabPanel(0, .05, 1, .85, true, wndHelp)
 		btnClose = guiCreateButton(.4, .92, .2, .08, "Close", true, wndHelp)
+		pageList = guiCreateGridList(.02, .08, .14, .82, true, wndHelp)
+		pageColumn = guiGridListAddColumn(pageList, "Help page", 1)
 
+		addEventHandler("onClientGUIClick", pageList,
+			function()
+				local row = guiGridListGetSelectedItem(pageList)
+				if row == -1 then return end
+				local resourceName = guiGridListGetItemData(pageList, row, pageColumn)
+				local resource = resourceName and getResourceFromName(resourceName)
+				if resource and pages[resource] then
+					selectHelpPage(resource)
+				end
+			end, false
+		)
 		guiSetVisible(wndHelp, false)
 
 		guiWindowSetSizable(wndHelp, false)
@@ -67,57 +105,42 @@ function hideHelp()
 end
 addEventHandler("doHideHelp", root, hideHelp)
 
+-- Retain the export name; the returned content element is now a memo, not a tab.
 function addHelpTab(resource, showPopup)
-
-	if showPopup == nil then
-		showPopup = true
-	end
-
-	-- block duplicates
-	if tab[resource] then
+	if pages[resource] then
 		return false
 	end
 
-	local tabtext = getResourceName(resource)
-
-	local helpnode = getResourceConfig(":" .. getResourceName(resource) .. "/help.xml")
-
+	local title = getResourceName(resource)
+	local text = ""
+	local helpnode = getResourceConfig(":" .. title .. "/help.xml")
 	if helpnode then
-
-		local nameattribute = xmlNodeGetAttribute(helpnode, "title");
-
-		if nameattribute then
-			tabtext = nameattribute;
-		end
-
+		title = xmlNodeGetAttribute(helpnode, "title") or title
+		text = xmlNodeGetValue(helpnode) or ""
 	end
 
-	tab[resource] = guiCreateTab( tabtext , tPanel)
+	local memo = guiCreateMemo(.18, .08, .80, .82, text, true, wndHelp)
+	guiMemoSetReadOnly(memo, true)
+	pages[resource] = { title = title, memo = memo }
+	refreshPageList()
 
-	if showPopup then
+	if showPopup ~= false then
 		addHelpPopup(resource)
 	end
-
-	return tab[resource]
+	return memo
 end
 
 function removeHelpTab(resource)
 	if type(resource) ~= "userdata" then
 		resource = getResourceFromName(getElementID(source))
 	end
-
-	if not resource or not tab[resource] then
+	if not resource or not pages[resource] then
 		return false
 	end
 
-	if memo[resource] then
-		destroyElement(memo[resource])
-		memo[resource] = nil
-	end
-
-	guiDeleteTab(tab[resource], tPanel)
-	tab[resource] = nil
-
+	destroyElement(pages[resource].memo)
+	pages[resource] = nil
+	refreshPageList()
 	return true
 end
 addEventHandler("onClientResourceStop", root, removeHelpTab)
@@ -127,27 +150,14 @@ function addHelpTabFromXML(resource)
 	if type(resource) ~= "userdata" then
 		resource = getResourceFromName(getElementID(source))
 	end
+	if not resource or pages[resource] then return false end
 
-	if not resource then return false end
-
-	-- block duplicates
-	if tab[resource] then
-		return false
-	end
-
-	local helpnode = getResourceConfig(":"..getResourceName(resource).."/help.xml")
+	local helpnode = getResourceConfig(":" .. getResourceName(resource) .. "/help.xml")
 	if helpnode then
-		local helptext = xmlNodeGetValue(helpnode)
-		local showPopup = xmlNodeGetAttribute(helpnode, "popup") ~= "no"
-		if helptext then
-			addHelpTab(resource, showPopup)
-			memo[resource] = guiCreateMemo(.05, .05, .9, .9, helptext, true, tab[resource])
-			guiMemoSetReadOnly(memo[resource], true)
-		end
+		addHelpTab(resource, xmlNodeGetAttribute(helpnode, "popup") ~= "no")
 	end
 end
 addEventHandler("onClientResourceStart", root, addHelpTabFromXML)
-
 function clientToggleHelp(state)
 	if state ~= true and state ~= false then
 		state = not guiGetVisible(wndHelp)
